5459fde73895c4e31f11518889f531f8178c5ea5
Ксения <xskuznetsova@gmail.com>
2017-12-09 21:42:19 +0300

    Цикл for.

---
+++ IterationsAndDecisions/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IterationsAndDecisions
+{
+    class Program
+    {
+        static void Main()
+        {
+            ForLoopExample();
+            Console.ReadLine();
+        }
+
+        // Базовый цикл for.
+        static void ForLoopExample()
+        {
+            // Обратите внимание, что переменная i видима только в контексте цикла.
+            for (int i = 0; i < 4; i++) Console.WriteLine($"Число равно: {i}");
+            // Здесь переменная i больше видимой не будет.
+            Console.WriteLine();
+        }
+    }
+}
