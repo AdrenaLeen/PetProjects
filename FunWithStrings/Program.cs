11d7233f0aaa78ea2076d27ad7d8243d76a7e545
Ксения <xskuznetsova@gmail.com>
2017-12-09 20:27:25 +0300

    Базовые манипуляции строками.

---
+++ FunWithStrings/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithStrings
+{
+    class Program
+    {
+        static void Main()
+        {
+            BasicStringFunctionality();
+            Console.ReadLine();
+        }
+
+        static void BasicStringFunctionality()
+        {
+            Console.WriteLine("=> Базовая функциональность строк:");
+            string firstName = "Фредди";
+
+            // Значение firstName.
+            Console.WriteLine("Значение firstName: {0}", firstName);
+
+            // Длина firstName.
+            Console.WriteLine("firstName содержит {0} букв.", firstName.Length);
+
+            // firstName в верхнем регистре.
+            Console.WriteLine("firstName в верхнем регистре: {0}", firstName.ToUpper());
+
+            // firstName в нижнем регистре.
+            Console.WriteLine("firstName в нижнем регистре: {0}", firstName.ToLower());
+
+            // Содержит ли firstName букву и?
+            Console.WriteLine("firstName содержит букву и?: {0}", firstName.Contains("и"));
+
+            // firstName после замены.
+            Console.WriteLine("Новое имя: {0}", firstName.Replace("ди", ""));
+            Console.WriteLine();
+        }
+    }
+}
