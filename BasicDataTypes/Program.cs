aa99a325179ad8b6ab63268a784fe7d3b2c3d1ed
Ксения <xskuznetsova@gmail.com>
2017-12-09 19:40:05 +0300

    Иерархия классов для типов данных.

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -12,6 +12,7 @@ namespace BasicDataTypes
         {
             LocalVarDeclarations();
             NewingDataTypes();
+            ObjectFunctionality();
             Console.ReadLine();
         }
 
@@ -52,6 +53,17 @@ namespace BasicDataTypes
             DateTime dt = new DateTime();
 
             Console.WriteLine("{0}, {1}, {2}, {3}", b, i, d, dt);
+            Console.WriteLine();
+        }
+
+        private static void ObjectFunctionality()
+        {
+            Console.WriteLine("=> Функциональность System.Object:");
+            // Ключевое слово int языка C# - это в действительности сокращение для типа System.Int32, который наследует от System.Object следующие члены:
+            Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
+            Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
+            Console.WriteLine("12.ToString() = {0}", 12.ToString());
+            Console.WriteLine("12.GetType() = {0}", 12.GetType());
             Console.WriteLine();
         }
     }
