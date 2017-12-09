984478f894885616aec3e410b88e05fe3de8be9f
Ксения <xskuznetsova@gmail.com>
2017-12-09 21:44:23 +0300

    Цикл foreach.

--- IterationsAndDecisions/Program.cs
+++ IterationsAndDecisions/Program.cs
@@ -11,6 +11,7 @@ namespace IterationsAndDecisions
         static void Main()
         {
             ForLoopExample();
+            ForEachLoopExample();
             Console.ReadLine();
         }
 
@@ -20,6 +21,18 @@ namespace IterationsAndDecisions
             // Обратите внимание, что переменная i видима только в контексте цикла.
             for (int i = 0; i < 4; i++) Console.WriteLine($"Число равно: {i}");
             // Здесь переменная i больше видимой не будет.
+            Console.WriteLine();
+        }
+
+        // Проход по элементам массива посредством foreach.
+        static void ForEachLoopExample()
+        {
+            string[] carTypes = { "Ford", "BMW", "Yugo", "Honda" };
+            foreach (string c in carTypes) Console.WriteLine(c);
+
+            int[] myInts = { 10, 20, 30, 40 };
+            foreach (int i in myInts) Console.WriteLine(i);
+
             Console.WriteLine();
         }
     }
