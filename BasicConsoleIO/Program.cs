d2b22f25d06a0a40b3df5e6337666016ff96a318
Ксения <xskuznetsova@gmail.com>
2017-12-09 19:02:12 +0300

    Форматирование числовых данных.

--- BasicConsoleIO/Program.cs
+++ BasicConsoleIO/Program.cs
@@ -19,7 +19,24 @@ namespace BasicConsoleIO
             // Выводит: 20, 10, 30
             Console.WriteLine("{1}, {0}, {2}", 10, 20, 30);
 
+            FormatNumericalData();
+
             Console.ReadLine();
+        }
+
+        // Демонстрация применения некоторых дескрипторов формата.
+        private static void FormatNumericalData()
+        {
+            Console.WriteLine("Значение 99999 в различных форматах:");
+            Console.WriteLine("Формат c: {0:c}", 99999);
+            Console.WriteLine("Формат d9: {0:d9}", 99999);
+            Console.WriteLine("Формат f3: {0:f3}", 99999);
+            Console.WriteLine("Формат n: {0:n}", 99999);
+            // Обратите внимание, что использование для символа шестнадцатеричного формата верхнего или нижнего регистра определяет регистр отображаемых символов.
+            Console.WriteLine("Формат E: {0:E}", 99999);
+            Console.WriteLine("Формат e: {0:e}", 99999);
+            Console.WriteLine("Формат X: {0:X}", 99999);
+            Console.WriteLine("Формат x: {0:x}", 99999);
         }
 
         private static void GetUserData()
