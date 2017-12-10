dbfb23054cbcc0e8670e59fe9f5308ce1eb19e4d
Ксения <xskuznetsova@gmail.com>
2017-12-10 21:22:10 +0300

    Инкапсуляция с использованием традиционных методов доступа и изменения.

---
+++ EmployeeApp/Employee.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Employee
+    {
+        // Поля данных.
+        private string empName;
+        private int empID;
+        private float currPay;
+
+        // Конструкторы.
+        public Employee() { }
+        public Employee(string name, int id, float pay)
+        {
+            empName = name;
+            empID = id;
+            currPay = pay;
+        }
+
+        // Методы.
+        public void GiveBonus(float amount)
+        {
+            currPay += amount;
+        }
+
+        public void DisplayStats()
+        {
+            // Имя сотрудника
+            Console.WriteLine($"Имя: {empName}");
+            // Идентификационный номер сотрудника
+            Console.WriteLine($"ID: {empID}");
+            // Текущая выплата
+            Console.WriteLine($"Выплата: {currPay}");
+        }
+
+        // Метод доступа (метод get).
+        public string GetName()
+        {
+            return empName;
+        }
+
+        // Метод изменения (метод set).
+        public void SetName(string name)
+        {
+            // Перед присваиванием проверить входное значение.
+            if (name.Length > 15) Console.WriteLine("Ошибка! Длина имени превышает 15 символов!");
+            else empName = name;
+        }
+    }
+}
---
+++ EmployeeApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Employee emp = new Employee();
+
+            Console.WriteLine("***** Инкапсуляция *****");
+            Employee emp2 = new Employee("Марвин", 456, 30000);
+            emp2.GiveBonus(1000);
+            emp2.DisplayStats();
+
+            // Использовать методы get/set для взаимодействия с именем сотрудника, представленного объектом.
+            emp2.SetName("Марв");
+            Console.WriteLine($"Сотрудник назван: {emp2.GetName()}");
+
+            Console.ReadLine();
+        }
+    }
+}
