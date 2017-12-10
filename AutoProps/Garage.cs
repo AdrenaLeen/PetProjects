7cc1825aa3e3a76004a391898b3d8b0e76f3a6a0
Ксения <xskuznetsova@gmail.com>
2017-12-10 22:11:55 +0300

    Автоматические свойства и стандартные значения.

---
+++ AutoProps/Garage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Garage
+    {
+        // Скрытое поддерживающее поле int установлено в 0!
+        public int NumberOfCars { get; set; }
+
+        // Скрытое поддерживающее поле Car установлено в null!
+        public Car MyAuto { get; set; }
+
+        // Для переопределения стандартных значений, присвоенных скрытым поддерживающим полям, должны использоваться конструкторы.
+        public Garage()
+        {
+            MyAuto = new Car();
+            NumberOfCars = 1;
+        }
+        public Garage(Car car, int number)
+        {
+            MyAuto = car;
+            NumberOfCars = number;
+        }
+    }
+}
--- AutoProps/Program.cs
+++ AutoProps/Program.cs
@@ -11,12 +11,22 @@ namespace AutoProps
         static void Main()
         {
             Console.WriteLine("***** Автоматические свойства *****");
+            // Создать объект автомобиля.
             Car c = new Car();
             c.PetName = "Франк";
             c.Speed = 55;
             c.Color = "Красный";
             Console.WriteLine($"Вашу машину зовут {c.PetName}? Это странно...");
             c.DisplayStats();
+            // Поместить автомобиль в гараж.
+            Garage g = new Garage();
+            g.MyAuto = c;
+            // Нормально, выводится стандартное значение 0.
+            // Вывести количество автомобилей в гараже
+            Console.WriteLine($"Число машин в гараже: {g.NumberOfCars}");
+            // Ошибка времени выполнения! Поддерживающее поле в данный момент равно null!
+            // Вывести название автомобиля.
+            Console.WriteLine($"Вашу машину зовут: {g.MyAuto.PetName}");
 
             Console.ReadLine();
         }
