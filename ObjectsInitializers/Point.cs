a5fa9d4b28ddbf2c79c265966e2723bef3dcc8fd
Ксения <xskuznetsova@gmail.com>
2017-12-10 22:46:33 +0300

    Вызов специальных конструкторов с помощью синтаксиса инициализации.

--- ObjectsInitializers/Point.cs
+++ ObjectsInitializers/Point.cs
@@ -6,22 +6,33 @@ using System.Threading.Tasks;
 
 namespace ObjectsInitializers
 {
+    public enum PointColor
+    { LightBlue, BloodRed, Gold }
+
     class Point
     {
         public int X { get; set; }
         public int Y { get; set; }
+        public PointColor Color { get; set; }
 
         public Point(int xVal, int yVal)
         {
             X = xVal;
             Y = yVal;
+            Color = PointColor.Gold;
         }
 
-        public Point() { }
+        public Point(PointColor ptColor)
+        {
+            Color = ptColor;
+        }
+
+        public Point() : this(PointColor.BloodRed) { }
 
         public void DisplayStats()
         {
             Console.WriteLine($"[{X}, {Y}]");
+            Console.WriteLine("Point является {0}", Color);
         }
     }
 }
--- ObjectsInitializers/Program.cs
+++ ObjectsInitializers/Program.cs
@@ -25,8 +25,20 @@ namespace ObjectsInitializers
             Console.WriteLine();
 
             // Создать объект Point, используя синтаксис инициализации объектов.
+            // Здесь стандартный конструктор вызывается неявно.
             Point finalPoint = new Point { X = 30, Y = 30 };
             finalPoint.DisplayStats();
+            Console.WriteLine();
+
+            // Здесь стандартный конструктор вызывается явно.
+            Point finalPoint2 = new Point() { X = 30, Y = 30 };
+
+            // Вызов специального конструктора.
+            Point pt = new Point(10, 16) { X = 100, Y = 100 };
+
+            // Вызов более интересного специального конструктора с помощью синтаксиса инициализации.
+            Point goldPoint = new Point(PointColor.Gold) { X = 90, Y = 20 };
+            goldPoint.DisplayStats();
             Console.WriteLine();
 
             Console.ReadLine();
