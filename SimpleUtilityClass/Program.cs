e2707b7fe82189ebc13fdf5f25f30a773619f34d
Ксения <xskuznetsova@gmail.com>
2017-12-10 21:06:26 +0300

    Определение статических классов.

---
+++ SimpleUtilityClass/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtilityClass
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Статические классы *****");
+
+            // Это работает нормально.
+            TimeUtilClass.PrintDate();
+            TimeUtilClass.PrintTime();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleUtilityClass/TimeUtilClass.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleUtilityClass
+{
+    // Статические классы могут содержать только статические члены!
+    static class TimeUtilClass
+    {
+        public static void PrintTime()
+        {
+            Console.WriteLine(DateTime.Now.ToShortTimeString());
+        }
+
+        public static void PrintDate()
+        {
+            Console.WriteLine(DateTime.Today.ToShortDateString());
+        }
+    }
+}
