bc36be2cad16904decbe04dd52325b2215c51c3d
Ксения <xskuznetsova@gmail.com>
2017-12-10 19:56:55 +0300

    Тип класса C#.

---
+++ SimpleClassExample/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Car
+    {
+        // 'Состояние' объекта Car.
+        public string petName;
+        public int currSpeed;
+
+        // Функциональность Car.
+        public void PrintState()
+        {
+            Console.WriteLine($"{petName} едет со скоростью {currSpeed} км/ч.");
+        }
+
+        public void SpeedUp(int delta)
+        {
+            currSpeed += delta;
+        }
+    }
+}
---
+++ SimpleClassExample/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleClassExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Тип класса *****");
+
+            // Разместить в памяти и сконфигурировать объект Car.
+            Car myCar = new Car();
+            myCar.petName = "Генри";
+            myCar.currSpeed = 10;
+
+            // Увеличить скорость автомобиля в несколько раз и вывести новое состояние.
+            for (int i = 0; i <= 10; i++)
+            {
+                myCar.SpeedUp(5);
+                myCar.PrintState();
+            }
+            Console.ReadLine();
+        }
+    }
+}
