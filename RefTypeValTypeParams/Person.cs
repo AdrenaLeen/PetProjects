275673149fd5d780b595e2bbf5005254b33dcdf0
Ксения <xskuznetsova@gmail.com>
2017-12-10 19:28:33 +0300

    Передача ссылочных типов по значению.

---
+++ RefTypeValTypeParams/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefTypeValTypeParams
+{
+    class Person
+    {
+        public string personName;
+        public int personAge;
+
+        // Конструкторы.
+        public Person(string name, int age)
+        {
+            personName = name;
+            personAge = age;
+        }
+        public Person() { }
+
+        public void Display()
+        {
+            Console.WriteLine($"Имя: {personName}, Возраст: {personAge}");
+        }
+    }
+}
---
+++ RefTypeValTypeParams/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RefTypeValTypeParams
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Передача ссылочных типов по значению
+            Console.WriteLine("***** Передача объектов Person по значению *****");
+
+            Person fred = new Person("Фред", 12);
+            // Перед вызовом
+            Console.WriteLine("До вызова по значению Person равно:");
+            fred.Display();
+            SendAPersonByValue(fred);
+            // После вызова
+            Console.WriteLine("После вызова по значению Person равно:");
+            fred.Display();
+
+            Console.ReadLine();
+        }
+
+        static void SendAPersonByValue(Person p)
+        {
+            // Изменить значение возраста в p?
+            p.personAge = 99;
+
+            // Увидит ли вызывающий код это изменение?
+            p = new Person("Никки", 99);
+        }
+    }
+}
