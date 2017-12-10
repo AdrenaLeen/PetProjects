3c3c486c584fa4721e6a854d57b1143d5a9061c6
Ксения <xskuznetsova@gmail.com>
2017-12-10 22:34:56 +0300

    Синтаксис инициализации объектов.

---
+++ ObjectsInitializers/Point.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsInitializers
+{
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xVal, int yVal)
+        {
+            X = xVal;
+            Y = yVal;
+        }
+
+        public Point() { }
+
+        public void DisplayStats()
+        {
+            Console.WriteLine($"[{X}, {Y}]");
+        }
+    }
+}
---
+++ ObjectsInitializers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectsInitializers
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Синтаксис инициализации объектов *****");
+
+            // Создать объект Point, устанавливая каждое свойство вручную.
+            Point firstPoint = new Point();
+            firstPoint.X = 10;
+            firstPoint.Y = 10;
+            firstPoint.DisplayStats();
+            Console.WriteLine();
+
+            // Создать объект Point посредством специального конструктора.
+            Point anotherPoint = new Point(20, 20);
+            anotherPoint.DisplayStats();
+            Console.WriteLine();
+
+            // Создать объект Point, используя синтаксис инициализации объектов.
+            Point finalPoint = new Point { X = 30, Y = 30 };
+            finalPoint.DisplayStats();
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
