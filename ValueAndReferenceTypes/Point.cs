c8d8e5813bb990e65cc68823b53675b8e0c5327c
Ксения <xskuznetsova@gmail.com>
2017-12-10 15:42:26 +0300

    Типы значений, ссылочные типы и операция присваивания.

---
+++ ValueAndReferenceTypes/Point.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndReferenceTypes
+{
+    struct Point
+    {
+        // Поля структуры.
+        public int X;
+        public int Y;
+
+        // Специальный конструктор.
+        public Point(int XPos, int YPos)
+        {
+            X = XPos;
+            Y = YPos;
+        }
+
+        // Добавить 1 к позиции (X, Y).
+        public void Increment()
+        {
+            X++; Y++;
+        }
+
+        // Вычесть 1 из позиции (X, Y).
+        public void Decrement()
+        {
+            X--; Y--;
+        }
+
+        // Отобразить текущую позицию.
+        public void Display()
+        {
+            Console.WriteLine($"X = {X}, Y = {Y}");
+        }
+    }
+}
---
+++ ValueAndReferenceTypes/PointRef.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ValueAndReferenceTypes
+{
+    // Классы всегда являются ссылочными типами.
+    class PointRef
+    {
+        // Те же самые члены, что и в структуре Point...
+        public int X;
+        public int Y;
+
+        // Не забудьте изменить имя конструктора на PointRef!
+        public PointRef(int XPos, int YPos)
+        {
+            X = XPos;
+            Y = YPos;
+        }
+
+        public void Increment()
+        {
+            X++; Y++;
+        }
+
+        public void Decrement()
+        {
+            X--; Y--;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine($"X = {X}, Y = {Y}");
+        }
+    }
+}
