003bc1241b42bf4972093737c4bc9c75c6eb1c22
Ксения <xskuznetsova@gmail.com>
2017-12-10 21:57:34 +0300

    Автоматические свойства.

---
+++ AutoProps/Car.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoProps
+{
+    class Car
+    {
+        // Автоматические свойства! Нет нужды в определении поддерживаемых полей.
+        public string PetName { get; set; }
+        public int Speed { get; set; }
+        public string Color { get; set; }
+
+        // Свойство только для чтения? Допустимо!
+        public int MyReadOnlyProp { get; }
+    }
+}
