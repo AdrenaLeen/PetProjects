a5bbc2f621f0168edb90621f47bb1be57b0ac494
Ксения <xskuznetsova@gmail.com>
2017-12-10 14:51:22 +0300

    Тип enum.

---
+++ FunWithEnums/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithEnums
+{
+    // Специальное перечисление.
+    // Начать нумерацию со значения 102.
+    // Значения элементов в перечислении не обязательно должны быть последовательными!
+    enum EmpType : byte
+    {
+        Manager = 10,
+        Grunt = 1,
+        Contractor = 100,
+        VicePresident = 9
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
