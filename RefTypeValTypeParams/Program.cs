25e82bf3dcaae24bfdb0e4114d9dd4e696c01cc1
Ксения <xskuznetsova@gmail.com>
2017-12-10 19:32:20 +0300

    Передача ссылочных типов по ссылке.

--- RefTypeValTypeParams/Program.cs
+++ RefTypeValTypeParams/Program.cs
@@ -22,6 +22,18 @@ namespace RefTypeValTypeParams
             Console.WriteLine("После вызова по значению Person равно:");
             fred.Display();
 
+            // Передача ссылочных типов по ссылке
+            Console.WriteLine("***** Передача объектов Person по ссылке *****");
+
+            Person mel = new Person("Мэл", 23);
+            // Перед вызовом
+            Console.WriteLine("До вызова по ссылке Person равно:");
+            mel.Display();
+            SendAPersonByReference(ref mel);
+            // После вызова
+            Console.WriteLine("После вызова по ссылке Person равно:");
+            mel.Display();
+
             Console.ReadLine();
         }
 
@@ -32,6 +44,15 @@ namespace RefTypeValTypeParams
 
             // Увидит ли вызывающий код это изменение?
             p = new Person("Никки", 99);
+        }
+
+        static void SendAPersonByReference(ref Person p)
+        {
+            // Изменить некоторые данные в p.
+            p.personAge = 555;
+
+            // p теперь указывает на новый объект в куче!
+            p = new Person("Никки", 999);
         }
     }
 }
