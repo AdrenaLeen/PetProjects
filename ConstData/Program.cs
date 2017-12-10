42f73af735109b5cf2762f7a9e88cc2778eacdcc
Ксения <xskuznetsova@gmail.com>
2017-12-10 23:08:00 +0300

    Работа с данными константных полей.

---
+++ ConstData/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConstData
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Константы *****");
+            Console.WriteLine($"Значение PI равно: {MyMathClass.PI}");
+            LocalConstStringVariable();
+
+            Console.ReadLine();
+        }
+
+        static void LocalConstStringVariable()
+        {
+            // Доступ к локальным константным данным можно получать напрямую.
+            const string fixedStr = "Фиксированные строковые данные";
+            Console.WriteLine(fixedStr);
+        }
+    }
+}
