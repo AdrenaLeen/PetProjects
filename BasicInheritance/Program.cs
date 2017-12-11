32950d52b911949d0409b19f8cef2595b77455e8
Ксения <xskuznetsova@gmail.com>
2017-12-11 21:44:14 +0300

    Базовый механизм наследования.

---
+++ BasicInheritance/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicInheritance
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Базовое наследование *****");
+            // Создать объект Car и установить базовую скорость.
+            Car myCar = new Car(80);
+
+            // Установить текущую скорость и вывести её на консоль.
+            myCar.Speed = 50;
+            Console.WriteLine($"Моя машина едет со скоростью {myCar.Speed} км/ч");
+
+            Console.ReadLine();
+        }
+    }
+}
