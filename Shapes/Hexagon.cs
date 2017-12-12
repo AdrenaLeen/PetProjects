ef6dcfb5fe07bc941b2ba28c51d35631edc49fdd
Ксения <xskuznetsova@gmail.com>
2017-12-13 01:42:39 +0300

    Полиморфные интерфейсы.

---
+++ Shapes/Hexagon.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    // Класс Hexagon переопределяет метод Draw().
+    class Hexagon : Shape
+    {
+        public Hexagon() { }
+        public Hexagon(string name) : base(name) { }
+        public override void Draw()
+        {
+            Console.WriteLine($"Отрисовка {PetName} класса Hexagon");
+        }
+    }
+}
---
+++ Shapes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Полиморфизм *****");
+            Hexagon hex = new Hexagon("Бэт");
+            hex.Draw();
+            Circle cir = new Circle("Синди");
+            // Вызывает реализацию базового класса!
+            cir.Draw();
+
+            // Создать массив совместимых с Shape объектов.
+            Shape[] myShapes = {new Hexagon(), new Circle(), new Hexagon("Мик"), new Circle("Бэт"), new Hexagon("Линда")};
+
+            // Пройти в цикле по всем элементам и взаимодействовать с полиморфным интерфейсом.
+            foreach (Shape s in myShapes) s.Draw();
+
+            Console.ReadLine();
+        }
+    }
+}
