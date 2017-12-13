9cfd64b9d519735444fb92468b313cd896aac4e2
Ксения <xskuznetsova@gmail.com>
2017-12-13 22:32:46 +0300

    Переопределение метода System.Object.GetHashCode().

--- ObjectOverrides/Person.cs
+++ ObjectOverrides/Person.cs
@@ -7,11 +7,13 @@ using System.Threading.Tasks;
 namespace ObjectOverrides
 {
     // Не забывайте, что класс Person расширяет Object.
+    // Предположим, что имеется свойство SSN.
     class Person
     {
         public string FirstName { get; set; } = "";
         public string LastName { get; set; } = "";
         public int Age { get; set; }
+        public string SSN { get; set; } = "";
 
         public Person(string fName, string lName, int personAge)
         {
@@ -30,6 +32,12 @@ namespace ObjectOverrides
         {
             // Больше нет необходимости приводить obj с типу Person, т.к. у всех типов имеется метод ToString().
             return obj.ToString() == ToString();
+        }
+
+        // Возвратить хеш-код на основе значения, возвращаемого методом ToString() для объекта Person.
+        public override int GetHashCode()
+        {
+            return SSN.GetHashCode();
         }
     }
 }
