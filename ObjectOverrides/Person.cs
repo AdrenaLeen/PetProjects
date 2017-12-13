46b4cd747ce2465f2275885fea7c7f20103cd7ba
Ксения <xskuznetsova@gmail.com>
2017-12-13 22:16:06 +0300

    Главный родительский класс System.Object.

---
+++ ObjectOverrides/Person.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    // Не забывайте, что класс Person расширяет Object.
+    class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+        public Person() { }
+    }
+}
---
+++ ObjectOverrides/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ObjectOverrides
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Тип System.Object *****");
+            Person p1 = new Person();
+            // Использовать унаследованные члены System.Object.
+            Console.WriteLine($"ToString: {p1.ToString()}");
+            Console.WriteLine($"Хэш: {p1.GetHashCode()}");
+            Console.WriteLine($"Тип: {p1.GetType()}");
+            // Создать другие ссылки на p1.
+            Person p2 = p1;
+            object o = p2;
+            // Указывают ли ссылки на один и тот же объект в памяти?
+            if (o.Equals(p1) && p2.Equals(o))
+            {
+                Console.WriteLine("Одинаковые сущности!");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
