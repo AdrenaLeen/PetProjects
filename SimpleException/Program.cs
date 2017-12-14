73e8d6cc399aebb340748db6abd365e2f90c18a4
Ксения <xskuznetsova@gmail.com>
2017-12-14 15:03:32 +0300

    Перехват исключений.

--- SimpleException/Program.cs
+++ SimpleException/Program.cs
@@ -14,7 +14,19 @@ namespace SimpleException
             Console.WriteLine("=> Создание автомобиля!");
             Car myCar = new Car("Циппи", 20);
             myCar.CrankTunes(true);
-            for (int i = 0; i < 10; i++) myCar.Accelerate(10);
+            try
+            {
+                for (int i = 0; i < 10; i++) myCar.Accelerate(10);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("*** Ошибка! ***");
+                Console.WriteLine($"Метод: {e.TargetSite}");
+                Console.WriteLine($"Сообщение: {e.Message}");
+                Console.WriteLine($"Источник: {e.Source}");
+            }
+            // Ошибка была обработана, продолжается выполнение следующего оператора.
+            Console.WriteLine("***** Вне логики исключения *****");
 
             Console.ReadLine();
         }
