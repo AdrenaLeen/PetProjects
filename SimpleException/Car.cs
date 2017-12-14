bc0d5ffc78d80e6568f3f91607dc3e0ae9ce9ed2
Ксения <xskuznetsova@gmail.com>
2017-12-14 14:49:58 +0300

    Простейший пример.

---
+++ SimpleException/Car.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Car
+    {
+        // Константа для представления максимальной скорости.
+        public const int MaxSpeed = 100;
+
+        // Свойства автомобиля.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // Не вышел ли автомобиль из строя?
+        private bool carIsDead;
+
+        // В автомобиле имеется радиоприёмник.
+        private Radio theMusicBox = new Radio();
+
+        // Конструкторы.
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Делегировать запрос внутреннему объекту.
+            theMusicBox.TurnOn(state);
+        }
+
+        // Проверить, не перегрелся ли автомобиль.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead) Console.WriteLine($"{PetName} сломан...");
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    Console.WriteLine($"{PetName} перегрет!");
+                    CurrentSpeed = 0;
+                    carIsDead = true;
+                }
+                else Console.WriteLine($"=> CurrentSpeed = {CurrentSpeed}");
+            }
+        }
+    }
+}
---
+++ SimpleException/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleException
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Пример простого исключения *****");
+            Console.WriteLine("=> Создание автомобиля!");
+            Car myCar = new Car("Циппи", 20);
+            myCar.CrankTunes(true);
+            for (int i = 0; i < 10; i++) myCar.Accelerate(10);
+
+            Console.ReadLine();
+        }
+    }
+}
