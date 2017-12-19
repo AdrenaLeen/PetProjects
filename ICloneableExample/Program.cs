1ab1829587d6cf3711926088d293f1066fc5fc3d
Ксения <xskuznetsova@gmail.com>
2017-12-19 16:19:14 +0300

    Сравнение интерфейсных типов и абстрактных базовых классов.

---
+++ ICloneableExample/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace ICloneableExample
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Интерфейсы на первый взгляд *****");
+
+            // Все эти классы поддерживают интерфейс ICloneable.
+            string myStr = "Привет";
+            OperatingSystem unixOS = new OperatingSystem(PlatformID.Unix, new Version());
+            SqlConnection sqlCnn = new SqlConnection();
+
+            // Следовательно, все они могут быть переданы методу, принимающему параметр типа ICloneable.
+            CloneMe(myStr);
+            CloneMe(unixOS);
+            CloneMe(sqlCnn);
+            Console.ReadLine();
+        }
+
+        private static void CloneMe(ICloneable c)
+        {
+            // Клонировать то, что получено, и вывести его имя.
+            object theClone = c.Clone();
+            Console.WriteLine($"Ваша копия: {theClone.GetType().Name}");
+        }
+    }
+}
