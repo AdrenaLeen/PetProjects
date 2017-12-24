9e409317b8b904d02a42a44e2fcf396fe18dc6b8
Ксения <xskuznetsova@gmail.com>
2017-12-24 20:24:10 +0300

    Интерфейсы IEnumerable и IEnumerator.

---
+++ CustomEnumerator/Garage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CustomEnumerator
+{
+    // Garage содержит набор объектов Car.
+    public class Garage : IEnumerable
+    {
+        // System.Array уже реализует IEnumerator!
+        private Car[] carArray = new Car[4];
+
+        // При запуске заполнить несколькими объектами Car.
+        public Garage()
+        {
+            carArray[0] = new Car("Расти", 30);
+            carArray[1] = new Car("Драндулет", 55);
+            carArray[2] = new Car("Циппи", 30);
+            carArray[3] = new Car("Фред", 30);
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            // Возвратить IEnumerator объекта массива.
+            return carArray.GetEnumerator();
+        }
+    }
+}
---
+++ CustomEnumerator/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumerator
+{
+    // Код выглядит корректным...
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** IEnumerable / IEnumerator *****");
+            Garage carLot = new Garage();
+
+            // Проход по всем объектам Car в коллекции?
+            foreach (Car c in carLot) Console.WriteLine($"{c.PetName} едет со скоростью {c.CurrentSpeed} км/ч");
+
+            Console.ReadLine();
+        }
+    }
+}
