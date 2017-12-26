13c4a07877f75ab4419ac5d4c0589092a1b8e123
Ксения <xskuznetsova@gmail.com>
2017-12-26 22:21:39 +0300

    Интерфейс ICloneable.

---
+++ CloneablePoint/Point.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    // Теперь Point поддерживает способность клонирования.
+    class Point : ICloneable
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+        public Point() { }
+
+        // Переопределить Object.ToString().
+        public override string ToString()
+        {
+            return $"X = {X}; Y = {Y}";
+        }
+
+        // Возвратить копию текущего объекта.
+        public object Clone()
+        {
+            return new Point(X, Y);
+        }
+    }
+}
---
+++ CloneablePoint/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CloneablePoint
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Клонирование объекта *****");
+            
+            // Две ссылки на один и тот же объект!
+            Point p1 = new Point(50, 50);
+            Point p2 = p1;
+            p2.X = 0;
+            Console.WriteLine(p1);
+            Console.WriteLine(p2);
+
+            // Обратите внимание, что Clone() возвращает простой тип object. Для получения производного типа требуется явное приведение.
+            Point p3 = new Point(100, 100);
+            Point p4 = (Point)p3.Clone();
+
+            // Изменить p4.X (что не приводит к изменению p3.X).
+            p4.X = 0;
+
+            // Вывести все объекты.
+            Console.WriteLine(p3);
+            Console.WriteLine(p4);
+
+            Console.ReadLine();
+        }
+    }
+}
