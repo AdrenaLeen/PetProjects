2fce60ce7a8e47624bfbc59cdac57f86006cf1cb
Ксения <xskuznetsova@gmail.com>
2017-12-26 18:03:48 +0300

    Построение методов итератора с использованием ключевого слова yield.

---
+++ CustomEnumeratorWithYield/Car.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomEnumeratorWithYield
+{
+    class Car
+    {
+        // Константа для представления максимальной скорости.
+        public const int MaxSpeed = 100;
+
+        // Свойства автомобиля.
+        public int CurrentSpeed { get; set; } = 0;
+        public string PetName { get; set; } = "";
+
+        // Не вышел ли автомобиль из строя?
+        private bool carIsDead;
+
+        // В автомобиле имеется радиоприёмник.
+        private Radio theMusicBox = new Radio();
+
+        // Конструкторы.
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            CurrentSpeed = speed;
+            PetName = name;
+        }
+
+        public void CrankTunes(bool state)
+        {
+            // Делегировать запрос внутреннему объекту.
+            theMusicBox.TurnOn(state);
+        }
+
+        // Проверить, не перегрелся ли автомобиль.
+        public void Accelerate(int delta)
+        {
+            if (carIsDead) Console.WriteLine($"{PetName} сломан...");
+            else
+            {
+                CurrentSpeed += delta;
+                if (CurrentSpeed > MaxSpeed)
+                {
+                    carIsDead = true;
+                    CurrentSpeed = 0;
+                    // Использовать ключевое слово throw для генерации исключения.
+                    // Мы хотим обращаться к свойству HelpLink, поэтому необходимо создать локальную переменную перед генерацией объекта Exception.
+                    Exception ex = new Exception($"{PetName} перегрет!");
+                    ex.HelpLink = "http://www.CarsRUs.com";
+                    // Предоставить специальные данные, касающиеся ошибки.
+                    ex.Data.Add("TimeStamp", $"Машина вышла из строя {DateTime.Now}");
+                    ex.Data.Add("Cause", "Забыли убрать ногу с газа.");
+                    throw ex;
+                }
+                // Вывод текущей скорости.
+                else Console.WriteLine($"=> CurrentSpeed = {CurrentSpeed}");
+            }
+        }
+    }
+}
---
+++ CustomEnumeratorWithYield/Garage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace CustomEnumeratorWithYield
+{
+    // Garage содержит набор объектов Car.
+    public class Garage : IEnumerable
+    {
+        // System.Array уже реализует IEnumerator!
+        private Car[] carArray = new Car[4];
+
+        // При запуске заполнить несколькими объектами Car.
+        public Garage()
+        {
+            carArray[0] = new Car("Расти", 30);
+            carArray[1] = new Car("Драндулет", 55);
+            carArray[2] = new Car("Циппи", 30);
+            carArray[3] = new Car("Фред", 30);
+        }
+
+        // Метод итератора.
+        public IEnumerator GetEnumerator()
+        {
+            foreach (Car c in carArray) yield return c;
+        }
+    }
+}
