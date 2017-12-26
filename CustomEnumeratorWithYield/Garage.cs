ec6d53ea4e192325a9c31f9b43d773a1abbbc221
Ксения <xskuznetsova@gmail.com>
2017-12-26 18:05:24 +0300

    Построение именованного итератора.

--- CustomEnumeratorWithYield/Garage.cs
+++ CustomEnumeratorWithYield/Garage.cs
@@ -27,5 +27,19 @@ namespace CustomEnumeratorWithYield
         {
             foreach (Car c in carArray) yield return c;
         }
+
+        public IEnumerable GetTheCars(bool returnRevesed)
+        {
+            // Возвратить элементы в обратном порядке.
+            if (returnRevesed)
+            {
+                for (int i = carArray.Length; i != 0; i--) yield return carArray[i - 1];
+            }
+            else
+            {
+                // Возвратить элементы в том порядке, в каком они размещены в массиве.
+                foreach (Car c in carArray) yield return c;
+            }
+        }
     }
 }
--- CustomEnumeratorWithYield/Program.cs
+++ CustomEnumeratorWithYield/Program.cs
@@ -10,7 +10,17 @@ namespace CustomEnumeratorWithYield
     {
         static void Main()
         {
-            
+            Console.WriteLine("***** Ключевое слово yield *****");
+            Garage carLot = new Garage();
+
+            // Получить элементы, используя GetEnumerator().
+            foreach (Car c in carLot) Console.WriteLine($"{c.PetName} едет со скоростью {c.CurrentSpeed} км/ч");
+            Console.WriteLine();
+
+            // Получить элементы (в обратном порядке!), используя именованный итератор.
+            foreach (Car c in carLot.GetTheCars(true)) Console.WriteLine($"{c.PetName} едет со скоростью {c.CurrentSpeed} км/ч");
+
+            Console.ReadLine();
         }
     }
 }
