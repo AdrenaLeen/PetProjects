b127441d677b7bb6f2d0fe0c3a90c8461bf88b71
Ксения <xskuznetsova@gmail.com>
2017-12-30 01:17:27 +0300

    Выведение параметров типа.

---
+++ CustomGenericMethods/MyGenericMethods.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomGenericMethods
+{
+    public static class MyGenericMethods
+    {
+        // Этот метод будет менять местами два элемента типа, указанного в параметре <T>.
+        public static void Swap<T>(ref T a, ref T b)
+        {
+            Console.WriteLine($"Вы послали методу Swap() {typeof(T)}");
+            T temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+
+        public static void DisplayBaseClass<T>()
+        {
+            // BaseType - это метод, используемый в рефлексии.
+            Console.WriteLine($"Базовый класс {typeof(T)}: {typeof(T).BaseType}.");
+        }
+    }
+}
--- CustomGenericMethods/Program.cs
+++ CustomGenericMethods/Program.cs
@@ -15,16 +15,27 @@ namespace CustomGenericMethods
             // Обмен двух целочисленных значений.
             int a = 10, b = 90;
             Console.WriteLine($"До обмена: {a}, {b}");
-            Swap<int>(ref a, ref b);
+            MyGenericMethods.Swap<int>(ref a, ref b);
             Console.WriteLine($"После обмена: {a}, {b}");
             Console.WriteLine();
 
             // Обмен двух строковых значений.
             string s1 = "Привет", s2 = "Всем";
             Console.WriteLine($"До обмена: {s1} {s2}");
-            Swap<string>(ref s1, ref s2);
+            MyGenericMethods.Swap<string>(ref s1, ref s2);
             Console.WriteLine($"После обмена: {s1} {s2}");
             Console.WriteLine();
+
+            // Компилятор выведет тип System.Boolean.
+            bool b1 = true, b2 = false;
+            Console.WriteLine($"До обмена: {b1}, {b2}");
+            MyGenericMethods.Swap(ref b1, ref b2);
+            Console.WriteLine($"После обмена: {b1}, {b2}");
+            Console.WriteLine();
+
+            // Если метод не принимает параметров, то должно быть указан параметр типа.
+            MyGenericMethods.DisplayBaseClass<int>();
+            MyGenericMethods.DisplayBaseClass<string>();
 
             Console.ReadLine();
         }
@@ -41,16 +52,6 @@ namespace CustomGenericMethods
         static void Swap(ref Person a, ref Person b)
         {
             Person temp;
-            temp = a;
-            a = b;
-            b = temp;
-        }
-
-        // Этот метод будет менять местами два элемента типа, указанного в параметре <T>.
-        public static void Swap<T>(ref T a, ref T b)
-        {
-            Console.WriteLine($"Вы послали методу Swap() {typeof(T)}");
-            T temp;
             temp = a;
             a = b;
             b = temp;
