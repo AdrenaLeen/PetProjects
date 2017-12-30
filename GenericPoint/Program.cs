f9fff7557697749fe2e66c7086abd49f01a658ad
Ксения <xskuznetsova@gmail.com>
2017-12-30 21:22:11 +0300

    Реализовала сброс координат точки с помощью ключевого слова default в обобщённом коде.

--- GenericPoint/Program.cs
+++ GenericPoint/Program.cs
@@ -10,11 +10,22 @@ namespace GenericPoint
     {
         static void Main()
         {
+            Console.WriteLine("***** Обобщённые структуры *****");
+
             // Точка с координатами типа int.
             Point<int> p = new Point<int>(10, 10);
+            Console.WriteLine($"p.ToString()={p}");
+            p.ResetPoint();
+            Console.WriteLine($"p.ToString()={p}");
+            Console.WriteLine();
 
             // Точка с координатами типа double.
             Point<double> p2 = new Point<double>(5.4, 3.3);
+            Console.WriteLine($"p2.ToString()={p2}");
+            p2.ResetPoint();
+            Console.WriteLine($"p2.ToString()={p2}");
+
+            Console.ReadLine();
         }
     }
 }
