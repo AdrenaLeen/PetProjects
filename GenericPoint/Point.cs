8c569262f312883c883f1b2ab7967da51b8e6b63
Ксения <xskuznetsova@gmail.com>
2017-12-30 21:12:55 +0300

    Создала специальную обобщённую структуру.

---
+++ GenericPoint/Point.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPoint
+{
+    // Обобщённая структура Point.
+    public struct Point<T>
+    {
+        // Обобщённые данные состояния.
+        public T X { get; set; }
+        public T Y { get; set; }
+
+        // Обобщённый конструктор.
+        public Point(T xVal, T yVal)
+        {
+            X = xVal;
+            Y = yVal;
+        }
+
+        public override string ToString()
+        {
+            return $"[{X}, {Y}]";
+        }
+
+        // Сбросить поля в стандартные значения для заданного параметра типа.
+        public void ResetPoint()
+        {
+            X = default(T);
+            Y = default(T);
+        }
+    }
+}
---
+++ GenericPoint/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericPoint
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Точка с координатами типа int.
+            Point<int> p = new Point<int>(10, 10);
+
+            // Точка с координатами типа double.
+            Point<double> p2 = new Point<double>(5.4, 3.3);
+        }
+    }
+}
