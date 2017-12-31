93b2bf99405307b5fc9d697afc8e3d6c3ef66b13
Ксения <xskuznetsova@gmail.com>
2017-12-31 18:26:09 +0300

    Реализован пример простейшего делегата.

---
+++ SimpleDelegate/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    class Program
+    {
+        // Этот делегат может указывать на любой метод, который принимает два целочисленных значения и возвращает целое значение.
+        public delegate int BinaryOp(int x, int y);
+
+        static void Main()
+        {
+            Console.WriteLine("***** Пример простейшего делегата *****");
+
+            // Создать объект делегата BinaryOp, который "указывает" на SimpleMath.Add().
+            BinaryOp b = new BinaryOp(SimpleMath.Add);
+
+            // Вызвать метод Add() косвенно с использованием объекта делегата.
+            // В действительности здесь вызывается метод Invoke()!
+            Console.WriteLine($"10 + 10 равно {b(10, 10)}");
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ SimpleDelegate/SimpleMath.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDelegate
+{
+    // Этот класс содержит методы, на которые будет указывать BinaryOp.
+    class SimpleMath
+    {
+        public static int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        public static int Subtract(int x, int y)
+        {
+            return x - y;
+        }
+
+        public static int SquareNumber(int a)
+        {
+            return a * a;
+        }
+    }
+}
