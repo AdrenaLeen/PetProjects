191a7945a081e85677699c1a303c3a118294b630
Ксения <xskuznetsova@gmail.com>
2018-01-03 01:50:23 +0300

    Обобщённые делегаты.

---
+++ GenericDelegate/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericDelegate
+{
+    class Program
+    {
+        // Этот обобщённый делегат может вызывать любой метод, который возвращает void и принимает единственный параметр типа T.
+        public delegate void MyGenericDelegate<T>(T arg);
+
+        static void Main()
+        {
+            Console.WriteLine("***** Обобщённые делегаты *****");
+
+            // Зарегистрировать цели.
+            MyGenericDelegate<string> strTarget = new MyGenericDelegate<string>(StringTarget);
+            strTarget("Некоторые строковые данные");
+
+            MyGenericDelegate<int> intTarget = new MyGenericDelegate<int>(IntTarget);
+            intTarget(9);
+            Console.ReadLine();
+        }
+
+        static void StringTarget(string arg)
+        {
+            Console.WriteLine($"arg в верхнем регистре: {arg.ToUpper()}");
+        }
+
+        static void IntTarget(int arg)
+        {
+            Console.WriteLine($"++arg: {++arg}");
+        }
+    }
+}
