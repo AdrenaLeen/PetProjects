3a8ae15c4a9596b0a04a85bf6314a2326813c9a0
Ксения <xskuznetsova@gmail.com>
2018-01-05 17:28:15 +0300

    Прослушивание входящих событий.

--- CarEvents/Program.cs
+++ CarEvents/Program.cs
@@ -10,6 +10,39 @@ namespace CarEvents
     {
         static void Main()
         {
+            Console.WriteLine("***** События *****");
+            Car c1 = new Car("СлагБаг", 100, 10);
+
+            // Зарегистрировать обработчики событий.
+            c1.AboutToBlow += CarIsAlmostDoomed;
+            c1.AboutToBlow += CarAboutToBlow;
+            c1.Exploded += CarExploded;
+
+            Console.WriteLine("***** Увеличение скорости *****");
+            for (int i = 0; i < 6; i++) c1.Accelerate(20);
+
+            // Удалить метод CarExploded из списка вызовов.
+            c1.Exploded -= CarExploded;
+
+            Console.WriteLine("***** Увеличение скорости *****");
+            for (int i = 0; i < 6; i++) c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+
+        public static void CarAboutToBlow(string msg)
+        {
+            Console.WriteLine(msg);
+        }
+
+        public static void CarIsAlmostDoomed(string msg)
+        {
+            Console.WriteLine($"=> Критическое сообщение от объекта Car: {msg}");
+        }
+
+        public static void CarExploded(string msg)
+        {
+            Console.WriteLine(msg);
         }
     }
 }
