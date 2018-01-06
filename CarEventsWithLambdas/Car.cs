e40fa16b86a9eb007152c39dd6a5fb7f9e473d0a
Ксения <xskuznetsova@gmail.com>
2018-01-06 23:57:49 +0300

    Лямбда-выражения.

---
+++ CarEventsWithLambdas/Car.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEventsWithLambdas
+{
+    class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        public string PetName { get; set; }
+        private bool carIsDead;
+
+        public Car() { MaxSpeed = 100; }
+        public Car(string name, int maxSp, int currSp)
+        {
+            CurrentSpeed = currSp;
+            MaxSpeed = maxSp;
+            PetName = name;
+        }
+
+        // Этот делегат работает в сочетании с событиями Car.
+        public delegate void CarEngineHandler(object sender, CarEventArgs e);
+
+        // Car может отправлять следующие события.
+        public event CarEngineHandler Exploded;
+        public event CarEngineHandler AboutToBlow;
+
+        public void Accelerate(int delta)
+        {
+            // Если автомобиль сломан, инициировать событие Exploded.
+            if (carIsDead) Exploded?.Invoke(this, new CarEventArgs("Простите, эта машина умерла..."));
+            else
+            {
+                CurrentSpeed += delta;
+
+                // Почти сломан?
+                if (10 == MaxSpeed - CurrentSpeed) AboutToBlow?.Invoke(this, new CarEventArgs("Осторожнее, приятель! Скоро взорвётся!"));
+
+                // Всё ещё в порядке!
+                if (CurrentSpeed >= MaxSpeed) carIsDead = true;
+                else Console.WriteLine($"CurrentSpeed = {CurrentSpeed}");
+            }
+        }
+    }
+}
---
+++ CarEventsWithLambdas/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarEventsWithLambdas
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Лямбда-выражения *****");
+            
+            // Создать объект Car обычным образом.
+            Car c1 = new Car("СлагБаг", 100, 10);
+
+            // Привязаться к событиям с помощью лямбда-выражений.
+            c1.AboutToBlow += (sender, e) => { Console.WriteLine(e.msg); };
+            c1.Exploded += (sender, e) => { Console.WriteLine(e.msg); };
+
+            // Увеличить скорость (это инициирует события).
+            Console.WriteLine("***** Увеличение скорости *****");
+            for (int i = 0; i < 6; i++) c1.Accelerate(20);
+
+            Console.ReadLine();
+        }
+    }
+}
