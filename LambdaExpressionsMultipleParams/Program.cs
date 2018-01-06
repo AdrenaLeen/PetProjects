647ea845aadc2ae7e33b738d5b3851c8163a3819
Ксения <xskuznetsova@gmail.com>
2018-01-06 22:24:35 +0300

    Лямбда-выражения с несколькими параметрами и без параметров.

---
+++ LambdaExpressionsMultipleParams/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LambdaExpressionsMultipleParams
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Зарегистрировать делегат как лямбда-выражение.
+            SimpleMath m = new SimpleMath();
+            m.SetMathHandler((msg, result) =>
+            {
+                Console.WriteLine($"Сообщение: {msg}, Результат: {result}");
+            });
+
+            // Это приведёт к выполнению лямбда-выражения.
+            m.Add(10, 10);
+
+            Console.ReadLine();
+        }
+    }
+}
