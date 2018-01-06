42e522b8e6344d379498c8404b984b00c2448ce9
Ксения <xskuznetsova@gmail.com>
2018-01-06 17:37:25 +0300

    Доступ к локальным переменным.

--- AnonimousMethods/Program.cs
+++ AnonimousMethods/Program.cs
@@ -11,15 +11,20 @@ namespace AnonimousMethods
         static void Main()
         {
             Console.WriteLine("***** Анонимные методы *****");
+            int aboutToBlowCounter = 0;
+
+            // Создать объект Car обычным образом.
             Car c1 = new Car("СлагБаг", 100, 10);
 
             // Зарегистрировать обработчики событий как анонимные методы.
             c1.AboutToBlow += delegate
             {
+                aboutToBlowCounter++;
                 Console.WriteLine("Эй! Слишком быстро!");
             };
             c1.AboutToBlow += delegate (object sender, CarEventArgs e)
             {
+                aboutToBlowCounter++;
                 Console.WriteLine($"Сообщение от объекта Car: {e.msg}");
             };
             c1.Exploded += delegate (object sender, CarEventArgs e)
@@ -29,6 +34,7 @@ namespace AnonimousMethods
 
             // В конце концов, этот код будет инициировать события.
             for (int i = 0; i < 6; i++) c1.Accelerate(20);
+            Console.WriteLine($"Событие AboutToBlow было инициировано {aboutToBlowCounter} раз.");
 
             Console.ReadLine();
         }
