555aa7c15cdeb26bd3e34dca77cd415227adf024
Ксения <xskuznetsova@gmail.com>
2018-01-07 20:48:03 +0300

    Получить объект лица Homer и вывести данные.

---
+++ StringIndexer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StringIndexer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Индексаторы *****");
+            PersonCollection myPeople = new PersonCollection();
+            myPeople["Homer"] = new Person("Гомер", "Симпсон", 40);
+            myPeople["Marge"] = new Person("Мардж", "Симпсон", 38);
+
+            // Получить объект лица "Homer" и вывести данные.
+            Person homer = myPeople["Homer"];
+            Console.WriteLine(homer);
+
+            Console.ReadLine();
+        }
+    }
+}
