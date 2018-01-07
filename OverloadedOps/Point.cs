da317382d751baf787fd1c79b6b7be843cd8fb2a
Ксения <xskuznetsova@gmail.com>
2018-01-07 21:46:13 +0300

    Перегрузка бинарных операций.

---
+++ OverloadedOps/Point.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    // Простой будничный класс C#.
+    // Более интеллектуальный тип Point.
+    class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+
+        public override string ToString() => $"[{X}, {Y}]";
+
+        // Перегруженная операция +
+        public static Point operator +(Point p1, Point p2)
+        {
+            return new Point(p1.X + p2.X, p1.Y + p2.Y);
+        }
+
+        // Перегруженная операция -
+        public static Point operator -(Point p1, Point p2)
+        {
+            return new Point(p1.X - p2.X, p1.Y - p2.Y);
+        }
+
+        public static Point operator +(Point p1, int change)
+        {
+            return new Point(p1.X + change, p1.Y + change);
+        }
+
+        public static Point operator +(int change, Point p1)
+        {
+            return new Point(p1.X + change, p1.Y + change);
+        }
+    }
+}
---
+++ OverloadedOps/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    class Program
+    {
+        // Сложение и вычитание двух точек?
+        static void Main()
+        {
+            Console.WriteLine("***** Перегрузка операторов *****");
+
+            // Создать две точки.
+            Point ptOne = new Point(100, 100);
+            Point ptTwo = new Point(40, 40);
+            Console.WriteLine($"ptOne = {ptOne}");
+            Console.WriteLine($"ptTwo = {ptTwo}");
+
+            // Сложить две точки, чтобы получить большую?
+            Console.WriteLine($"ptOne + ptTwo: {ptOne + ptTwo}");
+
+            // Вычесть одну точку из другой, чтобы получить меньшую?
+            Console.WriteLine("ptOne - ptTwo: {0} ", ptOne - ptTwo);
+
+            // Выводит [110, 110]
+            Point biggerPoint = ptOne + 10;
+            Console.WriteLine($"ptOne + 10 = {biggerPoint}");
+
+            // Выводит [120, 120]
+            Console.WriteLine($"10 + biggerPoint = {10 + biggerPoint}");
+            Console.WriteLine();
+
+            Console.ReadLine();
+        }
+    }
+}
