828a61cf2e04be5607d0b45822b86bf5cd932365
Ксения <xskuznetsova@gmail.com>
2018-01-07 18:03:47 +0300

    Индексаторы.

---
+++ SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SimpleIndexer
+{
+    // Добавим индексатор к существующему определению класса.
+    class PersonCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+
+        // Приведение для вызывающего кода.
+        public Person GetPerson(int pos)
+        {
+            return (Person)arPeople[pos];
+        }
+
+        // Вставка только объектов Person.
+        public void AddPerson(Person p)
+        {
+            arPeople.Add(p);
+        }
+
+        public void ClearPeople()
+        {
+            arPeople.Clear();
+        }
+
+        public int Count { get { return arPeople.Count; } }
+
+        // Поддержка перечисления с помощью foreach.
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return arPeople.GetEnumerator();
+        }
+
+        // Специальный индексатор для этого класса.
+        public Person this[int index]
+        {
+            get { return (Person)arPeople[index]; }
+            set { arPeople.Insert(index, value); }
+        }
+    }
+}
---
+++ SimpleIndexer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleIndexer
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Индексаторы *****");
+            PersonCollection myPeople = new PersonCollection();
+
+            // Добавить объекты с применением синтаксиса индексатора.
+            myPeople[0] = new Person("Гомер", "Симпсон", 40);
+            myPeople[1] = new Person("Мардж", "Симпсон", 38);
+            myPeople[2] = new Person("Лиза", "Симпсон", 9);
+            myPeople[3] = new Person("Барт", "Симпсон", 7);
+            myPeople[4] = new Person("Мэгги", "Симпсон", 2);
+
+            // Получить и отобразить элементы с использованием индексатора.
+            for (int i = 0; i < myPeople.Count; i++)
+            {
+                Console.WriteLine($"Номер лица: {i}");
+                Console.WriteLine($"Имя и фамилия: {myPeople[i].FirstName} {myPeople[i].LastName}");
+                Console.WriteLine($"Возраст: {myPeople[i].Age}");
+                Console.WriteLine();
+            }
+
+            UseGenericListOfPeople();
+
+            Console.ReadLine();
+        }
+
+        static void UseGenericListOfPeople()
+        {
+            List<Person> myPeople = new List<Person>();
+            myPeople.Add(new Person("Лиза", "Симпсон", 9));
+            myPeople.Add(new Person("Барт", "Симпсон", 7));
+
+            // Изменить первый объект лица с помощью индексатора.
+            myPeople[0] = new Person("Мэгги", "Симпсон", 2);
+
+            // Now obtain and display each item using indexer.
+            for (int i = 0; i < myPeople.Count; i++)
+            {
+                Console.WriteLine($"Номер лица: {i}");
+                Console.WriteLine($"Имя и фамилия: {myPeople[i].FirstName} {myPeople[i].LastName}");
+                Console.WriteLine($"Возраст: {myPeople[i].Age}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
