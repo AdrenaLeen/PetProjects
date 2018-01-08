e10d94f98352ee31f7af877a4f7394b0f679af12
Ксения <xskuznetsova@gmail.com>
2018-01-08 22:39:53 +0300

    Вызов расширяющих методов.

--- ExtensionMethods/Program.cs
+++ ExtensionMethods/Program.cs
@@ -10,7 +10,25 @@ namespace ExtensionMethods
     {
         static void Main()
         {
-            
+            Console.WriteLine("***** Расширяющие методы *****");
+
+            // В int появилась новая отличительная черта!
+            int myInt = 12345678;
+            myInt.DisplayDefiningAssembly();
+
+            // То же и в DataSet!
+            System.Data.DataSet d = new System.Data.DataSet();
+            d.DisplayDefiningAssembly();
+
+            // И в SoundPlayer!
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer();
+            sp.DisplayDefiningAssembly();
+
+            // Использовать новую функциональность int.
+            Console.WriteLine($"Значение myInt: {myInt}");
+            Console.WriteLine($"Обратный порядок следования цифр myInt: {myInt.ReverseDigits()}");
+
+            Console.ReadLine();
         }
     }
 }
