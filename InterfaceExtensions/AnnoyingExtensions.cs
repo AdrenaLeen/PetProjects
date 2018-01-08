d0dc896a39b8795ee806f049b2e46f2fa4ab3f04
Ксения <xskuznetsova@gmail.com>
2018-01-09 00:22:31 +0300

    Расширение типов, которые реализуют специфичные интерфейсы.

---
+++ InterfaceExtensions/AnnoyingExtensions.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace InterfaceExtensions
+{
+    static class AnnoyingExtensions
+    {
+        public static void PrintDataAndBeep(this IEnumerable iterator)
+        {
+            foreach (var item in iterator)
+            {
+                Console.WriteLine(item);
+                Console.Beep();
+            }
+        }
+    }
+}
---
+++ InterfaceExtensions/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterfaceExtensions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Расширение типов, реализующих интерфейсы *****");
+
+            // System.Array реализует IEnumerable!
+            string[] data = { "Wow", "this", "is", "sort", "of", "annoying", "but", "in", "a", "weird", "way", "fun!"};
+
+            data.PrintDataAndBeep();
+            Console.WriteLine();
+
+            // List<T> реализует IEnumerable!
+            List<int> myInts = new List<int>() { 10, 15, 20 };
+            myInts.PrintDataAndBeep();
+
+            Console.ReadLine();
+        }
+    }
+}
