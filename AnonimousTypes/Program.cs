957c63fb9b8d8d738c89d5199451f935eed546e7
Ксения <xskuznetsova@gmail.com>
2018-01-09 13:38:30 +0300

    Определение анонимного типа.

---
+++ AnonimousTypes/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AnonimousTypes
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Анонимные типы *****");
+
+            // Создать анонимный тип, представляющий автомобиль.
+            var myCar = new { Color = "Ярко-розовый", Make = "Saab", CurrentSpeed = 55 };
+
+            // Вывести на консоль цвет и производителя.
+            Console.WriteLine($"Моя машина - {myCar.Color} {myCar.Make}.");
+
+            // А теперь вызвать вспомогательный метод для построения анонимного типа с указанием аргументов
+            BuildAnonType("BMW", "Чёрный", 90);
+
+            Console.ReadLine();
+        }
+
+        static void BuildAnonType(string make, string color, int currSp)
+        {
+            // Построить анонимный тип с применением входных аргументов.
+            var car = new { Make = make, Color = color, Speed = currSp };
+
+            // Обратите внимание, что теперь этот тип можно использовать для получения данных свойств!
+            Console.WriteLine($"У вас есть {car.Color} {car.Make}, который едет со скоростью {car.Speed} км/ч");
+
+            // Анонимные типы имеют специальные реализации каждого виртуального метода System.Object. Например:
+            Console.WriteLine($"ToString() == {car.ToString()}");
+        }
+    }
+}
