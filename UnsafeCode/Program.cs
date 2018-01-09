75cd61a39874fcf1031f4d90519efe2f8a406c34
Ксения <xskuznetsova@gmail.com>
2018-01-09 20:57:03 +0300

    Работа с операциями * и &.

--- UnsafeCode/Program.cs
+++ UnsafeCode/Program.cs
@@ -14,6 +14,8 @@ namespace UnsafeCode
             SquareIntPointer(&myInt2);
             Console.WriteLine($"myInt: {myInt2}");
 
+            PrintValueAndAddress();
+
             Console.ReadLine();
         }
 
@@ -21,6 +23,21 @@ namespace UnsafeCode
         {
             // Возвести значение в квадрат просто для тестирования.
             *myIntPointer *= *myIntPointer;
+        }
+
+        unsafe static void PrintValueAndAddress()
+        {
+            int myInt;
+
+            // Определить указатель на int и присвоить ему адрес myInt.
+            int* ptrToMyInt = &myInt;
+
+            // Присвоить значение myInt, используя обращение через указатель.
+            *ptrToMyInt = 123;
+
+            // Вывести некоторые значения.
+            Console.WriteLine($"Значение myInt: {myInt}");
+            Console.WriteLine($"Адрес myInt: {(int)&ptrToMyInt:X}");
         }
     }
 }
