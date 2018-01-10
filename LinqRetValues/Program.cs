07e6e3e2f90b3d057e99c2382b4ad3f40f96ccd2
Ксения <xskuznetsova@gmail.com>
2018-01-10 16:00:36 +0300

    Возвращение результатов запроса LINQ.

---
+++ LinqRetValues/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqRetValues
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Возвращение результатов запроса LINQ *****");
+            IEnumerable<string> subset = GetStringSubset();
+            foreach (string item in subset) Console.WriteLine(item);
+
+            Console.ReadLine();
+        }
+
+        static IEnumerable<string> GetStringSubset()
+        {
+            string[] colors = {"Light Red", "Green", "Yellow", "Dark Red", "Red", "Purple"};
+
+            // Обратите внимание, что subset является совместимым с IEnumerable<string> объектом.
+            IEnumerable<string> theRedColors = from c in colors
+                                               where c.Contains("Red")
+                                               select c;
+
+            return theRedColors;
+        }
+    }
+}
