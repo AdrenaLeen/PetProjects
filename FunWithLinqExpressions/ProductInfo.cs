3a3329912a00a04208354a456fc6f0341de8e833
Ксения <xskuznetsova@gmail.com>
2018-01-11 23:09:35 +0300

    Операции запроса LINQ.

---
+++ FunWithLinqExpressions/ProductInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLinqExpressions
+{
+    class ProductInfo
+    {
+        public string Name { get; set; } = "";
+        public string Description { get; set; } = "";
+        public int NumberInStock { get; set; }
+
+        public override string ToString() => $"Название={Name}, Описание={Description}, Номер в магазине={NumberInStock}";
+    }
+}
---
+++ FunWithLinqExpressions/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithLinqExpressions
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Операции запросов *****");
+
+            // Этот массив будет основой для тестирования...
+            ProductInfo[] itemsInStock = new[]
+            {
+                new ProductInfo{ Name = "Mac's Coffee", Description = "Coffee with TEETH", NumberInStock = 24},
+                new ProductInfo{ Name = "Milk Maid Milk", Description = "Milk cow's love", NumberInStock = 100},
+                new ProductInfo{ Name = "Pure Silk Tofu", Description = "Bland as Possible", NumberInStock = 120},
+                new ProductInfo{ Name = "Cruchy Pops", Description = "Cheezy, peppery goodness", NumberInStock = 2},
+                new ProductInfo{ Name = "RipOff Water", Description = "From the tap to your wallet", NumberInStock = 100},
+                new ProductInfo{ Name = "Classic Valpo Pizza", Description = "Everyone loves pizza!", NumberInStock = 73}
+            };
+
+            // Здесь мы будем вызывать разнообразные методы!
+            Console.ReadLine();
+        }
+    }
+}
