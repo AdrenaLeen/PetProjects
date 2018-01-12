ebe2bf0f1b995a4028d4a338ad5b21aaec1991c2
Ксения <xskuznetsova@gmail.com>
2018-01-12 20:49:21 +0300

    Построение выражений запросов с использованием типа Enumerable и лямбда-выражений.

--- LinqUsingEnumerable/Program.cs
+++ LinqUsingEnumerable/Program.cs
@@ -11,6 +11,8 @@ namespace LinqUsingEnumerable
         static void Main()
         {
             QueryStringWithOperators();
+            QueryStringsWithEnumerableAndLambdas();
+            QueryStringsWithEnumerableAndLambdas2();
 
             Console.ReadLine();
         }
@@ -25,6 +27,36 @@ namespace LinqUsingEnumerable
                          orderby game
                          select game;
             foreach (string s in subset) Console.WriteLine($"Элемент: {s}");
+            Console.WriteLine();
+        }
+
+        static void QueryStringsWithEnumerableAndLambdas()
+        {
+            Console.WriteLine("***** Использование Enumerable / лямбда-выражений *****");
+
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2"};
+
+            // Построить выражение запроса с использование расширяющих методов, предоставленных типу Array через тип Enumerable.
+            IEnumerable<string> subset = currentVideoGames.Where(game => game.Contains(" ")).OrderBy(game => game).Select(game => game);
+
+            // Вывести результаты.
+            foreach (string game in subset) Console.WriteLine($"Элемент: {game}");
+
+            Console.WriteLine();
+        }
+
+        static void QueryStringsWithEnumerableAndLambdas2()
+        {
+            Console.WriteLine("***** Использование Enumerable / лямбда-выражений *****");
+
+            string[] currentVideoGames = {"Morrowind", "Uncharted 2", "Fallout 3", "Daxter", "System Shock 2"};
+
+            // Разбить на части.
+            IEnumerable<string> gamesWithSpaces = currentVideoGames.Where(game => game.Contains(" "));
+            IOrderedEnumerable<string> orderedGames = gamesWithSpaces.OrderBy(game => game);
+            IEnumerable<string> subset = orderedGames.Select(game => game);
+
+            foreach (string game in subset) Console.WriteLine($"Элемент: {game}");
             Console.WriteLine();
         }
     }
