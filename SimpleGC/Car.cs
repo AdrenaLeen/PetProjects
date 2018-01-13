39cdf21a42828c7cddd313b75e313f97147d5a77
Ксения <xskuznetsova@gmail.com>
2018-01-14 00:32:15 +0300

    Классы, объекты и ссылки.

---
+++ SimpleGC/Car.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGC
+{
+    class Car
+    {
+        public int CurrentSpeed { get; set; }
+        public string PetName { get; set; }
+
+        public Car() { }
+        public Car(string name, int speed)
+        {
+            PetName = name;
+            CurrentSpeed = speed;
+        }
+        public override string ToString() => $"{PetName} едет со скоростью {CurrentSpeed} км/ч";
+    }
+}
---
+++ SimpleGC/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleGC
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Основы сборки мусора *****");
+
+            // Создать новый объект Car в управляемой куче. Возвращается ссылка на этот объект (refToMyCar).
+            Car refToMyCar = new Car("Циппи", 50);
+
+            // Операция точки (.) используется для обращения к членам объекта с применением ссылочной переменной.
+            Console.WriteLine(refToMyCar.ToString());
+
+            Console.ReadLine();
+        }
+    }
+}
