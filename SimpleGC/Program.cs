6ffbbedf91ad4821910949829a099a09a7c351e6
Ксения <xskuznetsova@gmail.com>
2018-01-14 00:47:33 +0300

    Тип System.GC.

--- SimpleGC/Program.cs
+++ SimpleGC/Program.cs
@@ -13,12 +13,22 @@ namespace SimpleGC
             Console.WriteLine("***** Основы сборки мусора *****");
 
             // Создать новый объект Car в управляемой куче. Возвращается ссылка на этот объект (refToMyCar).
-            Car refToMyCar = new Car("Циппи", 50);
+            Car refToMyCar = new Car("Циппи", 100);
 
-            // Операция точки (.) используется для обращения к членам объекта с применением ссылочной переменной.
-            Console.WriteLine(refToMyCar.ToString());
+            Console.WriteLine(refToMyCar);
 
             MakeACar();
+
+            Console.WriteLine("***** System.GC *****");
+
+            // Вывести оценочное количество байтов, выделенных в куче.
+            Console.WriteLine($"Оценочное количество байтов в куче: {GC.GetTotalMemory(false)}");
+
+            // Значения MaxGeneration начинаются с нуля, поэтому при выводе добавить 1.
+            Console.WriteLine($"У этой ОС {(GC.MaxGeneration + 1)} поколений объектов.");
+
+            // Вывести поколение объекта refToMyCar.
+            Console.WriteLine($"Поколение refToMyCar: {GC.GetGeneration(refToMyCar)}");
 
             Console.ReadLine();
         }
