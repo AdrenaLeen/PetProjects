6ac71469fc03b3e56a52cac765dfbc5a96c9b180
Ксения <xskuznetsova@gmail.com>
2018-01-15 01:42:54 +0300

    Переопределение метода System.Object.Finalize().

---
+++ SimpleFinalize/MyResourceWrapper.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFinalize
+{
+    // Переопределить System.Object.Finalize() посредством синтаксиса финализатора.
+    class MyResourceWrapper
+    {
+        ~MyResourceWrapper()
+        {
+            // Очистить неуправляемые ресурсы.
+            // Выдать звуковой сигнал при уничтожении (только в целях тестирования).
+            Console.Beep();
+        }
+    }
+}
---
+++ SimpleFinalize/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleFinalize
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Финализаторы *****");
+            Console.WriteLine("Нажмите клавишу <Enter>, чтобы завершить приложение");
+            Console.WriteLine("и заставить сборщик мусора вызвать метод Finalize()");
+            Console.WriteLine("для всех финализируемых объектов, которые были созданы в домене этого приложения.");
+            Console.ReadLine();
+            MyResourceWrapper rw = new MyResourceWrapper();
+        }
+    }
+}
