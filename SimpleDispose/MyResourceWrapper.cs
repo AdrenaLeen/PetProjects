4dda48f681d17f99504c05aad50a0a5742bffbe5
Ксения <xskuznetsova@gmail.com>
2018-01-15 22:36:24 +0300

    Построение освобождаемых объектов.

---
+++ SimpleDispose/MyResourceWrapper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDispose
+{
+    // Реализация интерфейса IDisposable.
+    class MyResourceWrapper : IDisposable
+    {
+        // После окончания работы с объектом пользователь объекта должен вызывать этот метод.
+        public void Dispose()
+        {
+            // Очистить неуправляемые ресурсы...
+            // Освободить другие освобождаемые объекты, содержащиеся внутри...
+            // Только для целей тестирования.
+            Console.WriteLine("***** В Dispose! *****");
+        }
+    }
+}
---
+++ SimpleDispose/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleDispose
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Fun with Dispose *****\n");
+
+            // Создать освобождаемый объект и вызвать метод Dispose() для освобождения любых внутренних ресурсов.
+            MyResourceWrapper rw = new MyResourceWrapper();
+            if (rw is IDisposable) rw.Dispose();
+
+            Console.ReadLine();
+        }
+    }
+}
