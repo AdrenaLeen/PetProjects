a8fab49ade0197cb1c83d4ce3054edfe78df17aa
Ксения <xskuznetsova@gmail.com>
2018-01-15 22:52:13 +0300

    Повторное использование ключевого слова using в C#.

--- SimpleDispose/Program.cs
+++ SimpleDispose/Program.cs
@@ -10,11 +10,15 @@ namespace SimpleDispose
     {
         static void Main()
         {
-            Console.WriteLine("***** Fun with Dispose *****\n");
+            Console.WriteLine("***** Освобождение объектов *****\n");
 
             // Создать освобождаемый объект и вызвать метод Dispose() для освобождения любых внутренних ресурсов.
-            MyResourceWrapper rw = new MyResourceWrapper();
-            if (rw is IDisposable) rw.Dispose();
+            // Метод Dispose() вызывается автоматически при выходе за пределы области действия using.
+            // Использование списка с разделителями-запятыми для объявления нескольких объектов, подлежащих освобождению.
+            using (MyResourceWrapper rw = new MyResourceWrapper(), rw2 = new MyResourceWrapper())
+            {
+                // Работать с объектами rw и rw2.
+            }
 
             Console.ReadLine();
         }
