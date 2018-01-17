e188920cc63fed22db1afc3ee4c02738a856500a
Ксения <xskuznetsova@gmail.com>
2018-01-17 22:26:54 +0300

    Формализованный шаблон освобождения.

--- FinalizableDisposableClass/Program.cs
+++ FinalizableDisposableClass/Program.cs
@@ -10,6 +10,16 @@ namespace FinalizableDisposableClass
     {
         static void Main()
         {
+            Console.WriteLine("***** Dispose() / Destructor Combo Platter *****");
+
+            // Вызвать метод Dispose() вручную. Это не приведёт к вызову финализатора.
+            MyResourceWrapper rw = new MyResourceWrapper();
+            rw.Dispose();
+
+            // Не вызывать метод Dispose(). Это приведёт к вызову финализатора и выдаче звукового сигнала.
+            MyResourceWrapper rw2 = new MyResourceWrapper();
+
+            Console.ReadLine();
         }
     }
 }
