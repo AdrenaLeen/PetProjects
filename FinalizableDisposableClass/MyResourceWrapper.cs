f8b87dc37b4f67d8d2aa71589a165771b6dfbb88
Ксения <xskuznetsova@gmail.com>
2018-01-17 22:06:32 +0300

    Создание финализируемых и освобождаемых типов.

---
+++ FinalizableDisposableClass/MyResourceWrapper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalizableDisposableClass
+{
+    // Усовершенствованная оболочка для ресурсов.
+    class MyResourceWrapper : IDisposable
+    {
+        // Сборщик мусора будет вызывать этот метод, если пользователь объекта забыл вызвать Dispose().
+        ~MyResourceWrapper()
+        {
+            // Очистить любые внутренние неуправляемые ресурсы.
+            // **Не** вызывать Dispose() на управляемых объектах.
+        }
+
+        // Пользователь объекта будет вызывать этот метод для как можно более скорой очистки ресурсов.
+        public void Dispose()
+        {
+            // Очистить неуправляемые ресурсы.
+            // Вызвать Dispose() для других освобождаемых объектов, содержащихся внутри.
+            // Если пользователь вызвал Dispose(), то финализация не нужна, поэтому подавить её.
+            GC.SuppressFinalize(this);
+        }
+    }
+}
