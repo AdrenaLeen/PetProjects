8609505f0c482c4cb9d8523fe7b58c2192570c07
Ксения <xskuznetsova@gmail.com>
2018-01-18 17:39:43 +0300

    Определение специальных пространств имён.

---
+++ CustomNamespaces/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyShapes;
+
+namespace CustomNamespaces
+{
+    class Program
+    {
+        static void Main()
+        {
+            Hexagon h = new Hexagon();
+            Circle c = new Circle();
+            Square s = new Square();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CustomNamespaces/Square.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomNamespaces
+{
+    class Square
+    {
+        // Даже ещё более интересные методы...
+    }
+}
