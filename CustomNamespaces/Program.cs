278c5e4b5f0c88a6b0797bd11c46e8eeb4b7a629
Ксения <xskuznetsova@gmail.com>
2018-01-18 18:16:30 +0300

    Разрешение конфликтов имён с помощью псевдонимов.

--- CustomNamespaces/Program.cs
+++ CustomNamespaces/Program.cs
@@ -5,6 +5,10 @@ using System.Text;
 using System.Threading.Tasks;
 using MyShapes;
 using My3DShapes;
+using System.Runtime.Serialization.Formatters.Binary;
+
+// Устранить неоднозначность, используя специальный псевдоним.
+using The3DHexagon = My3DShapes.Hexagon;
 
 namespace CustomNamespaces
 {
@@ -12,9 +16,12 @@ namespace CustomNamespaces
     {
         static void Main()
         {
-            My3DShapes.Hexagon h = new My3DShapes.Hexagon();
+            // На самом деле здесь создаётся экземпляр класса My3DShapes.Hexagon.
+            The3DHexagon h = new The3DHexagon();
             My3DShapes.Circle c = new My3DShapes.Circle();
             MyShapes.Square s = new MyShapes.Square();
+
+            BinaryFormatter b = new BinaryFormatter();
 
             Console.ReadLine();
         }
