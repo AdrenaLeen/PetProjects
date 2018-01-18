adf3c5d4e430c18e485fe3ca9900b0c8297eecd4
Ксения <xskuznetsova@gmail.com>
2018-01-18 20:54:16 +0300

    Построение специальной библиотеки классов.

---
+++ CarLibrary/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarLibrary
+{
+    // Представляет состояние двигателя.
+    public enum EngineState
+    { engineAlive, engineDead }
+
+    // Абстрактный базовый класс в иерархии.
+    public abstract class Car
+    {
+        public string PetName { get; set; }
+        public int CurrentSpeed { get; set; }
+        public int MaxSpeed { get; set; }
+        protected EngineState egnState = EngineState.engineAlive;
+        public EngineState EngineState
+        {
+            get { return egnState; }
+        }
+        public abstract void TurboBoost();
+        public Car() { }
+        public Car(string name, int maxSp, int currSp)
+        {
+            PetName = name;
+            MaxSpeed = maxSp;
+            CurrentSpeed = currSp;
+        }
+    }
+}
---
+++ CarLibrary/DerivedCars.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CarLibrary
+{
+    public class SportsCar : Car
+    {
+        public SportsCar() { }
+        public SportsCar(string name, int maxSp, int currSp) : base(name, maxSp, currSp) { }
+
+        public override void TurboBoost()
+        {
+            MessageBox.Show("Набор скорости!", "Лучше быстрее...");
+        }
+    }
+
+    public class MiniVan : Car
+    {
+        public MiniVan() { }
+        public MiniVan(string name, int maxSp, int currSp) : base(name, maxSp, currSp) { }
+
+        public override void TurboBoost()
+        {
+            // Минивэны имеют плохие возможности ускорения!
+            egnState = EngineState.engineDead;
+            MessageBox.Show("Ой!", "Ваш двигатель взорвался!");
+        }
+    }
+}
