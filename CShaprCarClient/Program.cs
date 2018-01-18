8ffeb45367a9cc09a9c4a2e284ac93d8bc5541b2
Ксения <xskuznetsova@gmail.com>
2018-01-18 22:05:25 +0300

    Построение клиентского приложения C#.

---
+++ CShaprCarClient/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CShaprCarClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Клиентское приложение C# CarLibrary *****");
+            // Создать объект SportsCar.
+            SportsCar viper = new SportsCar("Вайпер", 240, 40);
+            viper.TurboBoost();
+
+            // Создать объект MiniVan.
+            MiniVan mv = new MiniVan();
+            mv.TurboBoost();
+
+            Console.WriteLine("Готово. Нажмите любую клавишу для завершения");
+            Console.ReadLine();
+        }
+    }
+}
