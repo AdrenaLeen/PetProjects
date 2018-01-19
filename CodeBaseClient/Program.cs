2427f6c1734bf95fbdecf374b0d1b86ef5988f69
Ксения <xskuznetsova@gmail.com>
2018-01-19 19:17:39 +0300

    Элемент <codeBase>.

---
+++ CodeBaseClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace CodeBaseClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** CodeBase *****");
+            SportsCar c = new SportsCar();
+            Console.WriteLine("Был выделен спортивный автомобиль.");
+            Console.ReadLine();
+        }
+    }
+}
