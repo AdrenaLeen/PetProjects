5e9b903bac722232e031d5f2381de35ab0e01be4
Ксения <xskuznetsova@gmail.com>
2018-01-19 20:51:42 +0300

    Пространство имён System.Configuration.

---
+++ AppConfigReaderApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Configuration;
+
+namespace AppConfigReaderApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Чтение данных <appSettings> *****");
+            // Извлечь специальные данные из файла *.config.
+            AppSettingsReader ar = new AppSettingsReader();
+            int numbOfTimes = (int)ar.GetValue("RepeatCount", typeof(int));
+            string textColor = (string)ar.GetValue("TextColor", typeof(string));
+            Console.ForegroundColor = (ConsoleColor)Enum.Parse(typeof(ConsoleColor), textColor);
+            // Вывести сообщение нужное количество раз.
+            for (int i = 0; i < numbOfTimes; i++) Console.WriteLine("Привет!");
+            Console.ReadLine();
+        }
+    }
+}
