fb8d3d36f53d0dd51a278ee8e782d008ad5d74b9
Ксения <xskuznetsova@gmail.com>
2018-01-19 16:14:32 +0300

    Потребление разделяемой сборки.

---
+++ SharedCarLibClient/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CarLibrary;
+
+namespace SharedCarLibClient
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Клиент для разделяемой сборки *****");
+            SportsCar c = new SportsCar();
+            c.TurboBoost();
+            Console.ReadLine();
+        }
+    }
+}
