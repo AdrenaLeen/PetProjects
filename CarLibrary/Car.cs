6657be301a8ad32e81eb5e695882a9189faf2e36
Ксения <xskuznetsova@gmail.com>
2018-01-19 18:57:59 +0300

    Построение разделяемой сборки версии 2.0.0.0.

--- CarLibrary/Car.cs
+++ CarLibrary/Car.cs
@@ -3,12 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace CarLibrary
 {
     // Представляет состояние двигателя.
     public enum EngineState
     { engineAlive, engineDead }
+
+    // Тип музыкального устройства, установленного в автомобиле.
+    public enum MusicMedia
+    {
+        musicCd,
+        musicTape,
+        musicRadio,
+        musicMp3
+    }
 
     // Абстрактный базовый класс в иерархии.
     public abstract class Car
@@ -22,12 +32,22 @@ namespace CarLibrary
             get { return egnState; }
         }
         public abstract void TurboBoost();
-        public Car() { }
+        public Car()
+        {
+            MessageBox.Show("CarLibrary версия 2.0!");
+        }
         public Car(string name, int maxSp, int currSp)
         {
+            MessageBox.Show("CarLibrary версия 2.0!");
             PetName = name;
             MaxSpeed = maxSp;
             CurrentSpeed = currSp;
+        }
+
+        public void TurnOnRadio(bool musicOn, MusicMedia mm)
+        {
+            if (musicOn) MessageBox.Show(string.Format("Слушаем {0}", mm));
+            else MessageBox.Show("Время тишины...");
         }
     }
 }
