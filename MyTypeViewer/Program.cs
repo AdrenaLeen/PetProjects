387ffe8a425b9b8c735c34626bbf3d6b9fc66d36
Ксения <xskuznetsova@gmail.com>
2018-01-20 18:49:42 +0300

    Рефлексия методов.

---
+++ MyTypeViewer/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reflection;
+
+namespace MyTypeViewer
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+
+        // Отобразить имена методов в типе.
+        static void ListMethods(Type t)
+        {
+            Console.WriteLine("***** Методы *****");
+            IEnumerable<string> methodNames = from n in t.GetMethods() select n.Name;
+            foreach (string name in methodNames) Console.WriteLine($"->{name}");
+            Console.WriteLine();
+        }
+    }
+}
