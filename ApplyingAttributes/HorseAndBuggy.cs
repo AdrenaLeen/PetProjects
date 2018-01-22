130001fe981d6b77962eea4c9f0537c78db18869
Ксения <xskuznetsova@gmail.com>
2018-01-22 17:58:02 +0300

    Применение атрибутов.

---
+++ ApplyingAttributes/HorseAndBuggy.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    [Serializable]
+    [Obsolete("Используйте другое транспортное средство!")]
+    class HorseAndBuggy
+    {
+    }
+}
---
+++ ApplyingAttributes/Motorcycle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ApplyingAttributes
+{
+    // Этот класс может быть сохранён на диске.
+    [Serializable]
+    class Motorcycle
+    {
+        // Однако это поле сохраняться не будет.
+        [NonSerialized]
+        float weightOfCurrentPassengers;
+
+        // Эти поля остаются сериализируемыми.
+        bool hasRadioSystem;
+        bool hasHeadSet;
+        bool hasSissyBar;
+    }
+}
