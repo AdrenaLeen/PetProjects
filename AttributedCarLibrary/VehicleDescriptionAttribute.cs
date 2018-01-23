c111a6cdb2ad934e543d19e2bfd883c258d06c60
Ксения <xskuznetsova@gmail.com>
2018-01-23 21:43:42 +0300

    Построение специальных атрибутов.

---
+++ AttributedCarLibrary/VehicleDescriptionAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributedCarLibrary
+{
+    // Специальный атрибут.
+    public class VehicleDescriptionAttribute : Attribute
+    {
+        public string Description { get; set; }
+
+        public VehicleDescriptionAttribute(string vehicalDescription)
+        {
+            Description = vehicalDescription;
+        }
+
+        public VehicleDescriptionAttribute() { }
+    }
+}
