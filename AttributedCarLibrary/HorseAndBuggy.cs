e97e003a03f05f421c06c38ef007191e24770e44
Ксения <xskuznetsova@gmail.com>
2018-01-23 21:58:56 +0300

    Применение специальных атрибутов.

---
+++ AttributedCarLibrary/HorseAndBuggy.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributedCarLibrary
+{
+    [Serializable]
+    [Obsolete("Используйте другое транспортное средство!")]
+    [VehicleDescription("Старая серая кобыла, она не та, чем была раньше...")]
+    public class HorseAndBuggy
+    {
+    }
+}
---
+++ AttributedCarLibrary/Motorcycle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AttributedCarLibrary
+{
+    // Назначить описание с помощью "именованного свойства".
+    [Serializable]
+    [VehicleDescription(Description = "Мой звёздный Харлей")]
+    public class Motorcycle
+    {
+    }
+}
