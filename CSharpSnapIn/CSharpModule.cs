99e329cd6f31f1e1d49977762e28460d5600a8ca
Ксения <xskuznetsova@gmail.com>
2018-01-30 01:49:42 +0300

    Построение оснастки на C#.

---
+++ CSharpSnapIn/CSharpModule.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CommonSnappableTypes;
+using System.Windows.Forms;
+
+namespace CSharpSnapIn
+{
+    [CompanyInfo(CompanyName = "FooBar", CompanyUrl = "www.FooBar.com")]
+    public class CSharpModule : IAppFunctionality
+    {
+        void IAppFunctionality.DoIt()
+        {
+            MessageBox.Show("Вы только что использовали оснастку C#!");
+        }
+    }
+}
