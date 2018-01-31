c082e5d7ec7f69180e717c073b893a528373dd65
Ксения <xskuznetsova@gmail.com>
2018-01-31 15:22:18 +0300

    Создан класс с применением ключевого слова dynamic.

---
+++ DynamicKeyword/VeryDynamicClass.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DynamicKeyword
+{
+    class VeryDynamicClass
+    {
+        // Динамическое поле.
+        private static dynamic myDynamicField;
+
+        // Динамическое свойство.
+        public dynamic DynamicProperty { get; set; }
+
+        // Динамический тип возврата и динамический тип параметра.
+        public dynamic DynamicMethod(dynamic dynamicParam)
+        {
+            // Динамическая локальная переменная.
+            dynamic dynamicLocalVar = "Локальная переменная";
+
+            int myInt = 10;
+            if (dynamicParam is int) return dynamicLocalVar;
+            else return myInt;
+        }
+    }
+}
