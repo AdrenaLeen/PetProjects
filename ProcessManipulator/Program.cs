816351af0302a44806fd3e0a4ff8c7963470f526
Ксения <xskuznetsova@gmail.com>
2018-02-04 17:24:53 +0300

    Перечисление выполняющихся процессов.

---
+++ ProcessManipulator/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace ProcessManipulator
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Процессы *****");
+            ListAllRunningProcesses();
+            Console.ReadLine();
+        }
+
+        static void ListAllRunningProcesses()
+        {
+            // Получить все процессы на локальной машине, упорядоченные по PID.
+            IOrderedEnumerable<Process> runningProcs = from proc in Process.GetProcesses(".") orderby proc.Id select proc;
+
+            // Вывести для каждого процесса идентификатор PID и имя.
+            foreach (Process p in runningProcs)
+            {
+                string info = string.Format($"-> PID: {p.Id}\tИмя: {p.ProcessName}");
+                Console.WriteLine(info);
+            }
+            Console.WriteLine("************************************");
+        }
+    }
+}
