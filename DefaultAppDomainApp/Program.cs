398178d240a1aa4c1c36093d5daa3cd0e67ca09a
Ксения <xskuznetsova@gmail.com>
2018-02-05 06:04:07 +0300

    Взаимодействие со стандартным доменом приложения.

---
+++ DefaultAppDomainApp/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefaultAppDomainApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Стандартный домен приложения *****");
+            DisplayDADStats();
+            Console.ReadLine();
+        }
+
+        private static void DisplayDADStats()
+        {
+            // Получить доступ к домену приложения для текущего потока.
+            AppDomain defaultAD = AppDomain.CurrentDomain;
+
+            // Вывести разнообразные статистические данные об этом домене.
+            Console.WriteLine($"Дружественное имя этого домена: {defaultAD.FriendlyName}");
+            Console.WriteLine($"Идентификатор домена в этом процессе: {defaultAD.Id}");
+            Console.WriteLine($"Является ли стандартным доменом?: {defaultAD.IsDefaultAppDomain()}");
+            Console.WriteLine($"Базовый каталог этого домена: {defaultAD.BaseDirectory}");
+        }
+    }
+}
