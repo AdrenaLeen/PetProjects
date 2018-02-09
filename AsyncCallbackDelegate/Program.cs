8146a1f7c5e6cdc2cbaf8d3f3a629f7b2167074c
Ксения <xskuznetsova@gmail.com>
2018-02-09 20:57:58 +0300

    Использование делегата AsyncCallback.

---
+++ AsyncCallbackDelegate/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace AsyncCallbackDelegate
+{
+    public delegate int BinaryOp(int x, int y);
+
+    class Program
+    {
+        private static bool isDone = false;
+
+        static void Main()
+        {
+            Console.WriteLine("***** Пример делегата AsyncCallback *****");
+            Console.WriteLine($"Main() вызывается в потоке {Thread.CurrentThread.ManagedThreadId}.");
+            BinaryOp b = new BinaryOp(Add);
+            IAsyncResult iftAR = b.BeginInvoke(10, 10, new AsyncCallback(AddComplete), null);
+
+            // Предположим, что здесь делается какая-то другая работа...
+            while (!isDone)
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Работаем...");
+            }
+
+            Console.ReadLine();
+        }
+
+        static int Add(int x, int y)
+        {
+            Console.WriteLine($"Add() вызывается в потоке {Thread.CurrentThread.ManagedThreadId}.");
+            Thread.Sleep(5000);
+            return x + y;
+        }
+
+        static void AddComplete(IAsyncResult itfAR)
+        {
+            Console.WriteLine($"AddComplete() вызывается в потоке {Thread.CurrentThread.ManagedThreadId}.");
+            Console.WriteLine("Ваше суммирование завершено.");
+            isDone = true;
+        }
+    }
+}
