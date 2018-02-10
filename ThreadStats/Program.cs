d7ef28d39e183dae2711a2d16ecaef50cb1b7e3a
Ксения <xskuznetsova@gmail.com>
2018-02-11 00:06:58 +0300

    Получение статистики о текущем потоке выполнения.

---
+++ ThreadStats/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadStats
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Статистика о текущем потоке *****");
+
+            // Получить имя текущего потока.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "ThePrimaryThread";
+
+            // Показать детали обслуживающего домена приложения и контекста.
+            Console.WriteLine($"Имя текущего домена приложения: {Thread.GetDomain().FriendlyName}");
+            Console.WriteLine($"Идентификатор текущего контекста: {Thread.CurrentContext.ContextID}");
+
+            // Вывести некоторую статистику о текущем потоке.
+            Console.WriteLine($"Имя потока: {primaryThread.Name}");
+            Console.WriteLine($"Запущен ли поток?: {primaryThread.IsAlive}");
+            Console.WriteLine($"Приоритет потока: {primaryThread.Priority}");
+            Console.WriteLine($"Состояние потока: {primaryThread.ThreadState}");
+            Console.ReadLine();
+        }
+    }
+}
