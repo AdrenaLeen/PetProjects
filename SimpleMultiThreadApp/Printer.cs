26a5a390405cc760585fc7f5edec9d3a046bcc05
Ксения <xskuznetsova@gmail.com>
2018-02-11 21:35:52 +0300

    Работа с делегатом ThreadStart.

---
+++ SimpleMultiThreadApp/Printer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace SimpleMultiThreadApp
+{
+    class Printer
+    {
+        public void PrintNumbers()
+        {
+            // Вывести информацию о потоке.
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} выполняет PrintNumbers()");
+
+            // Вывести числа.
+            Console.Write("Ваши числа: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write($"{i}, ");
+                Thread.Sleep(2000);
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ SimpleMultiThreadApp/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace SimpleMultiThreadApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Потоки *****");
+            Console.Write("Вы хотите [1] или [2] потока? ");
+            string threadCount = Console.ReadLine();
+
+            // Назначить имя текущему потоку.
+            Thread primaryThread = Thread.CurrentThread;
+            primaryThread.Name = "Primary";
+
+            // Вывести информацию о потоке.
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} выполняет Main()");
+
+            // Создать рабочий класс.
+            Printer p = new Printer();
+
+            switch (threadCount)
+            {
+                case "2":
+                    // Создать поток.
+                    Thread backgroundThread = new Thread(new ThreadStart(p.PrintNumbers));
+                    backgroundThread.Name = "Secondary";
+                    backgroundThread.Start();
+                    break;
+                case "1":
+                    p.PrintNumbers();
+                    break;
+                default:
+                    Console.WriteLine("Я не знаю, чего вы хотите... Вы получаете 1 поток.");
+                    // Переход к варианту с одним потоком.
+                    goto case "1";
+            }
+
+            // Выполнить некоторую дополнительную работу.
+            MessageBox.Show("Я занят!", "Работа в основном потоке...");
+            Console.ReadLine();
+        }
+    }
+}
