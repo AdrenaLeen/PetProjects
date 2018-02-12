bfa874c359b45f77633830e73ec250f612849687
Ксения <xskuznetsova@gmail.com>
2018-02-12 20:53:01 +0300

    Проиллюстрированы проблемы параллелизма.

---
+++ MultiThreadedPrinting/Printer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    class Printer
+    {
+        public void PrintNumbers()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(100 * r.Next(5));
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ MultiThreadedPrinting/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace MultiThreadedPrinting
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Синхронизация потоков *****");
+
+            Printer p = new Printer();
+
+            // Создать 10 потоков, которые указывают на один и тот же метод того же самого объекта.
+            Thread[] threads = new Thread[10];
+            for (int i = 0; i < 10; i++)
+            {
+                threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
+                threads[i].Name = string.Format($"Рабочий поток #{i}");
+            }
+
+            // Теперь запустить их все
+            foreach (Thread t in threads) t.Start();
+            Console.ReadLine();
+        }
+    }
+}
