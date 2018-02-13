e91570498e9059bf29d1704ce36fc9a71344c1a1
Ксения <xskuznetsova@gmail.com>
2018-02-13 20:44:41 +0300

    Программирование с использованием обратных вызовов Timer.

---
+++ TimerApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace TimerApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Работа с типом Timer *****");
+
+            // Создать делегат для типа Timer.
+            TimerCallback timeCB = new TimerCallback(PrintTime);
+
+            // Установить параметры таймера.
+            Timer t = new Timer(
+              timeCB, // Объект делегата TimerCallback.
+              "Привет из Main", // Информация для передачи в вызванный метод (null, если информация отсутствует).
+              0, // Период ожидания перед запуском (в миллисекундах).
+              1000); // Интервал между вызовами (в миллисекундах).
+
+            Console.WriteLine("Нажмите клавишу для завершения...");
+            Console.ReadLine();
+        }
+
+        static void PrintTime(object state) => Console.WriteLine($"Время: {DateTime.Now.ToLongTimeString()}, параметр: {state.ToString()}");
+    }
+}
