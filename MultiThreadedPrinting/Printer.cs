bc0feb60b1b9d4380315d9b6ec5b6bc21026cb39
Ксения <xskuznetsova@gmail.com>
2018-02-13 20:35:06 +0300

    Синхронизация с использованием атрибута [Synchronization].

--- MultiThreadedPrinting/Printer.cs
+++ MultiThreadedPrinting/Printer.cs
@@ -4,32 +4,28 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using System.Runtime.Remoting.Contexts;
 
 namespace MultiThreadedPrinting
 {
-    class Printer
+    // Все методы класса Printer теперь безопасны к потокам!
+    [Synchronization]
+    class Printer : ContextBoundObject
     {
-        // Маркер блокировки.
-        private object threadLock = new object();
-
         public void PrintNumbers()
         {
-            // Использовать маркер блокировки.
-            lock (threadLock)
-            {
-                // Вывести информацию о потоке.
-                Console.WriteLine($"-> {Thread.CurrentThread.Name} выполняет PrintNumbers()");
+            // Вывести информацию о потоке.
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} выполняет PrintNumbers()");
 
-                // Вывести числа.
-                Console.Write("Ваши числа: ");
-                for (int i = 0; i < 10; i++)
-                {
-                    Random r = new Random();
-                    Thread.Sleep(100 * r.Next(5));
-                    Console.Write($"{i}, ");
-                }
-                Console.WriteLine();
+            // Вывести числа.
+            Console.Write("Ваши числа: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(100 * r.Next(5));
+                Console.Write($"{i}, ");
             }
+            Console.WriteLine();
         }
     }
 }
