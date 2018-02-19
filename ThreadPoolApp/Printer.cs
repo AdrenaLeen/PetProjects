f99eb22680476ec98a3f7952c47874cb2441aa0b
Ксения <xskuznetsova@gmail.com>
2018-02-19 22:08:23 +0300

    Работа с пулом потоков CLR.

---
+++ ThreadPoolApp/Printer.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Contexts;
+
+namespace ThreadPoolApp
+{
+    [Synchronization]
+    class Printer : ContextBoundObject
+    {
+        public void PrintNumbers()
+        {
+            // Вывести информацию о потоке.
+            Console.WriteLine($"-> {Thread.CurrentThread.Name} выполняет PrintNumbers()");
+
+            // Вывести числа.
+            Console.Write("Ваши числа: ");
+            for (int i = 0; i < 10; i++)
+            {
+                Random r = new Random();
+                Thread.Sleep(100 * r.Next(5));
+                Console.Write($"{i}, ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
---
+++ ThreadPoolApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace ThreadPoolApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Пул потоков CLR *****");
+
+            Console.WriteLine($"Запущен главный поток. ThreadID = {Thread.CurrentThread.ManagedThreadId}");
+            Printer p = new Printer();
+            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
+
+            // Поставить в очередь метод 10 раз.
+            for (int i = 0; i < 10; i++) ThreadPool.QueueUserWorkItem(workItem, p);
+
+            Console.WriteLine("Все задачи поставлены в очередь.");
+            Console.ReadLine();
+        }
+
+        static void PrintTheNumbers(object state)
+        {
+            Printer task = (Printer)state;
+            task.PrintNumbers();
+        }
+    }
+}
