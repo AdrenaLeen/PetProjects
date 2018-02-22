c331ef2c03ec6d07c3ad8a53ab03c06774f3569e
Ксения <xskuznetsova@gmail.com>
2018-02-22 19:14:58 +0300

    Обработка запроса на отмену.

--- DataParallelismWithForEach/MainWindow.xaml.cs
+++ DataParallelismWithForEach/MainWindow.xaml.cs
@@ -23,6 +23,9 @@ namespace DataParallelismWithForEach
     /// </summary>
     public partial class MainWindow : Window
     {
+        // Новая переменная уровня MainWindow.
+        private CancellationTokenSource cancelToken = new CancellationTokenSource();
+
         public MainWindow()
         {
             InitializeComponent();
@@ -39,27 +42,53 @@ namespace DataParallelismWithForEach
 
         private void ProcessFiles()
         {
+            // Использовать экземпляр ParallelOptions для хранения CancellationToken.
+            ParallelOptions parOpts = new ParallelOptions();
+            parOpts.CancellationToken = cancelToken.Token;
+            parOpts.MaxDegreeOfParallelism = Environment.ProcessorCount;
+
             // Загрузить все файлы *.jpg и создать новый каталог для модифицированных данных.
             string[] files = Directory.GetFiles(@"D:\OneDrive\Pictures\Фотоальбом", "*.jpg", SearchOption.AllDirectories);
             string newDir = @"D:\OneDrive\Pictures\ModifiedPictures";
             Directory.CreateDirectory(newDir);
 
-            // Обработать данные изображений в параллельном режиме!
-            Parallel.ForEach(files, currentFile =>
+            try
             {
-                string filename = System.IO.Path.GetFileName(currentFile);
-                using (Bitmap bitmap = new Bitmap(currentFile))
+                // Обработать данные изображений в параллельном режиме!
+                Parallel.ForEach(files, parOpts, currentFile =>
                 {
-                    bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                    bitmap.Save(System.IO.Path.Combine(newDir, filename));
-
-                    // Вызвать Invoke на объекте MainWindow, чтобы позволить вторичным потокам получать доступ к элементам управления в безопасной к потокам манере.
-                    this.Dispatcher.Invoke((Action)delegate
+                    parOpts.CancellationToken.ThrowIfCancellationRequested();
+                    string filename = System.IO.Path.GetFileName(currentFile);
+                    using (Bitmap bitmap = new Bitmap(currentFile))
                     {
-                        this.Title = $"Обработка {filename} в потоке {Thread.CurrentThread.ManagedThreadId}";
-                    });
-                }
-            });
+                        bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                        bitmap.Save(System.IO.Path.Combine(newDir, filename));
+
+                        // Вызвать Invoke на объекте MainWindow, чтобы позволить вторичным потокам получать доступ к элементам управления в безопасной к потокам манере.
+                        this.Dispatcher.Invoke((Action)delegate
+                        {
+                            this.Title = $"Обработка {filename} в потоке {Thread.CurrentThread.ManagedThreadId}";
+                        });
+                    }
+                });
+                this.Dispatcher.Invoke((Action)delegate
+                {
+                    this.Title = "Готово!";
+                });
+            }
+            catch (OperationCanceledException ex)
+            {
+                this.Dispatcher.Invoke((Action)delegate
+                {
+                    this.Title = ex.Message;
+                });
+            }
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            // Используется для сообщения всем рабочим потокам о необходимости останова!
+            cancelToken.Cancel();
         }
     }
 }
