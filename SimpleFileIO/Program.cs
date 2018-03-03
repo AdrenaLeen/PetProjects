0b1b9e48fcc9546a38262f99f07fa1ec2b91a24d
Ксения <xskuznetsova@gmail.com>
2018-03-03 20:15:31 +0300

    Работа с дополнительными членами File.

---
+++ SimpleFileIO/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SimpleFileIO
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Просто ввод-вывод с типом File *****");
+            try
+            {
+                string[] myTasks = { "Починить раковину в ванной", "Позвонить Дэйву", "Позвонить маме и папе", "Поиграть в Xbox 360"};
+
+                // Записать все данные в файл на диске D:.
+                File.WriteAllLines(@"D:\tasks.txt", myTasks);
+
+                // Прочитать все данные и вывести на консоль.
+                foreach (string task in File.ReadAllLines(@"D:\tasks.txt")) Console.WriteLine("TODO: {0}", task);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            Console.ReadLine();
+        }
+    }
+}
