96ff5383238bcd39ebe18ed69ddeaa3c9102168d
Ксения <xskuznetsova@gmail.com>
2018-03-04 22:40:55 +0300

    Запись в текстовый файл.

---
+++ StreamWriterReaderApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace StreamWriterReaderApp
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** StreamWriter / StreamReader *****");
+
+            // Получить объект StreamWriter и записать строковые данные.
+            using (StreamWriter writer = File.CreateText("reminders.txt"))
+            {
+                writer.WriteLine("Не забыть день матери в этом году...");
+                writer.WriteLine("Не забыть день отца в этом году...");
+                writer.WriteLine("Не забыть эти числа:");
+                for (int i = 0; i < 10; i++) writer.Write($"{i} ");
+
+                // Вставить новую строку.
+                writer.Write(writer.NewLine);
+            }
+            Console.WriteLine("Создать файл и записать некоторые мысли...");
+
+            Console.ReadLine();
+        }
+    }
+}
