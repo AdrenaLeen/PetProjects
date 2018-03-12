4d5c3330d3d02737e4f130a0e7a5cfd6e92c102f
Ксения <xskuznetsova@gmail.com>
2018-03-12 18:06:52 +0300

    Работа с классами BinaryWriter и BinaryReader.

---
+++ BinaryWriterReader/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace BinaryWriterReader
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Binary Writer / Reader *****");
+
+            // Открыть средство двоичной записи в файл.
+            FileInfo f = new FileInfo("BinFile.dat");
+            using (BinaryWriter bw = new BinaryWriter(f.OpenWrite()))
+            {
+                // Вывести на консоль тип BaseStream (System.IO.FileStream в данном случае).
+                Console.WriteLine($"Основной поток: {bw.BaseStream}");
+
+                // Создать некоторые данные для сохранения в файле.
+                double aDouble = 1234.67;
+                int anInt = 34567;
+                string aString = "A, B, C";
+
+                // Записать данные.
+                bw.Write(aDouble);
+                bw.Write(anInt);
+                bw.Write(aString);
+            }
+            Console.WriteLine("Готово!");
+
+            // Читать двоичные данные из потока.
+            using (BinaryReader br = new BinaryReader(f.OpenRead()))
+            {
+                Console.WriteLine(br.ReadDouble());
+                Console.WriteLine(br.ReadInt32());
+                Console.WriteLine(br.ReadString());
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
