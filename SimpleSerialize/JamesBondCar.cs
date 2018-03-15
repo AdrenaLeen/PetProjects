4221bbcccf86e88c9c94e28dae9112c973652def
Ксения <xskuznetsova@gmail.com>
2018-03-15 14:44:03 +0300

    Управление генерацией данных XML.

--- SimpleSerialize/JamesBondCar.cs
+++ SimpleSerialize/JamesBondCar.cs
@@ -3,13 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Serialization;
 
 namespace SimpleSerialize
 {
     [Serializable]
+    [XmlRoot(Namespace = "http://xskuznetsova.ru")]
     public class JamesBondCar : Car
     {
+        [XmlAttribute]
         public bool canFly;
+
+        [XmlAttribute]
         public bool canSubmerge;
     }
 }
