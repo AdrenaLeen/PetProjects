2d0deecffb14bc489cb87969c48c92ecfce42065
Ксения <xskuznetsova@gmail.com>
2018-03-15 20:34:35 +0300

    Настройка сериализации с использованием атрибутов.

---
+++ CustomSerialization/MoreData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CustomSerialization
+{
+    [Serializable]
+    class MoreData
+    {
+        private string dataItemOne = "Первый блок данных";
+        private string dataItemTwo = "Больше данных";
+
+        [OnSerializing]
+        private void OnSerializing(StreamingContext context)
+        {
+            // Вызывается в течение процесса сериализации.
+            dataItemOne = dataItemOne.ToUpper();
+            dataItemTwo = dataItemTwo.ToUpper();
+        }
+
+        [OnDeserialized]
+        private void OnDeserialized(StreamingContext context)
+        {
+            // Вызывается, когда процесс десериализации завершён.
+            dataItemOne = dataItemOne.ToLower();
+            dataItemTwo = dataItemTwo.ToLower();
+        }
+    }
+}
