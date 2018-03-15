d1c7bcde34239a1e826f1922226ac6b5d3856490
Ксения <xskuznetsova@gmail.com>
2018-03-15 20:31:49 +0300

    Настройка сериализации с использованием ISerializable.

---
+++ CustomSerialization/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.IO;
+
+namespace CustomSerialization
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Настраиваемая сериализация *****");
+
+            // Этот тип реализует интерфейс ISerializable.
+            StringData myData = new StringData();
+
+            // Сохранить экземляр в локальный файл в формате SOAP.
+            SoapFormatter soapFormat = new SoapFormatter();
+            using (Stream fStream = new FileStream("MyData.soap", FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                soapFormat.Serialize(fStream, myData);
+            }
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CustomSerialization/StringData.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.Serialization;
+
+namespace CustomSerialization
+{
+    [Serializable]
+    class StringData : ISerializable
+    {
+        private string dataItemOne = "Первый блок данных";
+        private string dataItemTwo = "Больше данных";
+
+        public StringData() { }
+        protected StringData(SerializationInfo si, StreamingContext ctx)
+        {
+            // Восстановить переменные-члены из потока.
+            dataItemOne = si.GetString("First_Item").ToLower();
+            dataItemTwo = si.GetString("dataItemTwo").ToLower();
+        }
+
+        void ISerializable.GetObjectData(SerializationInfo info, StreamingContext ctx)
+        {
+            // Наполнить объект SerializationInfo форматированными данными.
+            info.AddValue("First_Item", dataItemOne.ToUpper());
+            info.AddValue("dataItemTwo", dataItemTwo.ToUpper());
+        }
+    }
+}
