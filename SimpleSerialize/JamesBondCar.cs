5f9e4d8ce55d28c530147ca516c41a8109976fdd
Ксения <xskuznetsova@gmail.com>
2018-03-15 14:22:58 +0300

    Определены сериализуемые типы.

---
+++ SimpleSerialize/JamesBondCar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSerialize
+{
+    [Serializable]
+    class JamesBondCar : Car
+    {
+        public bool canFly;
+        public bool canSubmerge;
+    }
+}
---
+++ SimpleSerialize/Radio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleSerialize
+{
+    [Serializable]
+    class Radio
+    {
+        public bool hasTweeters;
+        public bool hasSubWoofers;
+        public double[] stationPresets;
+
+        [NonSerialized]
+        public string radioID = "XF-552RR6";
+    }
+}
