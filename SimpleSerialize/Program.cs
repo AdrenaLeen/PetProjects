516685b2eaced0313652861d89dd278f2d9f4596
Ксения <xskuznetsova@gmail.com>
2018-03-15 14:29:09 +0300

    Сериализация объектов с использованием BinaryFormatter.

--- SimpleSerialize/Program.cs
+++ SimpleSerialize/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
 
 namespace SimpleSerialize
 {
@@ -10,6 +12,30 @@ namespace SimpleSerialize
     {
         static void Main()
         {
+            Console.WriteLine("***** Сериализация объектов *****");
+
+            // Создать объект JamesBondCar и установить состояние.
+            JamesBondCar jbc = new JamesBondCar();
+            jbc.canFly = true;
+            jbc.canSubmerge = false;
+            jbc.theRadio.stationPresets = new double[] { 89.3, 105.1, 97.1 };
+            jbc.theRadio.hasTweeters = true;
+
+            // Сохранить объект JamesBondCar в указанном файле в двоичном формате.
+            SaveAsBinaryFormat(jbc, "CarData.dat");
+
+            Console.ReadLine();
+        }
+
+        static void SaveAsBinaryFormat(object objGraph, string fileName)
+        {
+            // Сохранить граф объектов в файл CarData.dat в двоичном виде.
+            BinaryFormatter binFormat = new BinaryFormatter();
+            using (Stream fStream = new FileStream(fileName, FileMode.Create, FileAccess.Write, FileShare.None))
+            {
+                binFormat.Serialize(fStream, objGraph);
+            }
+            Console.WriteLine("=> Машина сохранена в двоичном формате!");
         }
     }
 }
