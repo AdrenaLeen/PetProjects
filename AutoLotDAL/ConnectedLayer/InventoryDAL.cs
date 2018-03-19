5f02cdd8e8f8bde5e80844d27219cd13ef071586
Ксения <xskuznetsova@gmail.com>
2018-03-19 13:14:03 +0300

    Добавлена логика подключения.

--- AutoLotDAL/ConnectedLayer/InventoryDAL.cs
+++ AutoLotDAL/ConnectedLayer/InventoryDAL.cs
@@ -3,10 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace AutoLotDAL.ConnectedLayer
 {
     public class InventoryDAL
     {
+        // Этот член будет использоваться всеми методами.
+        private SqlConnection sqlConnection = null;
+
+        public void OpenConnection(string connectionString)
+        {
+            sqlConnection = new SqlConnection { ConnectionString = connectionString };
+            sqlConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            sqlConnection.Close();
+        }
     }
 }
