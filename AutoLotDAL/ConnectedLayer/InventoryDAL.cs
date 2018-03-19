cd3d717bd50683114aba4e4b6f457f7be54dc4d7
Ксения <xskuznetsova@gmail.com>
2018-03-19 13:25:50 +0300

    Добавлена логика вставки.

--- AutoLotDAL/ConnectedLayer/InventoryDAL.cs
+++ AutoLotDAL/ConnectedLayer/InventoryDAL.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using AutoLotDAL.Models;
 
 namespace AutoLotDAL.ConnectedLayer
 {
@@ -21,6 +22,30 @@ namespace AutoLotDAL.ConnectedLayer
         public void CloseConnection()
         {
             sqlConnection.Close();
+        }
+
+        public void InsertAuto(string color, string make, string petName)
+        {
+            // Сформировать и выполнить оператор SQL.
+            string sql = $"Insert Into Inventory (Make, Color, PetName) Values ('{make}', '{color}', '{petName}')";
+
+            // Выполнить, используя наше подключение.
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
+        }
+
+        public void InsertAuto(NewCar car)
+        {
+            // Сформатировать и выполнить оператор SQL.
+            string sql = $"Insert Into Inventory (Make, Color, PetName) Values ('{car.Make}', '{car.Color}', '{car.PetName}')";
+
+            // Выполнить, используя наше подключение.
+            using (SqlCommand command = new SqlCommand(sql, sqlConnection))
+            {
+                command.ExecuteNonQuery();
+            }
         }
     }
 }
