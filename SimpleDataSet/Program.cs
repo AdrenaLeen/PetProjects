a487b3d4e747e76f9a23039e8441c0111b69471d
Ксения <xskuznetsova@gmail.com>
2018-03-23 20:18:16 +0300

    Построен объект DataSet.

---
+++ SimpleDataSet/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+
+namespace SimpleDataSet
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** DataSet *****");
+
+            // Создать объект DataSet и добавить несколько свойств.
+            DataSet carsInventoryDS = new DataSet("Car Inventory");
+
+            carsInventoryDS.ExtendedProperties["TimeStamp"] = DateTime.Now;
+            carsInventoryDS.ExtendedProperties["DataSetID"] = Guid.NewGuid();
+            carsInventoryDS.ExtendedProperties["Company"] = "Mikko’s Hot Tub Super Store";
+
+            FillDataSet(carsInventoryDS);
+            PrintDataSet(carsInventoryDS);
+            
+            Console.ReadLine();
+        }
+
+        private static void PrintDataSet(DataSet carsInventoryDS)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static void FillDataSet(DataSet carsInventoryDS)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
