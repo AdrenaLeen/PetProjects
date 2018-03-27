36372a22d6e74c9285cc9607cceca43d9035b4df
Ксения <xskuznetsova@gmail.com>
2018-03-27 03:06:30 +0300

    Определён начальный класс.

---
+++ AutoLotDAL/DisconnectedLayer/InventoryDALDC.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AutoLotDAL.DisconnectedLayer
+{
+    public class InventoryDALDC
+    {
+        // Поля данных.
+        private string connectionString;
+        private SqlDataAdapter adapter = null;
+
+        public InventoryDALDC(string connStr)
+        {
+            connectionString = connStr;
+
+            // Конфигурировать объект SqlDataAdapter.
+            ConfigureAdapter(out adapter);
+        }
+
+        private void ConfigureAdapter(out SqlDataAdapter adapter)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
