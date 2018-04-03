4cc42ee44969efb51b8170555cfe466e97adf3a6
Ксения <xskuznetsova@gmail.com>
2018-04-04 00:36:07 +0300

    Добавлены сгенерированные классы модели.

---
+++ AutoLotConsoleApp/EF/CarPartial.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLotConsoleApp.EF
+{
+    public partial class Car
+    {
+        public override string ToString()
+        {
+            // Поскольку столбец PetName может быть пустым, предоставить стандартное имя **Безымянный**.
+            return $"{CarNickName ?? "**Безымянный**"} - это {Color} {Make} с ID {CarId}.";
+        }
+    }
+}
