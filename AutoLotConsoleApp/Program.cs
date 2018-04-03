9a58c0f61f76c18cca06a1374a7a2740eaf7cfbf
Ксения <xskuznetsova@gmail.com>
2018-04-04 00:44:06 +0300

    Реализована вставка записи.

--- AutoLotConsoleApp/Program.cs
+++ AutoLotConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoLotConsoleApp.EF;
 
 namespace AutoLotConsoleApp
 {
@@ -10,6 +11,33 @@ namespace AutoLotConsoleApp
     {
         static void Main()
         {
+            Console.WriteLine("***** ADO.NET EF *****");
+            int carId = AddNewRecord();
+            Console.WriteLine(carId);
+            Console.ReadLine();
+        }
+
+        private static int AddNewRecord()
+        {
+            // Добавить запись в таблицу Inventory базы данных AutoLot.
+            using (AutoLotEntities context = new AutoLotEntities())
+            {
+                try
+                {
+                    // В целях тестирования жёстко закодировать данные для новой записи.
+                    Car car = new Car() { Make = "Yugo", Color = "Коричневый", CarNickName = "Брауни" };
+                    context.Cars.Add(car);
+                    context.SaveChanges();
+                    
+                    // В случае успешного сохранения EF заполняет поле идентификатора значением, сгенерированным базой данных.
+                    return car.CarId;
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                    return 0;
+                }
+            }
         }
     }
 }
