f6a4252463408bd8807737ecf3ecb94e65d49a55
Ксения <xskuznetsova@gmail.com>
2018-04-05 17:38:24 +0300

    Добавлен класс, производный от DbContext.

---
+++ AutoLotDAL/EF/AutoLotEntities.cs
@@ -0,0 +1,31 @@
+﻿namespace AutoLotDAL.EF
+{
+    using System;
+    using System.Data.Entity;
+    using System.Linq;
+
+    public class AutoLotEntities : DbContext
+    {
+        // Контекст настроен для использования строки подключения "AutoLotEntities" из файла конфигурации  
+        // приложения (App.config или Web.config). По умолчанию эта строка подключения указывает на базу данных 
+        // "AutoLotDAL.EF.AutoLotEntities" в экземпляре LocalDb. 
+        // 
+        // Если требуется выбрать другую базу данных или поставщик базы данных, измените строку подключения "AutoLotEntities" 
+        // в файле конфигурации приложения.
+        public AutoLotEntities()
+            : base("name=AutoLotEntities")
+        {
+        }
+
+        // Добавьте DbSet для каждого типа сущности, который требуется включить в модель. Дополнительные сведения 
+        // о настройке и использовании модели Code First см. в статье http://go.microsoft.com/fwlink/?LinkId=390109.
+
+        // public virtual DbSet<MyEntity> MyEntities { get; set; }
+    }
+
+    //public class MyEntity
+    //{
+    //    public int Id { get; set; }
+    //    public string Name { get; set; }
+    //}
+}
