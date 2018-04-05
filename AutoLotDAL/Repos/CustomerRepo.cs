2b8190a05fba282879a39842d72257924be5bda9
Ксения <xskuznetsova@gmail.com>
2018-04-05 18:49:22 +0300

    Добавлены остальные хранилища.

---
+++ AutoLotDAL/Repos/CustomerRepo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLotDAL.Models;
+
+namespace AutoLotDAL.Repos
+{
+    public class CustomerRepo: BaseRepo<Customer>, IRepo<Customer>
+    {
+        public CustomerRepo()
+        {
+            Table = Context.Customers;
+        }
+
+        public int Delete(int id, byte[] timeStamp)
+        {
+            Context.Entry(new Customer() { CustId = id }).State = EntityState.Deleted;
+            return SaveChanges();
+        }
+
+        public Task<int> DeleteAsync(int id, byte[] timeStamp)
+        {
+            Context.Entry(new Customer() { CustId = id }).State = EntityState.Deleted;
+            return SaveChangesAsync();
+        }
+    }
+}
