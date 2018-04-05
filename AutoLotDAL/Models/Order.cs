c0e27d47237e205fc74c91cb581c1dbcd5c1cde7
Ксения <xskuznetsova@gmail.com>
2018-04-05 17:21:32 +0300

    Создан класс модели Order.

--- AutoLotDAL/Models/Order.cs
+++ AutoLotDAL/Models/Order.cs
@@ -3,10 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoLotDAL.Models
 {
-    public class Order
+    public partial class Order
     {
+        [Key, Required, DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int OrderId { get; set; }
+
+        [Required]
+        public int CustId { get; set; }
+
+        [Required]
+        public int CarId { get; set; }
+
+        [ForeignKey("CustId")]
+        public virtual Customer Customer { get; set; }
+
+        [ForeignKey("CarId")]
+        public virtual Inventory Car { get; set; }
     }
 }
