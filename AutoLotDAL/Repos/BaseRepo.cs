70c0f27d17c15792486d17ad271812474c8387a4
Ксения <xskuznetsova@gmail.com>
2018-04-05 18:21:52 +0300

    Реализованы вспомогательные методы SaveChanges().

--- AutoLotDAL/Repos/BaseRepo.cs
+++ AutoLotDAL/Repos/BaseRepo.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using AutoLotDAL.EF;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace AutoLotDAL.Repos
 {
@@ -12,5 +13,60 @@ namespace AutoLotDAL.Repos
     {
         public AutoLotEntities Context { get; } = new AutoLotEntities();
         protected DbSet<T> Table;
+
+        internal int SaveChanges()
+        {
+            try
+            {
+                return Context.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                // Генерируется, когда возникла ошибка параллелизма; пока что просто сгенерировать исключение повторно.
+                throw;
+            }
+            catch (DbUpdateException ex)
+            {
+                // Генерируется, когда обновление базы данных терпит отказ. Проверить внутреннее исключение (исключения), чтобы получить дополнительные сведения и затронутые объекты; пока что просто сгенерировать исключение повторно.
+                throw;
+            }
+            catch (CommitFailedException ex)
+            {
+                // Обработать здесь ошибки, связанные с транзакцией; пока что просто сгенерировать исключение повторно.
+                throw;
+            }
+            catch (Exception ex)
+            {
+                // Были сгенерированы какие-то другие исключения, которые должны быть обработаны.
+                throw;
+            }
+        }
+        internal async Task<int> SaveChangesAsync()
+        {
+            try
+            {
+                return await Context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException ex)
+            {
+                // Генерируется, когда возникла ошибка параллелизма; пока что просто сгенерировать исключение повторно.
+                throw;
+            }
+            catch (DbUpdateException ex)
+            {
+                // Генерируется, когда обновление базы данных терпит отказ. Проверить внутреннее исключение (исключения), чтобы получить дополнительные сведения и затронутые объекты; пока что просто сгенерировать исключение повторно.
+                throw;
+            }
+            catch (CommitFailedException ex)
+            {
+                // Обработать здесь ошибки, связанные с транзакцией; пока что просто сгенерировать исключение повторно.
+                throw;
+            }
+            catch (Exception ex)
+            {
+                // Были сгенерированы какие-то другие исключения, которые должны быть обработаны.
+                throw;
+            }
+        }
     }
 }
