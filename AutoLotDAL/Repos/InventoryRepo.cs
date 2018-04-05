c61e86785fb2513b8ffe4465849e157f51532de4
Ксения <xskuznetsova@gmail.com>
2018-04-05 18:34:58 +0300

    Добавлено хранилище для таблицы Inventory.

---
+++ AutoLotDAL/Repos/InventoryRepo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLotDAL.Models;
+
+namespace AutoLotDAL.Repos
+{
+    public class InventoryRepo : BaseRepo<Inventory>, IRepo<Inventory>
+    {
+        public InventoryRepo()
+        {
+            Table = Context.Inventory;
+        }
+
+        public int Delete(int id, byte[] timeStamp)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<int> DeleteAsync(int id, byte[] timeStamp)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
