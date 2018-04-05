8e92da4a7dc3e8400fdbda309788fa8b2326555f
Ксения <xskuznetsova@gmail.com>
2018-04-05 17:02:29 +0300

    Добавлен класс InventoryPartial.

---
+++ AutoLotDAL/Models/Partials/InventoryPartial.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AutoLotDAL.Models
+{
+    public partial class Inventory
+    {
+        public override string ToString()
+        {
+            // Поскольку столбец PetName может быть пустым, предоставим стандартное имя **Безымянный**.
+            return $"{PetName ?? "**Безымянный**"} - это {Color} {Make} с ID {CarId}.";
+        }
+
+        [NotMapped]
+        public string MakeColor => $"{Make} + ({Color})";
+    }
+}
