c46187c05c9820e87f6b7f18217a0ed76b085587
Ксения <xskuznetsova@gmail.com>
2018-04-05 18:13:32 +0300

    Добавлен класс BaseRepo.

---
+++ AutoLotDAL/Repos/BaseRepo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoLotDAL.EF;
+using System.Data.Entity;
+
+namespace AutoLotDAL.Repos
+{
+    public abstract class BaseRepo<T> where T:class,new()
+    {
+        public AutoLotEntities Context { get; } = new AutoLotEntities();
+        protected DbSet<T> Table;
+    }
+}
