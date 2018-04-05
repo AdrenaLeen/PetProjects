33c56f165f4474551338e6a01959405827f50983
Ксения <xskuznetsova@gmail.com>
2018-04-05 17:16:29 +0300

    Создан класс модели Customer.

--- AutoLotDAL/Models/Customer.cs
+++ AutoLotDAL/Models/Customer.cs
@@ -3,10 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoLotDAL.Models
 {
-    class Customer
+    public partial class Customer
     {
+        [Key]
+        public int CustId { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
+
+        [NotMapped]
+        public string FullName => $"{FirstName} {LastName}";
+
+        public virtual ICollection<Order> Orders { get; set; } = new HashSet<Order>();
     }
 }
