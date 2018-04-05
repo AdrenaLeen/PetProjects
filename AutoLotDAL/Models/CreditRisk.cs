ea6457f16c89739076ec6f452af93f5b073dc5b9
Ксения <xskuznetsova@gmail.com>
2018-04-05 17:32:45 +0300

    Создан класс CreditRisk.

--- AutoLotDAL/Models/CreditRisk.cs
+++ AutoLotDAL/Models/CreditRisk.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AutoLotDAL.Models
 {
-    class CreditRisk
+    public partial class CreditRisk
     {
+        [Key]
+        public int CustId { get; set; }
+
+        [StringLength(50)]
+        public string FirstName { get; set; }
+
+        [StringLength(50)]
+        public string LastName { get; set; }
     }
 }
