b2bcbed21bb51c272f94f7b6f79601d073a68c4f
Ксения <xskuznetsova@gmail.com>
2018-04-09 16:27:26 +0300

    Реализовано добавление записей в Inventory.

--- AutoLotTestDrive/Program.cs
+++ AutoLotTestDrive/Program.cs
@@ -16,6 +16,11 @@ namespace AutoLotTestDrive
         {
             Database.SetInitializer(new DataInitializer());
             Console.WriteLine("***** ADO.NET EF Code First *****");
+            Inventory car1 = new Inventory() { Make = "Yugo", Color = "Коричневый", PetName = "Брауни" };
+            Inventory car2 = new Inventory() { Make = "SmartCar", Color = "Коричневый", PetName = "Шорти" };
+            AddNewRecord(car1);
+            AddNewRecord(car2);
+            AddNewRecords(new List<Inventory> { car1, car2 });
             PrintAllInventory();
             Console.ReadLine();
         }
@@ -25,6 +30,24 @@ namespace AutoLotTestDrive
             using (InventoryRepo repo = new InventoryRepo())
             {
                 foreach (Inventory c in repo.GetAll()) Console.WriteLine(c);
+            }
+        }
+
+        private static void AddNewRecord(Inventory car)
+        {
+            // Добавить запись в таблицу Inventory базы данных AutoLot.
+            using (InventoryRepo repo = new InventoryRepo())
+            {
+                repo.Add(car);
+            }
+        }
+
+        private static void AddNewRecords(IList<Inventory> cars)
+        {
+            // Добавить записи в таблицу Inventory базы данных AutoLot.
+            using (InventoryRepo repo = new InventoryRepo())
+            {
+                repo.AddRange(cars);
             }
         }
     }
