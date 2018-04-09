98482071b358cd946bdc78ca7639bc6060b5cb9d
Ксения <xskuznetsova@gmail.com>
2018-04-09 16:05:12 +0300

    Реализован вывод всех записей из Inventory.

--- AutoLotTestDrive/Program.cs
+++ AutoLotTestDrive/Program.cs
@@ -1,10 +1,12 @@
 ﻿using AutoLotDAL.EF;
+using AutoLotDAL.Repos;
 using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoLotDAL.Models;
 
 namespace AutoLotTestDrive
 {
@@ -14,7 +16,16 @@ namespace AutoLotTestDrive
         {
             Database.SetInitializer(new DataInitializer());
             Console.WriteLine("***** ADO.NET EF Code First *****");
+            PrintAllInventory();
             Console.ReadLine();
+        }
+
+        private static void PrintAllInventory()
+        {
+            using (InventoryRepo repo = new InventoryRepo())
+            {
+                foreach (Inventory c in repo.GetAll()) Console.WriteLine(c);
+            }
         }
     }
 }
