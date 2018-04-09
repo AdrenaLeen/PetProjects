b2c7f350cd27fdf3782b484d45333479c199a36b
Ксения <xskuznetsova@gmail.com>
2018-04-09 16:01:27 +0300

    Тестирование сборки AutoLotDAL.

---
+++ AutoLotTestDrive/Program.cs
@@ -0,0 +1,20 @@
+﻿using AutoLotDAL.EF;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoLotTestDrive
+{
+    class Program
+    {
+        static void Main()
+        {
+            Database.SetInitializer(new DataInitializer());
+            Console.WriteLine("***** ADO.NET EF Code First *****");
+            Console.ReadLine();
+        }
+    }
+}
