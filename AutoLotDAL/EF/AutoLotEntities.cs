8b55e56dd76e85626b52d02bd957c68464e9390b
Ксения <xskuznetsova@gmail.com>
2018-04-10 20:25:17 +0300

    Зарегистрирован перехватчик.

--- AutoLotDAL/EF/AutoLotEntities.cs
+++ AutoLotDAL/EF/AutoLotEntities.cs
@@ -4,12 +4,16 @@
     using System.Data.Entity;
     using System.Linq;
     using AutoLotDAL.Models;
+    using System.Data.Entity.Infrastructure;
+    using System.Data.Entity.Infrastructure.Interception;
+    using AutoLotDAL.Interception;
 
     public class AutoLotEntities : DbContext
     {
         public AutoLotEntities()
             : base("name=AutoLotConnection")
         {
+            DbInterception.Add(new ConsoleWriterInterceptor());
         }
 
         public virtual DbSet<CreditRisk> CreditRisks { get; set; }
