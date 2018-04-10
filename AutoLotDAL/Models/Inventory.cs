765ddc3c0ac5a0e24fb55a068873aed54df6f6be
Ксения <xskuznetsova@gmail.com>
2018-04-10 18:04:39 +0300

    Обновлён класс Inventory.

--- AutoLotDAL/Models/Inventory.cs
+++ AutoLotDAL/Models/Inventory.cs
@@ -23,6 +23,9 @@ namespace AutoLotDAL.Models
         [StringLength(50)]
         public string PetName { get; set; }
 
+        [Timestamp]
+        public byte[] Timestamp { get; set; }
+
         public virtual ICollection<Order> Orders { get; set; } = new HashSet<Order>();
     }
 }
