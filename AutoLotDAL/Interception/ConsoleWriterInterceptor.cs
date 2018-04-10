5ed45202c2e599197f1a2099d32618ecaba456bb
Ксения <xskuznetsova@gmail.com>
2018-04-10 20:20:22 +0300

    Добавлен перехват в AutoLotDAL.

---
+++ AutoLotDAL/Interception/ConsoleWriterInterceptor.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity.Infrastructure.Interception;
+using System.Data.Common;
+
+namespace AutoLotDAL.Interception
+{
+    public class ConsoleWriterInterceptor : IDbCommandInterceptor
+    {
+        private void WriteInfo(bool isAsync, string commandText)
+        {
+            Console.WriteLine($"Асинхронный: {isAsync}, Текст команды: {commandText}");
+        }
+        public void NonQueryExecuting(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            WriteInfo(interceptionContext.IsAsync, command.CommandText);
+        }
+
+        public void NonQueryExecuted(DbCommand command, DbCommandInterceptionContext<int> interceptionContext)
+        {
+            WriteInfo(interceptionContext.IsAsync, command.CommandText);
+        }
+
+        public void ReaderExecuting(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            WriteInfo(interceptionContext.IsAsync, command.CommandText);
+        }
+
+        public void ReaderExecuted(DbCommand command, DbCommandInterceptionContext<DbDataReader> interceptionContext)
+        {
+            WriteInfo(interceptionContext.IsAsync, command.CommandText);
+        }
+
+        public void ScalarExecuting(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            WriteInfo(interceptionContext.IsAsync, command.CommandText);
+        }
+
+        public void ScalarExecuted(DbCommand command, DbCommandInterceptionContext<object> interceptionContext)
+        {
+            WriteInfo(interceptionContext.IsAsync, command.CommandText);
+        }
+    }
+}
