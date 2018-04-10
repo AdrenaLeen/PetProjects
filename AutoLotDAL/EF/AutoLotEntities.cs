55a78100f3e83b4211209808ae9558b5800a9d52
Ксения <xskuznetsova@gmail.com>
2018-04-10 20:32:24 +0300

    Добавлен перехватчик DababaseLogger.

--- AutoLotDAL/EF/AutoLotEntities.cs
+++ AutoLotDAL/EF/AutoLotEntities.cs
@@ -10,15 +10,25 @@
 
     public class AutoLotEntities : DbContext
     {
+        static readonly DatabaseLogger databaseLogger = new DatabaseLogger("sqllog.txt", true);
+
         public AutoLotEntities()
             : base("name=AutoLotConnection")
         {
-            DbInterception.Add(new ConsoleWriterInterceptor());
+            databaseLogger.StartLogging();
+            DbInterception.Add(databaseLogger);
         }
 
         public virtual DbSet<CreditRisk> CreditRisks { get; set; }
         public virtual DbSet<Customer> Customers { get; set; }
         public virtual DbSet<Inventory> Inventory { get; set; }
         public virtual DbSet<Order> Orders { get; set; }
+
+        protected override void Dispose(bool disposing)
+        {
+            DbInterception.Remove(databaseLogger);
+            databaseLogger.StopLogging();
+            base.Dispose(disposing);
+        }
     }
 }
