8e3771e9642a643f9ec3e28054e775018c20832b
Ксения <xskuznetsova@gmail.com>
2018-04-25 00:33:42 +0300

    Создано приложение для хостинга службы WCF.

---
+++ MagicEightBallServiceHost/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using MagicEightBallSeviceLib;
+
+namespace MagicEightBallServiceHost
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("***** Хост WCF *****");
+            Console.ReadLine();
+        }
+    }
+}
