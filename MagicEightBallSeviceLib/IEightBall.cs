81c0a4800ea036771d6b78ff041657d8cded8fba
Ксения <xskuznetsova@gmail.com>
2018-04-25 00:57:18 +0300

    Переименован проект и пространство имён.

--- MagicEightBallSeviceLib/IEightBall.cs
+++ MagicEightBallSeviceLib/IEightBall.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.ServiceModel;
 
-namespace MagicEightBallSeviceLib
+namespace MagicEightBallServiceLib
 {
     [ServiceContract(Namespace = "http://xskuznetsova.ru")]
     public interface IEightBall
