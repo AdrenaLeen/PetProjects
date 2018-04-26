f86f1d70fd4fe50671b33dc229ed6b71a6e946ee
Ксения <xskuznetsova@gmail.com>
2018-04-26 19:16:54 +0300

    Добавлен проект клиентского приложения WCF.

---
+++ MagicEightBallServiceClient/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MagicEightBallServiceClient
+{
+    class Program
+    {
+        static void Main()
+        {
+        }
+    }
+}
