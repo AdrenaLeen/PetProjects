a6420f2d7fe9ba57fb9e27c602d936644c4640bb
Ксения <xskuznetsova@gmail.com>
2018-04-26 19:30:12 +0300

    Сгенерирован код прокси в Visual Studio.

--- MagicEightBallServiceClient/Program.cs
+++ MagicEightBallServiceClient/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MagicEightBallServiceClient.ServiceReference1;
 
 namespace MagicEightBallServiceClient
 {
@@ -10,6 +11,16 @@ namespace MagicEightBallServiceClient
     {
         static void Main()
         {
+            Console.WriteLine("***** Спросите магический шар *****");
+
+            using (EightBallClient ball = new EightBallClient())
+            {
+                Console.Write("Ваш вопрос: ");
+                string question = Console.ReadLine();
+                string answer = ball.ObtainAnswerToQuestion(question);
+                Console.WriteLine("Шар говорит: {0}", answer);
+            }
+            Console.ReadLine();
         }
     }
 }
