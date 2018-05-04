4770da2b6d9d9b4244ec0a36f10f996e70e04314
Ксения <xskuznetsova@gmail.com>
2018-05-04 23:35:56 +0300

    Указаны ABC в коде.

--- MathWindowsServiceHost/MathService.cs
+++ MathWindowsServiceHost/MathService.cs
@@ -7,11 +7,16 @@ using System.Linq;
 using System.ServiceProcess;
 using System.Text;
 using System.Threading.Tasks;
+using MathServiceLibrary;
+using System.ServiceModel;
 
 namespace MathWindowsServiceHost
 {
     public partial class MathService : ServiceBase
     {
+        // Переменная-член типа ServiceHost.
+        private ServiceHost myHost;
+
         public MathService()
         {
             InitializeComponent();
@@ -19,10 +24,23 @@ namespace MathWindowsServiceHost
 
         protected override void OnStart(string[] args)
         {
+            // Проверить для подстраховки.
+            if (myHost != null) myHost.Close();
+
+            // Создать хост и указать URL для привязки HTTP.
+            myHost = new ServiceHost(typeof(MathService), new Uri("http://localhost:8080/MathServiceLibrary"));
+
+            // Выбрать стандартные конечные точки.
+            myHost.AddDefaultEndpoints();
+
+            // Открыть хост.
+            myHost.Open();
         }
 
         protected override void OnStop()
         {
+            // Остановить хост.
+            if (myHost != null) myHost.Close();
         }
     }
 }
