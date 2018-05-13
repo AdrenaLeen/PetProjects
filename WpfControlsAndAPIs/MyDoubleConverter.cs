8e51f07951f090666f2d94876ee1ea7d212d3a31
Ксения <xskuznetsova@gmail.com>
2018-05-14 00:18:41 +0300

    Реализовано преобразование данных с использованием IValueConverter.

---
+++ WpfControlsAndAPIs/MyDoubleConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+using System.Globalization;
+
+namespace WpfControlsAndAPIs
+{
+    class MyDoubleConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Преобразовать значение double в int.
+            double v = (double)value;
+            return (int)v;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            // Поскольку заботиться о "двунаправленной" привязке не нужно, просто возвратить значение value.
+            return value;
+        }
+    }
+}
