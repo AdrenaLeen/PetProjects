c47c5aa1f9900ceb0c37e1b7daf4449343ff6c62
Ксения <xskuznetsova@gmail.com>
2018-05-14 21:56:57 +0300

    Реализовано реагирование на изменение свойства.

--- CustomDepPropApp/ShowNumberControl.xaml.cs
+++ CustomDepPropApp/ShowNumberControl.xaml.cs
@@ -32,13 +32,25 @@ namespace CustomDepPropApp
         }
 
         // Это сделает возможной анимацию, применение стилей, привязку и т.д.
-        public static readonly DependencyProperty CurrentNumberProperty = DependencyProperty.Register("CurrentNumber", typeof(int), typeof(ShowNumberControl), new UIPropertyMetadata(100), new ValidateValueCallback(ValidateCurrentNumber));
+        public static readonly DependencyProperty CurrentNumberProperty = DependencyProperty.Register("CurrentNumber", typeof(int), typeof(ShowNumberControl), new UIPropertyMetadata(100, new PropertyChangedCallback(CurrentNumberChanged)), new ValidateValueCallback(ValidateCurrentNumber));
 
         public static bool ValidateCurrentNumber(object value)
         {
             // Очень просто бизнес-правило: значение должно находиться в диапазоне между 0 и 500.
             if (Convert.ToInt32(value) >= 0 && Convert.ToInt32(value) <= 500) return true;
             else return false;
+        }
+
+        private static void CurrentNumberChanged(DependencyObject depObj, DependencyPropertyChangedEventArgs args)
+        {
+            // Привести DependencyObject к ShowNumberControl.
+            ShowNumberControl c = (ShowNumberControl)depObj;
+
+            // Получить элемент управления Label в ShowNumberControl.
+            Label theLabel = c.numberDisplay;
+
+            // Установить для Label новое значение.
+            theLabel.Content = args.NewValue.ToString();
         }
     }
 }
