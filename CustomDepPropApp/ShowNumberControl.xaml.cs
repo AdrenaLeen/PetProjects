705c13314d03b425ffa8283d0b9ca73ab327e95c
Ксения <xskuznetsova@gmail.com>
2018-05-14 21:33:37 +0300

    Построено специальное свойство зависимости.

---
+++ CustomDepPropApp/ShowNumberControl.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CustomDepPropApp
+{
+    /// <summary>
+    /// Логика взаимодействия для ShowNumberControl.xaml
+    /// </summary>
+    public partial class ShowNumberControl : UserControl
+    {
+        public ShowNumberControl()
+        {
+            InitializeComponent();
+        }
+
+        public int CurrentNumber
+        {
+            get { return (int)GetValue(CurrentNumberProperty); }
+            set { SetValue(CurrentNumberProperty, value); }
+        }
+
+        // Это сделает возможной анимацию, применение стилей, привязку и т.д.
+        public static readonly DependencyProperty CurrentNumberProperty = DependencyProperty.Register("CurrentNumber", typeof(int), typeof(ShowNumberControl), new PropertyMetadata(0));
+    }
+}
