4813467d9a0f80ed22cc4576f74fe83b935c83b6
Ксения <xskuznetsova@gmail.com>
2018-05-18 15:49:24 +0300

    Визуализированы графические данные в специальном диспетчере компоновки.

---
+++ RenderingWithVisuals/CustomVisualFrameworkElement.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace RenderingWithVisuals
+{
+    class CustomVisualFrameworkElement : FrameworkElement
+    {
+        // Коллекция всех визуальных объектов.
+        VisualCollection theVisuals;
+
+        public CustomVisualFrameworkElement()
+        {
+            // Заполнить коллекцию VisualCollection несколькими объектами DrawingVisual.
+            // Аргумент конструктора представляет владельца визуальных объектов.
+            theVisuals = new VisualCollection(this);
+            theVisuals.Add(AddRect());
+            theVisuals.Add(AddCircle());
+        }
+
+        private Visual AddCircle()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+
+            // Получить объект DrawingContext для создания нового содержимого.
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                // Создать круг и нарисовать его в DrawingContext.
+                Rect rect = new Rect(new Point(160, 100), new Size(320, 80));
+                drawingContext.DrawEllipse(Brushes.DarkBlue, null, new Point(70, 90), 40, 50);
+            }
+            return drawingVisual;
+        }
+
+        private Visual AddRect()
+        {
+            DrawingVisual drawingVisual = new DrawingVisual();
+            using (DrawingContext drawingContext = drawingVisual.RenderOpen())
+            {
+                Rect rect = new Rect(new Point(160, 100), new Size(320, 80));
+                drawingContext.DrawRectangle(Brushes.Tomato, null, rect);
+            }
+            return drawingVisual;
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return theVisuals.Count; }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            // Значение должно быть больше нуля, поэтому разумно это проверить.
+            if (index < 0 || index >= theVisuals.Count) throw new ArgumentOutOfRangeException();
+            return theVisuals[index];
+        }
+    }
+}
