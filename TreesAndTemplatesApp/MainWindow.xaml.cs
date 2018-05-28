d41449fc0acbc3d41f526767633ae3127ccb7759
Ксения <xskuznetsova@gmail.com>
2018-05-28 22:54:39 +0300

    Реализовано программное инспектирование стандартного шаблона элемента управления.

--- TreesAndTemplatesApp/MainWindow.xaml.cs
+++ TreesAndTemplatesApp/MainWindow.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -8,10 +9,12 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Markup;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
 
 namespace TreesAndTemplatesApp
 {
@@ -21,6 +24,7 @@ namespace TreesAndTemplatesApp
     public partial class MainWindow : Window
     {
         private string dataToShow = string.Empty;
+        private Control ctrlToExamine = null;
 
         public MainWindow()
         {
@@ -65,6 +69,48 @@ namespace TreesAndTemplatesApp
             for (int i = 0; i < VisualTreeHelper.GetChildrenCount(obj); i++)
             {
                 BuildVisualTree(depth + 1, VisualTreeHelper.GetChild(obj, i));
+            }
+        }
+
+        private void btnTemplate_Click(object sender, RoutedEventArgs e)
+        {
+            dataToShow = "";
+            ShowTemplate();
+            txtDisplayArea.Text = dataToShow;
+        }
+
+        private void ShowTemplate()
+        {
+            // Удалить элемент, который в текущий момент находится в области предварительного просмотра.
+            if (ctrlToExamine != null) stackTemplatePanel.Children.Remove(ctrlToExamine);
+            try
+            {
+                // Загрузить PresentationFramework и создать экземпляр указанного элемента управления. Установить его размеры для отображения, а затем добавить в пустой контейнер StackPanel.
+                Assembly asm = Assembly.Load("PresentationFramework, Version=4.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35");
+                ctrlToExamine = (Control)asm.CreateInstance(txtFullName.Text);
+                ctrlToExamine.Height = 200;
+                ctrlToExamine.Width = 200;
+                ctrlToExamine.Margin = new Thickness(5);
+                stackTemplatePanel.Children.Add(ctrlToExamine);
+
+                // Определить настройки XML для предохранения отступов.
+                XmlWriterSettings xmlSettings = new XmlWriterSettings { Indent = true };
+
+                // Создать объект StringBuilder для хранения разметки XAML.
+                StringBuilder strBuilder = new StringBuilder();
+
+                // Создать объект XmlWriter на основе имеющихся настроек.
+                XmlWriter xWriter = XmlWriter.Create(strBuilder, xmlSettings);
+
+                // Сохранить разметку XAML в объекте XmlWriter на основе ControlTemplate.
+                XamlWriter.Save(ctrlToExamine.Template, xWriter);
+
+                // Отобразить разметку XAML в текстовом поле.
+                dataToShow = strBuilder.ToString();
+            }
+            catch (Exception ex)
+            {
+                dataToShow = ex.Message;
             }
         }
     }
