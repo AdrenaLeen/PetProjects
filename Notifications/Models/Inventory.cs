ab00f05397653af171ec96ea8497ae3cde3724de
Ксения <xskuznetsova@gmail.com>
2018-05-29 23:33:05 +0300

    Реализовано изменение значения свойства модели в пользовательском интерфейсе.

--- Notifications/Models/Inventory.cs
+++ Notifications/Models/Inventory.cs
@@ -1,16 +1,68 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Notifications.Models
 {
-    public class Inventory
+    public class Inventory : INotifyPropertyChanged
     {
-        public int CarId { get; set; }
-        public string Make { get; set; }
-        public string Color { get; set; }
-        public string PetName { get; set; }
+        private int carId;
+        public int CarId
+        {
+            get { return carId; }
+            set
+            {
+                if (value == carId) return;
+                carId = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string make;
+        public string Make
+        {
+            get { return make; }
+            set
+            {
+                if (value == make) return;
+                make = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string color;
+        public string Color
+        {
+            get { return color; }
+            set
+            {
+                if (value == color) return;
+                color = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private string petName;
+        public string PetName
+        {
+            get { return petName; }
+            set
+            {
+                if (value == petName) return;
+                petName = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        internal void OnPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
     }
 }
