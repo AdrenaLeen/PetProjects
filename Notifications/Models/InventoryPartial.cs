0809f3e609cd160bdd7f62b2cbf2f95a2acd340b
Ксения <xskuznetsova@gmail.com>
2018-05-30 22:14:12 +0300

    Перемещён поддерживающий код в базовый класс.

--- Notifications/Models/InventoryPartial.cs
+++ Notifications/Models/InventoryPartial.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 
 namespace Notifications.Models
 {
-    public partial class Inventory : IDataErrorInfo, INotifyDataErrorInfo
+    public partial class Inventory : EntityBase, IDataErrorInfo
     {
         public string this[string columnName]
         {
@@ -41,18 +41,6 @@ namespace Notifications.Models
 
         public string Error { get; }
 
-        private readonly Dictionary<string, List<string>> errors = new Dictionary<string, List<string>>();
-
-        public bool HasErrors => errors.Count != 0;
-
-        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
-
-        public IEnumerable GetErrors(string propertyName)
-        {
-            if (string.IsNullOrEmpty(propertyName)) return errors.Values;
-            return errors.ContainsKey(propertyName) ? errors[propertyName] : null;
-        }
-
         internal bool CheckMakeAndColor()
         {
             if (Make == "Chevy" && Color == "Розовый")
@@ -62,39 +50,6 @@ namespace Notifications.Models
                 return true;
             }
             return false;
-        }
-
-        private void OnErrorsChanged(string propertyName)
-        {
-            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
-        }
-
-        protected void ClearErrors(string propertyName = "")
-        {
-            errors.Remove(propertyName);
-            OnErrorsChanged(propertyName);
-        }
-
-        private void AddError(string propertyName, string error)
-        {
-            AddErrors(propertyName, new List<string> { error });
-        }
-
-        private void AddErrors(string propertyName, IList<string> myErrors)
-        {
-            bool changed = false;
-            if (!errors.ContainsKey(propertyName))
-            {
-                errors.Add(propertyName, new List<string>());
-                changed = true;
-            }
-            myErrors.ToList().ForEach(x =>
-            {
-                if (errors[propertyName].Contains(x)) return;
-                errors[propertyName].Add(x);
-                changed = true;
-            });
-            if (changed) OnErrorsChanged(propertyName);
         }
     }
 }
