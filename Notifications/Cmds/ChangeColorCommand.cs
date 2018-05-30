443b96a565b2f9a5e11bea8a99970d172ccfe352
Ксения <xskuznetsova@gmail.com>
2018-05-31 01:08:20 +0300

    Реализован интерфейс ICommand.

---
+++ Notifications/Cmds/ChangeColorCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Notifications.Models;
+
+namespace Notifications.Cmds
+{
+    class ChangeColorCommand : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter) => (parameter as Inventory) != null;
+
+        public void Execute(object parameter) => ((Inventory)parameter).Color = "Розовый";
+    }
+}
