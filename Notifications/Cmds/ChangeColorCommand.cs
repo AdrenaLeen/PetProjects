9569511996def6f9724be58ec4a3742826036a87
Ксения <xskuznetsova@gmail.com>
2018-05-31 01:20:01 +0300

    Команда присоединена к объекту CommandManager.

--- Notifications/Cmds/ChangeColorCommand.cs
+++ Notifications/Cmds/ChangeColorCommand.cs
@@ -10,7 +10,11 @@ namespace Notifications.Cmds
 {
     class ChangeColorCommand : ICommand
     {
-        public event EventHandler CanExecuteChanged;
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
 
         public bool CanExecute(object parameter) => (parameter as Inventory) != null;
 
