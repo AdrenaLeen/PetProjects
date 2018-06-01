cff33bf0ce078665b1e2e65d997c186aa9361934
Ксения <xskuznetsova@gmail.com>
2018-06-01 16:50:13 +0300

    Изменён класс InventoryPartial.

--- AutoLotDAL/Models/Partials/InventoryPartial.cs
+++ AutoLotDAL/Models/Partials/InventoryPartial.cs
@@ -17,5 +17,54 @@ namespace AutoLotDAL.Models
 
         [NotMapped]
         public string MakeColor => $"{Make} + ({Color})";
+
+        public override string this[string columnName]
+        {
+            get
+            {
+                string[] errors = null;
+                bool hasError = false;
+                switch (columnName)
+                {
+                    case nameof(CarId):
+                        errors = GetErrorsFromAnnotations(nameof(CarId), CarId);
+                        break;
+                    case nameof(Make):
+                        hasError = CheckMakeAndColor();
+                        if (Make == "ModelT")
+                        {
+                            AddError(nameof(Make), "Слишком старая");
+                            hasError = true;
+                        }
+                        errors = GetErrorsFromAnnotations(nameof(Make), Make);
+                        break;
+                    case nameof(Color):
+                        hasError = CheckMakeAndColor();
+                        errors = GetErrorsFromAnnotations(nameof(Color), Color);
+                        break;
+                    case nameof(PetName):
+                        errors = GetErrorsFromAnnotations(nameof(PetName), PetName);
+                        break;
+                }
+                if (errors != null && errors.Length != 0)
+                {
+                    AddErrors(columnName, errors);
+                    hasError = true;
+                }
+                if (!hasError) ClearErrors(columnName);
+                return string.Empty;
+            }
+        }
+
+        internal bool CheckMakeAndColor()
+        {
+            if (Make == "Chevy" && Color == "Розовый")
+            {
+                AddError(nameof(Make), $"{Make} не поставляется в цвете {Color}");
+                AddError(nameof(Color), $"{Make} не поставляется в цвете {Color}");
+                return true;
+            }
+            return false;
+        }
     }
 }
