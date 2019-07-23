ac505f6bd5e53a0a0f8669a705f8515dc5d4df98
Ксения <xskuznetsova@gmail.com>
2019-07-23 23:10:50 +0300

    Изменила целевую рабочую среду.

--- ImplicitlyTypedLocalVars/Program.cs
+++ ImplicitlyTypedLocalVars/Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ImplicitlyTypedLocalVars
 {
@@ -18,6 +15,8 @@ namespace ImplicitlyTypedLocalVars
             string myString = "Вставай!";
             var myData = myString;
 
+            GetAnInt();
+            ImplicitTypingIsStrongTyping();
             LinqQueryOverInts();
 
             Console.ReadLine();
