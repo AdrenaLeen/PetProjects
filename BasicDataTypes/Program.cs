4e97dfbc0aa2e75e6fa6dc4feffd01bb376a71c9
Ксения <xskuznetsova@gmail.com>
2019-07-23 22:45:53 +0300

    Добавила метод с литералом default.

--- BasicDataTypes/Program.cs
+++ BasicDataTypes/Program.cs
@@ -8,12 +8,13 @@ namespace BasicDataTypes
         static void Main()
         {
             LocalVarDeclarations();
+            DefaultDeclarations();
             NewingDataTypes();
             ObjectFunctionality();
             DataTypeFunctionality();
 
-            Console.WriteLine("bool.FalseString: {0}", bool.FalseString);
-            Console.WriteLine("bool.TrueString: {0}", bool.TrueString);
+            Console.WriteLine($"bool.FalseString: {bool.FalseString}");
+            Console.WriteLine($"bool.TrueString: {bool.TrueString}");
             Console.WriteLine();
 
             CharFunctionality();
@@ -41,7 +42,14 @@ namespace BasicDataTypes
             // Использовать тип данных System.Boolean для объявления булевской переменной.
             System.Boolean b4 = false;
 
-            Console.WriteLine("Ваши данные: {0}, {1}, {2}, {3}, {4}, {5}", myInt, myString, b1, b2, b3, b4);
+            Console.WriteLine($"Ваши данные: {myInt}, {myString}, {b1}, {b2}, {b3}, {b4}");
+            Console.WriteLine();
+        }
+
+        static void DefaultDeclarations()
+        {
+            Console.WriteLine("=> Литерал default");
+            int myInt = default;
             Console.WriteLine();
         }
 
@@ -60,7 +68,7 @@ namespace BasicDataTypes
             // Устанавливается в 1/1/0001 12:00:00 AM.
             DateTime dt = new DateTime();
 
-            Console.WriteLine("{0}, {1}, {2}, {3}", b, i, d, dt);
+            Console.WriteLine($"{b}, {i}, {d}, {dt}");
             Console.WriteLine();
         }
 
@@ -68,23 +76,23 @@ namespace BasicDataTypes
         {
             Console.WriteLine("=> Функциональность System.Object:");
             // Ключевое слово int языка C# - это в действительности сокращение для типа System.Int32, который наследует от System.Object следующие члены:
-            Console.WriteLine("12.GetHashCode() = {0}", 12.GetHashCode());
-            Console.WriteLine("12.Equals(23) = {0}", 12.Equals(23));
-            Console.WriteLine("12.ToString() = {0}", 12.ToString());
-            Console.WriteLine("12.GetType() = {0}", 12.GetType());
+            Console.WriteLine($"12.GetHashCode() = {12.GetHashCode()}");
+            Console.WriteLine($"12.Equals(23) = {12.Equals(23)}");
+            Console.WriteLine($"12.ToString() = {12.ToString()}");
+            Console.WriteLine($"12.GetType() = {12.GetType()}");
             Console.WriteLine();
         }
 
         private static void DataTypeFunctionality()
         {
             Console.WriteLine("=> Функциональность числовых типов данных:");
-            Console.WriteLine("Максимум int: {0}", int.MaxValue);
-            Console.WriteLine("Минимум int: {0}", int.MinValue);
-            Console.WriteLine("Максимум double: {0}", double.MaxValue);
-            Console.WriteLine("Минимум double: {0}", double.MinValue);
-            Console.WriteLine("double.Epsilon: {0}", double.Epsilon);
-            Console.WriteLine("double.PositiveInfitity: {0}", double.PositiveInfinity);
-            Console.WriteLine("double.NegativeInfitity: {0}", double.NegativeInfinity);
+            Console.WriteLine($"Максимум int: {int.MaxValue}");
+            Console.WriteLine($"Минимум int: {int.MinValue}");
+            Console.WriteLine($"Максимум double: {double.MaxValue}");
+            Console.WriteLine($"Минимум double: {double.MinValue}");
+            Console.WriteLine($"double.Epsilon: {double.Epsilon}");
+            Console.WriteLine($"double.PositiveInfitity: {double.PositiveInfinity}");
+            Console.WriteLine($"double.NegativeInfitity: {double.NegativeInfinity}");
             Console.WriteLine();
         }
 
@@ -92,11 +100,11 @@ namespace BasicDataTypes
         {
             Console.WriteLine("=> Функциональность типа char:");
             char myChar = 'а';
-            Console.WriteLine("char.IsDigit('а'): {0}", char.IsDigit(myChar));
-            Console.WriteLine("char.IsLetter('а'): {0}", char.IsLetter(myChar));
-            Console.WriteLine("char.IsWhiteSpace('Hello There', 5): {0}", char.IsWhiteSpace("Hello There", 5));
-            Console.WriteLine("char.IsWhiteSpace('Hello There', 6): {0}", char.IsWhiteSpace("Hello There", 6));
-            Console.WriteLine("char.IsPunctuation('?'): {0}", char.IsPunctuation('?'));
+            Console.WriteLine($"char.IsDigit('а'): {char.IsDigit(myChar)}");
+            Console.WriteLine($"char.IsLetter('а'): {char.IsLetter(myChar)}");
+            Console.WriteLine($"char.IsWhiteSpace('Hello There', 5): {char.IsWhiteSpace("Hello There", 5)}");
+            Console.WriteLine($"char.IsWhiteSpace('Hello There', 6): {char.IsWhiteSpace("Hello There", 6)}");
+            Console.WriteLine($"char.IsPunctuation('?'): {char.IsPunctuation('?')}");
             Console.WriteLine();
         }
 
@@ -104,13 +112,13 @@ namespace BasicDataTypes
         {
             Console.WriteLine("=> Разбор значений из строковых данных:");
             bool b = bool.Parse("True");
-            Console.WriteLine("Значение b: {0}", b);
+            Console.WriteLine($"Значение b: {b}");
             double d = double.Parse("99,884");
-            Console.WriteLine("Значение d: {0}", d);
+            Console.WriteLine($"Значение d: {d}");
             int i = int.Parse("8");
-            Console.WriteLine("Значение i: {0}", i);
+            Console.WriteLine($"Значение i: {i}");
             char c = Char.Parse("w");
-            Console.WriteLine("Значение c: {0}", c);
+            Console.WriteLine($"Значение c: {c}");
             Console.WriteLine();
         }
 
@@ -121,11 +129,11 @@ namespace BasicDataTypes
             DateTime dt = new DateTime(2015, 10, 17);
 
             // Какой это день месяца?
-            Console.WriteLine("День {0} - это {1}", dt.Date, dt.DayOfWeek);
+            Console.WriteLine($"День {dt.Date} - это {dt.DayOfWeek}");
 
             // Сейчас месяц декабрь.
             dt = dt.AddMonths(2);
-            Console.WriteLine("Летнее время: {0}", dt.IsDaylightSavingTime());
+            Console.WriteLine($"Летнее время: {dt.IsDaylightSavingTime()}");
 
             // Этот конструктор принимает часы, минуты и секунды.
             TimeSpan ts = new TimeSpan(4, 30, 0);
@@ -142,17 +150,17 @@ namespace BasicDataTypes
             BigInteger biggy = BigInteger.Parse("9999999999999999999999999999999999999999999999");
 
             // Значение biggy
-            Console.WriteLine("Значение biggy равно {0}", biggy);
+            Console.WriteLine($"Значение biggy равно {biggy}");
 
             // biggy - чётное?
-            Console.WriteLine("biggy - чётное число?: {0}", biggy.IsEven);
+            Console.WriteLine($"biggy - чётное число?: {biggy.IsEven}");
 
             // biggy - степень 2?
-            Console.WriteLine("biggy - степень двойки?: {0}", biggy.IsPowerOfTwo);
+            Console.WriteLine($"biggy - степень двойки?: {biggy.IsPowerOfTwo}");
             BigInteger reallyBig = BigInteger.Multiply(biggy, BigInteger.Parse("8888888888888888888888888888888888888888888"));
 
             // Значение reallyBig
-            Console.WriteLine("Значение reallyBig равно {0}", reallyBig);
+            Console.WriteLine($"Значение reallyBig равно {reallyBig}");
         }
     }
 }
