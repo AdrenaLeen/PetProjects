8cf0922c7efd3c97a6f14fa52c4b2d9185954c40
Ксения <xskuznetsova@gmail.com>
2019-07-29 16:17:03 +0300

    Попробовала кортежи как возвращаемые значения методов.

--- FunWithTuples/Program.cs
+++ FunWithTuples/Program.cs
@@ -31,8 +31,20 @@ namespace FunWithTuples
             var foo = new { Prop1 = "первый", Prop2 = "второй" };
             (string Prop1, string Prop2) bar = (foo.Prop1, foo.Prop2);
             Console.WriteLine($"{bar.Prop1};{bar.Prop2}");
+            Console.WriteLine();
+
+            (int a, string b, bool c) samples = FillTheseValues();
+            Console.WriteLine($"Int: {samples.a}");
+            Console.WriteLine($"String: {samples.b}");
+            Console.WriteLine($"Boolean: {samples.c}");
+            Console.WriteLine();
 
             Console.ReadLine();
         }
+
+        static (int a, string b, bool c) FillTheseValues() => (9, "Насладитесь этой строкой.", true);
+
+        // Действия, необходимые для расщепления полного имени.
+        static (string first, string middle, string last) SplitNames(string fullName) => ("Ксения", "Сергеевна", "Кузнецова");
     }
 }
