f31e3a65c651a6d0b352c8219c41139680d1640a
Ксения <xskuznetsova@gmail.com>
2019-08-08 16:31:55 +0300

    Обновила целевую рабочую среду.

--- SimpleUtilityClass/TimeUtilClass.cs
+++ SimpleUtilityClass/TimeUtilClass.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using static System.Console;
+﻿using static System.Console;
 using static System.DateTime;
 
 namespace SimpleUtilityClass
@@ -11,14 +6,8 @@ namespace SimpleUtilityClass
     // Статические классы могут содержать только статические члены!
     static class TimeUtilClass
     {
-        public static void PrintTime()
-        {
-            WriteLine(Now.ToShortTimeString());
-        }
+        public static void PrintTime() => WriteLine(Now.ToShortTimeString());
 
-        public static void PrintDate()
-        {
-            WriteLine(Today.ToShortDateString());
-        }
+        public static void PrintDate() => WriteLine(Today.ToShortDateString());
     }
 }
