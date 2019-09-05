2c1ae7281212939aba0731fcd309ef4b61161ab6
Ксения <xskuznetsova@gmail.com>
2019-09-05 16:25:01 +0300

    Изменила целевую рабочую среду.

--- MIInterfaceHierarchy/IDrawable.cs
+++ MIInterfaceHierarchy/IDrawable.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MIInterfaceHierarchy
+﻿namespace MIInterfaceHierarchy
 {
     // Множественное наследование для интерфейсных тиов допускается.
     interface IDrawable
--- MIInterfaceHierarchy/Program.cs
+++ MIInterfaceHierarchy/Program.cs
@@ -1,14 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MIInterfaceHierarchy
+﻿namespace MIInterfaceHierarchy
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
         }
     }
