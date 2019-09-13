fd876cb232b702bd32d692328752beb53d86bd65
Ксения <xskuznetsova@gmail.com>
2019-09-13 15:28:15 +0300

    Обновила целевую рабочую среду.

--- IssuesWithNongenericCollections/PersonCollection.cs
+++ IssuesWithNongenericCollections/PersonCollection.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Collections;
+﻿using System.Collections;
 
 namespace IssuesWithNongenericCollections
 {
@@ -12,28 +7,16 @@ namespace IssuesWithNongenericCollections
         private ArrayList arPeople = new ArrayList();
 
         // Приведение для вызывающего кода.
-        public Person GetPerson(int pos)
-        {
-            return (Person)arPeople[pos];
-        }
+        public Person GetPerson(int pos) => (Person)arPeople[pos];
 
         // Вставка только объектов Person.
-        public void AddPerson(Person p)
-        {
-            arPeople.Add(p);
-        }
+        public void AddPerson(Person p) => arPeople.Add(p);
 
-        public void ClearPeople()
-        {
-            arPeople.Clear();
-        }
+        public void ClearPeople() => arPeople.Clear();
 
-        public int Count { get { return arPeople.Count; } }
+        public int Count => arPeople.Count;
 
         // Поддержка перечисления с помощью foreach.
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return arPeople.GetEnumerator();
-        }
+        IEnumerator IEnumerable.GetEnumerator() => arPeople.GetEnumerator();
     }
 }
