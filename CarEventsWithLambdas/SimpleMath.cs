58c8b2e1b197dc72a65445c2447255d81331a63e
Ксения <xskuznetsova@gmail.com>
2019-09-29 16:45:50 +0300

    Обновлены лямбда-выражения и члены, сжатые до выражений.

---
+++ CarEventsWithLambdas/SimpleMath.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace CarEventsWithLambdas
+{
+    class SimpleMath
+    {
+        public int Add(int x, int y) => x + y;
+        public void PrintSum(int x, int y) => Console.WriteLine(x + y);
+    }
+}
