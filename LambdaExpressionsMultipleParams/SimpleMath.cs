1b5fd747d77f6a56bcf40728c9f88ee6b1f0d87d
Ксения <xskuznetsova@gmail.com>
2019-09-29 16:40:27 +0300

    Обновила целевую рабочую среду.

--- LambdaExpressionsMultipleParams/SimpleMath.cs
+++ LambdaExpressionsMultipleParams/SimpleMath.cs
@@ -1,24 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LambdaExpressionsMultipleParams
+﻿namespace LambdaExpressionsMultipleParams
 {
     class SimpleMath
     {
         public delegate void MathMessage(string msg, int result);
         private MathMessage mmDelegate;
 
-        public void SetMathHandler(MathMessage target)
-        {
-            mmDelegate = target;
-        }
+        public void SetMathHandler(MathMessage target) => mmDelegate = target;
 
-        public void Add(int x, int y)
-        {
-            mmDelegate?.Invoke("Суммирование завершено!", x + y);
-        }
+        public void Add(int x, int y) => mmDelegate?.Invoke("Суммирование завершено!", x + y);
     }
 }
