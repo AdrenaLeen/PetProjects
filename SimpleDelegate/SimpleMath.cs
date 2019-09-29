2854fe436a1063bc709f7efa661099efa02d3834
Ксения <xskuznetsova@gmail.com>
2019-09-29 16:16:51 +0300

    Обновила целевую рабочую среду.

--- SimpleDelegate/SimpleMath.cs
+++ SimpleDelegate/SimpleMath.cs
@@ -1,27 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace SimpleDelegate
+﻿namespace SimpleDelegate
 {
     // Этот класс содержит методы, на которые будет указывать BinaryOp.
     class SimpleMath
     {
-        public int Add(int x, int y)
-        {
-            return x + y;
-        }
+        public int Add(int x, int y) => x + y;
 
-        public static int Subtract(int x, int y)
-        {
-            return x - y;
-        }
+        public static int Subtract(int x, int y) => x - y;
 
-        public static int SquareNumber(int a)
-        {
-            return a * a;
-        }
+        public static int SquareNumber(int a) => a * a;
     }
 }
