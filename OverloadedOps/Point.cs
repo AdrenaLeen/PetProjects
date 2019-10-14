88d11a56841bdafc7ba257da508a6829bc27be30
Ксения <xskuznetsova@gmail.com>
2019-10-14 20:48:01 +0300

    Обновила целевую рабочую среду.

--- OverloadedOps/Point.cs
+++ OverloadedOps/Point.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace OverloadedOps
+﻿namespace OverloadedOps
 {
     // Простой будничный класс C#.
     // Более интеллектуальный тип Point.
