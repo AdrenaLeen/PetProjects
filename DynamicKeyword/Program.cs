fe93beb3e693bcb36d7c385db9d4435e3ef729d0
Ксения <xskuznetsova@gmail.com>
2019-10-31 23:13:18 +0300

    Обновила целевую рабочую среду.

--- DynamicKeyword/Program.cs
+++ DynamicKeyword/Program.cs
@@ -1,9 +1,6 @@
-﻿using System;
+﻿using Microsoft.CSharp.RuntimeBinder;
+using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.CSharp.RuntimeBinder;
 
 namespace DynamicKeyword
 {
