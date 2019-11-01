83a4cc0bc06b4a50a35b4bdfc1501df91efa3abe
Ксения <xskuznetsova@gmail.com>
2019-11-01 14:29:51 +0300

    Обновила целевую рабочую среду.

--- AsyncCallbackDelegate/Program.cs
+++ AsyncCallbackDelegate/Program.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Threading;
 using System.Runtime.Remoting.Messaging;
+using System.Threading;
 
 namespace AsyncCallbackDelegate
 {
