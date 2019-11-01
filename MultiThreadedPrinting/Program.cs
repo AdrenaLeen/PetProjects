049f90272015d52cfcc63e79a9cdd5b33e98e78d
Ксения <xskuznetsova@gmail.com>
2019-11-01 14:39:49 +0300

    Обновила целевую рабочую среду.

--- MultiThreadedPrinting/Program.cs
+++ MultiThreadedPrinting/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Threading;
 
 namespace MultiThreadedPrinting
@@ -17,11 +13,7 @@ namespace MultiThreadedPrinting
 
             // Создать 10 потоков, которые указывают на один и тот же метод того же самого объекта.
             Thread[] threads = new Thread[10];
-            for (int i = 0; i < 10; i++)
-            {
-                threads[i] = new Thread(new ThreadStart(p.PrintNumbers));
-                threads[i].Name = string.Format($"Рабочий поток #{i}");
-            }
+            for (int i = 0; i < 10; i++) threads[i] = new Thread(new ThreadStart(p.PrintNumbers)) { Name = string.Format($"Рабочий поток #{i}") };
 
             // Теперь запустить их все
             foreach (Thread t in threads) t.Start();
