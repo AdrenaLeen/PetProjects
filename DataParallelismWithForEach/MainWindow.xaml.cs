857e92c0c486b6e3f84a7bfc026766e9766d3e92
Ксения <xskuznetsova@gmail.com>
2019-11-01 15:30:18 +0300

    Обновила целевую рабочую среду.

--- DataParallelismWithForEach/MainWindow.xaml.cs
+++ DataParallelismWithForEach/MainWindow.xaml.cs
@@ -1,20 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Drawing;
+using System.IO;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Threading;
-using System.IO;
-using System.Drawing;
 
 namespace DataParallelismWithForEach
 {
@@ -24,28 +13,27 @@ namespace DataParallelismWithForEach
     public partial class MainWindow : Window
     {
         // Новая переменная уровня MainWindow.
-        private CancellationTokenSource cancelToken = new CancellationTokenSource();
+        private readonly CancellationTokenSource cancelToken = new CancellationTokenSource();
 
         public MainWindow()
         {
             InitializeComponent();
         }
 
-        private void btnProcessImages_Click(object sender, RoutedEventArgs e)
+        private void BtnProcessImages_Click(object sender, RoutedEventArgs e)
         {
             // Запустить новую "задачу" для обработки файлов.
-            Task.Factory.StartNew(() =>
-            {
-                ProcessFiles();
-            });
+            Task.Factory.StartNew(() => ProcessFiles());
         }
 
         private void ProcessFiles()
         {
             // Использовать экземпляр ParallelOptions для хранения CancellationToken.
-            ParallelOptions parOpts = new ParallelOptions();
-            parOpts.CancellationToken = cancelToken.Token;
-            parOpts.MaxDegreeOfParallelism = Environment.ProcessorCount;
+            ParallelOptions parOpts = new ParallelOptions
+            {
+                CancellationToken = cancelToken.Token,
+                MaxDegreeOfParallelism = Environment.ProcessorCount
+            };
 
             // Загрузить все файлы *.jpg и создать новый каталог для модифицированных данных.
             string[] files = Directory.GetFiles(@"D:\OneDrive\Pictures\Фотоальбом", "*.jpg", SearchOption.AllDirectories);
@@ -58,37 +46,26 @@ namespace DataParallelismWithForEach
                 Parallel.ForEach(files, parOpts, currentFile =>
                 {
                     parOpts.CancellationToken.ThrowIfCancellationRequested();
-                    string filename = System.IO.Path.GetFileName(currentFile);
+                    string filename = Path.GetFileName(currentFile);
                     using (Bitmap bitmap = new Bitmap(currentFile))
                     {
                         bitmap.RotateFlip(RotateFlipType.Rotate180FlipNone);
-                        bitmap.Save(System.IO.Path.Combine(newDir, filename));
+                        bitmap.Save(Path.Combine(newDir, filename));
 
                         // Вызвать Invoke на объекте MainWindow, чтобы позволить вторичным потокам получать доступ к элементам управления в безопасной к потокам манере.
-                        this.Dispatcher.Invoke((Action)delegate
-                        {
-                            this.Title = $"Обработка {filename} в потоке {Thread.CurrentThread.ManagedThreadId}";
-                        });
+                        Dispatcher.Invoke(delegate { Title = $"Обработка {filename} в потоке {Thread.CurrentThread.ManagedThreadId}"; });
                     }
                 });
-                this.Dispatcher.Invoke((Action)delegate
-                {
-                    this.Title = "Готово!";
-                });
+                Dispatcher.Invoke(delegate { Title = "Готово!"; });
             }
             catch (OperationCanceledException ex)
             {
-                this.Dispatcher.Invoke((Action)delegate
-                {
-                    this.Title = ex.Message;
-                });
+                Dispatcher.Invoke(delegate { Title = ex.Message; });
             }
         }
 
-        private void btnCancel_Click(object sender, RoutedEventArgs e)
-        {
+        private void BtnCancel_Click(object sender, RoutedEventArgs e) =>
             // Используется для сообщения всем рабочим потокам о необходимости останова!
             cancelToken.Cancel();
-        }
     }
 }
