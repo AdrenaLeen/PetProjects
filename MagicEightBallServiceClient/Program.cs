e2b0cdf04c0ceec57fcdb3333367559833b9ceb8
Ксения <xskuznetsova@gmail.com>
2019-11-03 22:55:04 +0300

    Обновила целевую рабочую среду.

--- MagicEightBallServiceClient/Program.cs
+++ MagicEightBallServiceClient/Program.cs
@@ -1,9 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using MagicEightBallServiceClient.ServiceReference1;
+﻿using MagicEightBallServiceClient.ServiceReference1;
+using System;
 
 namespace MagicEightBallServiceClient
 {
--- MagicEightBallServiceHost/Program.cs
+++ MagicEightBallServiceHost/Program.cs
@@ -1,11 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MagicEightBallServiceLib;
+using System;
 using System.ServiceModel;
 using System.ServiceModel.Description;
-using MagicEightBallServiceLib;
 
 namespace MagicEightBallServiceHost
 {
