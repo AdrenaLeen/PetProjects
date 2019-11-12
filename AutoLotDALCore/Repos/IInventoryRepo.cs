a1baaa178d41404b41d4930e51ba211224163a4b
Ксения <xskuznetsova@gmail.com>
2019-11-12 17:41:29 +0300

    Создан InventoryRepo.

---
+++ AutoLotDALCore/Repos/IInventoryRepo.cs
@@ -0,0 +1,12 @@
+﻿using AutoLotDALCore.Models;
+using System.Collections.Generic;
+
+namespace AutoLotDALCore.Repos
+{
+    public interface IInventoryRepo : IRepo<Inventory>
+    {
+        List<Inventory> Search(string searchString);
+        List<Inventory> GetPinkCars();
+        List<Inventory> GetRelatedData();
+    }
+}
---
+++ AutoLotDALCore/Repos/InventoryRepo.cs
@@ -0,0 +1,21 @@
+﻿using AutoLotDALCore.EF;
+using AutoLotDALCore.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AutoLotDALCore.Repos
+{
+    public class InventoryRepo : BaseRepo<Inventory>, IInventoryRepo
+    {
+        public InventoryRepo(AutoLotContext context) : base(context) { }
+
+        public override List<Inventory> GetAll() => GetAll(x => x.PetName, true).ToList();
+
+        public List<Inventory> GetPinkCars() => GetSome(x => x.Color == "Pink");
+
+        public List<Inventory> Search(string searchString) => Context.Cars.Where(c => Microsoft.EntityFrameworkCore.EF.Functions.Like(c.PetName, $"%{searchString}%")).ToList();
+
+        public List<Inventory> GetRelatedData() => Context.Cars.FromSql("SELECT * FROM Inventory").Include(x => x.Orders).ThenInclude(x => x.Customer).ToList();
+    }
+}
