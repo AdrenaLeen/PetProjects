c0e7c4cfeabc7157b483a0a4816600946a3651e1
Ксения <xskuznetsova@gmail.com>
2019-11-12 15:39:11 +0300

    Создала класс AutoLotContext.

---
+++ AutoLotDALCore/EF/AutoLotContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace AutoLotDALCore.EF
+{
+    public class AutoLotContextFactory : IDesignTimeDbContextFactory<AutoLotContext>
+    {
+        public AutoLotContext CreateDbContext(string[] args)
+        {
+            DbContextOptionsBuilder<AutoLotContext> optionsBuilder = new DbContextOptionsBuilder<AutoLotContext>();
+            string connectionString = @"server=.\SQLEXPRESS;database=AutoLotCore;integrated security=True;MultipleActiveResultSets=True;App=EntityFramework";
+            optionsBuilder.UseSqlServer(
+                    connectionString,
+                    options => options.EnableRetryOnFailure())
+                .ConfigureWarnings(warnings => warnings.Throw(RelationalEventId.QueryClientEvaluationWarning));
+            return new AutoLotContext(optionsBuilder.Options);
+        }
+    }
+}
