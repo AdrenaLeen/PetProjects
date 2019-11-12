aff700e5ba3e6aa0018e2a2aa95c574714af382e
Ксения <xskuznetsova@gmail.com>
2019-11-12 17:56:11 +0300

    Протестирована библиотека AutoLotDALCore.

--- AutoLotDALCoreTestDriver/Program.cs
+++ AutoLotDALCoreTestDriver/Program.cs
@@ -1,12 +1,29 @@
-﻿using System;
+﻿using AutoLotDALCore.DataInitialization;
+using AutoLotDALCore.EF;
+using AutoLotDALCore.Models;
+using AutoLotDALCore.Repos;
+using System;
 
 namespace AutoLotDALCoreTestDriver
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** ADO.NET EF Core *****");
+            using (AutoLotContext context = new AutoLotContext())
+            {
+                MyDataInitializer.RecreateDatabase(context);
+                MyDataInitializer.InitializeData(context);
+                foreach (Inventory c in context.Cars) Console.WriteLine(c);
+                Console.WriteLine();
+
+                Console.WriteLine("***** Использование репозитория *****");
+                using InventoryRepo repo = new InventoryRepo(context);
+                foreach (Inventory c in repo.GetAll()) Console.WriteLine(c);
+            }
+
+            Console.ReadLine();
         }
     }
 }
