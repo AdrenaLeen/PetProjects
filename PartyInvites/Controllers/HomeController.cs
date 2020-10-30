8c3cd48eadf1654e1562c3bb5c158b7ef04bdd66
Xenia <xskuznetsova@gmail.com>
2020-10-30 22:09:56 +0300

    Создала второе действие и строго типизированное представление.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -1,20 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.Extensions.Logging;
-using PartyInvites.Models;
 using System;
-using System.Diagnostics;
 
 namespace PartyInvites.Controllers
 {
     public class HomeController : Controller
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
         public ViewResult Index()
         {
             int hour = DateTime.Now.Hour;
@@ -22,15 +12,9 @@ namespace PartyInvites.Controllers
             return View("MyView");
         }
 
-        public IActionResult Privacy()
+        public ViewResult RsvpForm()
         {
             return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
     }
 }
