f2cefa0a13defc0eadff10739008d272f0dc1370
Xenia <xskuznetsova@gmail.com>
2020-10-31 15:51:16 +0300

    Реализовала сохранение ответов.

--- PartyInvites/Controllers/HomeController.cs
+++ PartyInvites/Controllers/HomeController.cs
@@ -21,8 +21,8 @@ namespace PartyInvites.Controllers
         [HttpPost]
         public ViewResult RsvpForm(GuestResponse guestResponse)
         {
-            // Что-то сделать: сохранить ответ от гостя
-            return View();
+            Repository.AddResponse(guestResponse);
+            return View("Thanks", guestResponse);
         }
     }
 }
