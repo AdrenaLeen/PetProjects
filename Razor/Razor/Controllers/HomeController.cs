45dd379be84ec8cdaf22be4d3eb883ed3382df6b
Xenia <xskuznetsova@gmail.com>
2020-11-17 17:41:15 +0300

    Создала контроллер.

---
+++ Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Каяк",
+                Description = "Лодка на одного человека",
+                Category = "Watersports",
+                Price = 275M
+            };
+            return View(myProduct);
+        }
+    }
+}
