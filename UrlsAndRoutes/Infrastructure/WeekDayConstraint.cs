4bfb7fa507b26a0cb298e117c2d38e33846b44cd
Xenia <xskuznetsova@gmail.com>
2021-03-11 14:41:40 +0300

    Добавила проект с маршрутизацией URL.

---
+++ UrlsAndRoutes/Infrastructure/WeekDayConstraint.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Routing;
+using System;
+using System.Linq;
+
+namespace UrlsAndRoutes.Infrastructure
+{
+    public class WeekDayConstraint : IRouteConstraint
+    {
+        static string[] Days = new[] { "mon", "tue", "wed", "thu", "fri", "sat", "sun" };
+
+        public bool Match(HttpContext httpContext, IRouter route, string routeKey, RouteValueDictionary values,
+            RouteDirection routeDirection) => Days.Contains(values[routeKey]?.ToString().ToLowerInvariant());
+    }
+}
