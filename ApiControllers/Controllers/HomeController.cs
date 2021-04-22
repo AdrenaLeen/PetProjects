e32833da81c660a0bc4fd2b9dfa5c03659fd1893
Xenia <xskuznetsova@gmail.com>
2021-04-22 22:46:45 +0300

    Добавила проект с контроллерами API.

---
+++ ApiControllers/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using ApiControllers.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ApiControllers.Controllers
+{
+    public class HomeController : Controller
+    {
+        private IRepository Repository { get; set; }
+
+        public HomeController(IRepository repo) => Repository = repo;
+
+        public ViewResult Index() => View(Repository.Reservations);
+
+        [HttpPost]
+        public IActionResult AddReservation(Reservation reservation)
+        {
+            Repository.AddReservation(reservation);
+            return RedirectToAction("Index");
+        }
+    }
+}
