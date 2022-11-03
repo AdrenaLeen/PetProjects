89714da329819b096d2ab8f00f58c8d11812360e
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2022-11-03 17:20:23 +0300

    Обновила целевую платформу до .NET 6.

--- ImplicitlyTypedLocalVars/Program.cs
+++ ImplicitlyTypedLocalVars/Program.cs
@@ -1,69 +1,79 @@
-﻿using System;
-using System.Linq;
+﻿DeclareImplicitVars();
+DeclarelmplicitNumerics();
 
-namespace ImplicitlyTypedLocalVars
+// Также допустимо!
+var myInt = 0;
+var anotherInt = myInt;
+string myString = "Вставай!";
+var myData = myString;
+
+GetAnInt();
+ImplicitTypingIsStrongTyping();
+LinqQueryOverInts();
+
+Console.ReadLine();
+
+static void DeclareImplicitVars()
 {
-    class Program
-    {
-        static void Main()
-        {
-            DeclareImplicitVars();
+    // Неявно типизированные локальные переменные объявляются следующим образом:
+    // var имяПеременной = начальноеЗначение;
+    var myInt = 0;
+    var myBool = true;
+    var myString = "Time, marches on...";
 
-            // Также допустимо!
-            var myInt = 0;
-            var anotherInt = myInt;
-            string myString = "Вставай!";
-            var myData = myString;
+    // Вывести имена лежащих в основе типов.
+    Console.WriteLine("myInt является: {0}", myInt.GetType().Name);
+    Console.WriteLine("myBool является: {0}", myBool.GetType().Name);
+    Console.WriteLine("myString является: {0}", myString.GetType().Name);
+    Console.WriteLine();
+}
 
-            GetAnInt();
-            ImplicitTypingIsStrongTyping();
-            LinqQueryOverInts();
+static int GetAnInt()
+{
+    var retVal = 9;
+    return retVal;
+}
 
-            Console.ReadLine();
-        }
+static void ImplicitTypingIsStrongTyping()
+{
+    // Компилятор знает, что s имеет тип System.String.
+    var s = "Эта переменная может содержать только строковые данные!";
+    s = "Это нормально...";
 
-        static void DeclareImplicitVars()
-        {
-            // Неявно типизированные локальные переменные объявляются следующим образом:
-            // var имяПеременной = начальноеЗначение;
-            var myInt = 0;
-            var myBool = true;
-            var myString = "Time, marches on...";
+    // Можно обращаться к любому члену лежащего в основе типа.
+    string upper = s.ToUpper();
+}
 
-            // Вывести имена лежащих в основе типов.
-            Console.WriteLine("myInt является: {0}", myInt.GetType().Name);
-            Console.WriteLine("myBool является: {0}", myBool.GetType().Name);
-            Console.WriteLine("myString является: {0}", myString.GetType().Name);
-        }
+static void LinqQueryOverInts()
+{
+    int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
+    var subset = from i in numbers where i < 10 select i;
 
-        static int GetAnInt()
-        {
-            var retVal = 9;
-            return retVal;
-        }
+    Console.Write("Значения в subset: ");
+    foreach (var i in subset) Console.Write($"{i} ");
+    Console.WriteLine();
 
-        static void ImplicitTypingIsStrongTyping()
-        {
-            // Компилятор знает, что s имеет тип System.String.
-            var s = "Эта переменная может содержать только строковые данные!";
-            s = "Это нормально...";
+    // И к какому же типу относится subset?
+    Console.WriteLine("subset является: {0}", subset.GetType().Name);
+    Console.WriteLine("subset определён в: {0}", subset.GetType().Namespace);
+}
 
-            // Можно обращаться к любому члену лежащего в основе типа.
-            string upper = s.ToUpper();
-        }
+static void DeclarelmplicitNumerics()
+{
+    // Неявно типизированные числовые переменные.
+    var myUInt = 0u;
+    var myInt = 0;
+    var myLong = 0L;
+    var myDouble = 0.5;
+    var myFloat = 0.5F;
+    var myDecimal = 0.5M;
 
-        static void LinqQueryOverInts()
-        {
-            int[] numbers = { 10, 20, 30, 40, 1, 2, 3, 8 };
-            var subset = from i in numbers where i < 10 select i;
-
-            Console.Write("Значения в subset: ");
-            foreach (var i in subset) Console.Write($"{i} ");
-            Console.WriteLine();
-
-            // И к какому же типу относится subset?
-            Console.WriteLine("subset является: {0}", subset.GetType().Name);
-            Console.WriteLine("subset определён в: {0}", subset.GetType().Namespace);
-        }
-    }
+    // Вывод лежащего в основе типа.
+    Console.WriteLine($"myUInt: {myUInt.GetType().Name}");
+    Console.WriteLine($"myInt: {myInt.GetType().Name}");
+    Console.WriteLine($"myLong: {myLong.GetType().Name}");
+    Console.WriteLine($"myDouble: {myDouble.GetType().Name}");
+    Console.WriteLine($"myFloat: {myFloat.GetType().Name}");
+    Console.WriteLine($"myDecimal: {myDecimal.GetType().Name}");
+    Console.WriteLine();
 }
