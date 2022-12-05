41e6525b69aaeb93e9ac773214f6fc85a8cb780c
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2022-12-06 00:36:49 +0300

    Обновила целевую платформу до .NET 6.

---
+++ FunWithMethodOverloading/AddOperations.cs
@@ -0,0 +1,13 @@
+﻿namespace FunWithMethodOverloading
+{
+    public static class AddOperations
+    {
+        public static int Add(int x, int y) => x + y;
+
+        public static int Add(int x, int y, int z = 0) => x + (y * z);
+
+        public static double Add(double x, double y) => x + y;
+
+        public static long Add(long x, long y) => x + y;
+    }
+}
---
+++ FunWithMethodOverloading/Program.cs
@@ -0,0 +1,14 @@
+﻿using static FunWithMethodOverloading.AddOperations;
+
+Console.WriteLine("***** Перегрузка методов *****");
+
+// Вызов int-версии Add().
+Console.WriteLine(Add(10, 10));
+
+// Вызов long-версии Add() с использованием нового разделителя групп цифр.
+Console.WriteLine(Add(900_000_000_000, 900_000_000_000));
+
+// Вызов double-версии Add().
+Console.WriteLine(Add(4.3, 4.4));
+
+Console.ReadLine();
