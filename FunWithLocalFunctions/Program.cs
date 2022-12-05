1fd0e0ba61037b3f0021e05374e02f92bebd6e5d
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2022-12-05 23:50:02 +0300

    Обновила целевую платформу до .NET 6.

--- FunWithLocalFunctions/Program.cs
+++ FunWithLocalFunctions/Program.cs
@@ -1,29 +1,42 @@
-﻿using System;
+﻿Console.WriteLine("Локальные функции");
 
-namespace FunWithLocalFunctions
+// Вызвать оригинальную версию Add().
+Console.WriteLine(Add(10, 10));
+
+// Вызвать обновлённую версию Add().
+Console.WriteLine(AddWrapper(10, 10));
+
+Console.WriteLine(AddWrapperWithSideEffect(10, 10));
+
+// Now it's all better :-)
+Console.WriteLine(AddWrapperWithStatic(10, 10));
+Console.ReadLine();
+
+// Выполнить здесь проверку достоверности.
+static int Add(int x, int y) => x + y;
+
+static int AddWrapper(int x, int y)
 {
-    class Program
+    // Выполнить здесь проверку достоверности.
+    return Add();
+    int Add() => x + y;
+}
+
+static int AddWrapperWithSideEffect(int x, int y)
+{
+    // Здесь должна выполняться какая-то проверка достоверности.
+    return Add();
+
+    int Add()
     {
-        static void Main()
-        {
-            Console.WriteLine("Локальные функции");
-
-            // Вызвать оригинальную версию Add().
-            Console.WriteLine(Add(10, 10));
-
-            // Вызвать обновлённую версию Add().
-            Console.WriteLine(AddWrapper(10, 10));
-            Console.ReadLine();
-        }
-
-        // Выполнить здесь проверку достоверности.
-        static int Add(int x, int y) => x + y;
-
-        static int AddWrapper(int x, int y)
-        {
-            // Выполнить здесь проверку достоверности.
-            return Add();
-            int Add() => x + y;
-        }
+        x += 1;
+        return x + y;
     }
+}
+static int AddWrapperWithStatic(int x, int y)
+{
+    // Здесь должна выполняться какая-то проверка достоверности.
+    return Add(x, y);
+
+    static int Add(int x, int y) => x + y;
 }
