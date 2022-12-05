254ca97f5d2d23b586539364be5acd5402a7051e
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2022-12-06 01:06:49 +0300

    Добавила проект FunWithBitwiseOperations.

---
+++ FunWithBitwiseOperations/ContactPreferenceEnum.cs
@@ -0,0 +1,11 @@
+﻿namespace FunWithBitwiseOperations
+{
+    [Flags]
+    public enum ContactPreferenceEnum
+    {
+        None = 1,
+        Email = 2,
+        Phone = 4,
+        Text = 8
+    }
+}
---
+++ FunWithBitwiseOperations/Program.cs
@@ -0,0 +1,21 @@
+﻿using FunWithBitwiseOperations;
+
+Console.WriteLine("***** Перечисления, флаги и побитовые операции *****");
+
+Console.WriteLine($"6 & 4 = {6 & 4} | {Convert.ToString(6 & 4, 2)}");
+Console.WriteLine($"6 | 4 = {6 | 4} | {Convert.ToString(6 | 4, 2)}");
+Console.WriteLine($"6 ^ 4 = {6 ^ 4} | {Convert.ToString(6 ^ 4, 2)}");
+Console.WriteLine($"6 << 1 = {6 << 1} | {Convert.ToString(6 << 1, 2)}");
+Console.WriteLine($"6 >> 1 = {6 >> 1} | {Convert.ToString(6 >> 1, 2)}");
+Console.WriteLine($"~6 = {~6} | {Convert.ToString(~6, 2)}");
+Console.WriteLine($"Int.MaxValue {Convert.ToString(int.MaxValue, 2)}");
+Console.WriteLine();
+
+ContactPreferenceEnum emailAndPhone = ContactPreferenceEnum.Email | ContactPreferenceEnum.Phone;
+
+Console.WriteLine($"None? {(emailAndPhone | ContactPreferenceEnum.None) == emailAndPhone}");
+Console.WriteLine($"Email? {(emailAndPhone | ContactPreferenceEnum.Email) == emailAndPhone}");
+Console.WriteLine($"Phone? {(emailAndPhone | ContactPreferenceEnum.Phone) == emailAndPhone}");
+Console.WriteLine($"Text? {(emailAndPhone | ContactPreferenceEnum.Text) == emailAndPhone}");
+
+Console.ReadLine();
