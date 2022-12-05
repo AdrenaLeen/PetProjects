db03ecc2c5f2c4fdab4b9bc6ff573cf2a0cc6609
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2022-12-06 00:49:04 +0300

    Обновила целевую платформу до .NET 6.

--- FunWithEnums/Program.cs
+++ FunWithEnums/Program.cs
@@ -1,85 +1,76 @@
-﻿using System;
+﻿Console.WriteLine("**** Тип enum *****");
 
-namespace FunWithEnums
+// Создать переменную типа EmpType.
+EmpTypeEnum emp = EmpTypeEnum.Contractor;
+AskForBonus(emp);
+Console.WriteLine();
+
+// Вывести тип хранилища для значений перечисления.
+Console.WriteLine($"EmpType использует {Enum.GetUnderlyingType(emp.GetType())} для хранения");
+Console.WriteLine();
+
+// На этот раз для получения информации о типе используется операция typeof.
+Console.WriteLine($"EmpType использует {Enum.GetUnderlyingType(typeof(EmpTypeEnum))} для хранения");
+Console.WriteLine();
+
+// Выводит строку "emp является Contractor".
+Console.WriteLine($"emp является {emp}");
+Console.WriteLine();
+
+// Выводит строку "Contractor = 100".
+Console.WriteLine($"{emp} = {(byte)emp}");
+Console.WriteLine();
+
+EmpTypeEnum e2 = EmpTypeEnum.Contractor;
+// Эти типы являются перечислениями из пространства имен System.
+DayOfWeek day = DayOfWeek.Monday;
+ConsoleColor cc = ConsoleColor.Gray;
+EvaluateEnum(e2);
+EvaluateEnum(day);
+EvaluateEnum(cc);
+
+Console.ReadLine();
+
+// Перечисления как параметры
+static void AskForBonus(EmpTypeEnum e)
 {
-    // Специальное перечисление.
-    // Начать нумерацию со значения 102.
-    // Значения элементов в перечислении не обязательно должны быть последовательными!
-    // На этот раз для элементов EmpType используется тип byte.
-    enum EmpType : byte
+    switch (e)
     {
-        Manager = 10,
-        Grunt = 1,
-        Contractor = 100,
-        VicePresident = 9
+        case EmpTypeEnum.Manager:
+            Console.WriteLine("Не желаете ли взамен фондовые опционы?");
+            break;
+        case EmpTypeEnum.Grunt:
+            Console.WriteLine("Вы, должно быть, шутите...");
+            break;
+        case EmpTypeEnum.Contractor:
+            Console.WriteLine("Вы уже получаете вполне достаточно...");
+            break;
+        case EmpTypeEnum.VicePresident:
+            Console.WriteLine("Очень хорошо, сэр!");
+            break;
     }
+}
 
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("**** Тип enum *****");
+// Этот метод выводит детали любого перечисления.
+static void EvaluateEnum(Enum e)
+{
+    Console.WriteLine($"=> Информация о {e.GetType().Name}");
+    Console.WriteLine($"Базовый тип хранения: {Enum.GetUnderlyingType(e.GetType())}");
 
-            // Создать переменную типа EmpType.
-            EmpType emp = EmpType.Contractor;
-            AskForBonus(emp);
+    // Получить все пары "имя-значение" для входного параметра.
+    Array enumData = Enum.GetValues(e.GetType());
+    Console.WriteLine($"У этого перечисления {enumData.Length} членов.");
 
-            // Вывести тип хранилища для значений перечисления.
-            Console.WriteLine($"EmpType использует {Enum.GetUnderlyingType(emp.GetType())} для хранения");
+    // Вывести строковое имя и ассоциированное значение, используя флаг формата D.
+    for (int i = 0; i < enumData.Length; i++) Console.WriteLine("Имя: {0}, значение: {0:D}", enumData.GetValue(i));
+    Console.WriteLine();
+}
 
-            // На этот раз для получения информации о типе используется операция typeof.
-            Console.WriteLine($"EmpType использует {Enum.GetUnderlyingType(typeof(EmpType))} для хранения");
-
-            // Выводит строку "emp является Contractor".
-            Console.WriteLine($"emp является {emp.ToString()}");
-
-            // Выводит строку "Contractor = 100".
-            Console.WriteLine($"{emp.ToString()} = {(byte)emp}");
-
-            EmpType e2 = EmpType.Contractor;
-            // Эти типы являются перечислениями из пространства имён System.
-            DayOfWeek day = DayOfWeek.Monday;
-            ConsoleColor cc = ConsoleColor.Gray;
-            EvaluateEnum(e2);
-            EvaluateEnum(day);
-            EvaluateEnum(cc);
-
-            Console.ReadLine();
-        }
-
-        // Перечисления как параметры
-        static void AskForBonus(EmpType e)
-        {
-            switch (e)
-            {
-                case EmpType.Manager:
-                    Console.WriteLine("Не желаете ли взамен фондовые опционы?");
-                    break;
-                case EmpType.Grunt:
-                    Console.WriteLine("Вы, должно быть, шутите...");
-                    break;
-                case EmpType.Contractor:
-                    Console.WriteLine("Вы уже получаете вполне достаточно...");
-                    break;
-                case EmpType.VicePresident:
-                    Console.WriteLine("Очень хорошо, сэр!");
-                    break;
-            }
-        }
-
-        // Этот метод выводит детали любого перечисления.
-        static void EvaluateEnum(Enum e)
-        {
-            Console.WriteLine($"=> Информация о {e.GetType().Name}");
-            Console.WriteLine($"Базовый тип хранения: {Enum.GetUnderlyingType(e.GetType())}");
-
-            // Получить все пары "имя-значение" для входного параметра.
-            Array enumData = Enum.GetValues(e.GetType());
-            Console.WriteLine($"У этого перечисления {enumData.Length} членов.");
-
-            // Вывести строковое имя и ассоциированное значение, используя флаг формата D.
-            for (int i = 0; i < enumData.Length; i++) Console.WriteLine("Имя: {0}, значение: {0:D}", enumData.GetValue(i));
-            Console.WriteLine();
-        }
-    }
+// Специальное перечисление.
+enum EmpTypeEnum : byte
+{
+    Manager = 10,
+    Grunt = 1,
+    Contractor = 100,
+    VicePresident = 9
 }
