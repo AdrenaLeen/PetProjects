f96e9f9abbc94dcfa50fdd3563d7693cca1b87dd
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2022-12-10 18:56:07 +0300

    Добавила проект FunWithNullableReferenceTypes.

---
+++ FunWithNullableReferenceTypes/Program.cs
@@ -0,0 +1,17 @@
+﻿using FunWithNullableReferenceTypes;
+
+string? nullableString = null;
+TestClass? myNullableClass = null;
+
+// Предупреждение CS8600 Преобразование литерала null или возможного значения null в тип, не допускающий null
+TestClass myNonNullableClass = myNullableClass;
+
+#nullable disable
+TestClass anotherNullableClass = null;
+
+//Предупреждение CS8632 Заметка для ссылочных типов, допускающих значение null, должна использоваться только в коде внутри аннотаций #nullable
+TestClass? badDefinition = null;
+
+//Предупреждение CS8632 Заметка для ссылочных типов, допускающих значение null, должна использоваться только в коде внутри аннотаций #nullable
+string? anotherNullableString = null;
+#nullable restore
