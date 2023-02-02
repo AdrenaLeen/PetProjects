90899fe50aef98da4e3095a11c5879a16863a5f6
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-02-02 22:35:38 +0300

    Обновила целевую среду до .NET 6.

--- ObjectOverrides/Person.cs
+++ ObjectOverrides/Person.cs
@@ -4,23 +4,24 @@
     // Предположим, что имеется свойство SSN.
     class Person
     {
-        public string FirstName { get; set; } = "";
-        public string LastName { get; set; } = "";
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public int Age { get; set; }
-        public string SSN { get; set; } = "";
+        public string SSN { get; set; } = string.Empty;
 
-        public Person(string fName, string lName, int personAge)
+        public Person(string fName, string lName, int personAge, string ssn)
         {
             FirstName = fName;
             LastName = lName;
             Age = personAge;
+            SSN = ssn;
         }
         public Person() { }
 
         public override string ToString() => $"[Имя: {FirstName}; Фамилия: {LastName}; Возраст: {Age}]";
 
         // Больше нет необходимости приводить obj с типу Person, т.к. у всех типов имеется метод ToString().
-        public override bool Equals(object obj) => obj.ToString() == ToString();
+        public override bool Equals(object? obj) => obj?.ToString() == ToString();
 
         // Возвратить хеш-код на основе значения, возвращаемого методом ToString() для объекта Person.
         public override int GetHashCode() => SSN.GetHashCode();
--- ObjectOverrides/Program.cs
+++ ObjectOverrides/Program.cs
@@ -1,57 +1,50 @@
-﻿using System;
+﻿using ObjectOverrides;
 
-namespace ObjectOverrides
+Console.WriteLine("***** Тип System.Object *****");
+var p1 = new Person();
+
+// Использовать унаследованные члены System.Object.
+Console.WriteLine($"ToString: {p1}");
+Console.WriteLine($"Хэш: {p1.GetHashCode()}");
+Console.WriteLine($"Тип: {p1.GetType()}");
+
+// Создать другие ссылки на p1.
+Person p2 = p1;
+object o = p2;
+// Указывают ли ссылки на один и тот же объект в памяти?
+if (o.Equals(p1) && p2.Equals(o)) Console.WriteLine("Одинаковые сущности!");
+
+// ПРИМЕЧАНИЕ: эти объекты идентичны и предназначены для тестирования методов Equals() и GetHashCode().
+var p3 = new Person("Гомер", "Симпсон", 50, "111-11-1111");
+var p4 = new Person("Гомер", "Симпсон", 50, "111-11-1111");
+
+// Получить строковые версии объектов.
+Console.WriteLine($"p3.ToString() = {p3}");
+Console.WriteLine($"p4.ToString() = {p4}");
+
+// Протестировать переопределённый метод Equals().
+Console.WriteLine($"p3 = p4?: {p3.Equals(p4)}");
+
+// Проверить хеш-коды.
+Console.WriteLine($"Одинаковые хеш-коды?: {p3.GetHashCode() == p4.GetHashCode()}");
+Console.WriteLine();
+
+// Изменить возраст p4 и протестировать снова.
+p4.Age = 45;
+Console.WriteLine($"p3.ToString() = {p3}");
+Console.WriteLine($"p4.ToString() = {p4}");
+Console.WriteLine($"p3 = p4?: {p3.Equals(p4)}");
+Console.WriteLine($"Одинаковые хеш-коды?: {p3.GetHashCode() == p4.GetHashCode()}");
+
+StaticMembersOfObject();
+
+Console.ReadLine();
+
+static void StaticMembersOfObject()
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Тип System.Object *****");
-            Person p1 = new Person();
-            // Использовать унаследованные члены System.Object.
-            Console.WriteLine($"ToString: {p1.ToString()}");
-            Console.WriteLine($"Хэш: {p1.GetHashCode()}");
-            Console.WriteLine($"Тип: {p1.GetType()}");
-            // Создать другие ссылки на p1.
-            Person p2 = p1;
-            object o = p2;
-            // Указывают ли ссылки на один и тот же объект в памяти?
-            if (o.Equals(p1) && p2.Equals(o)) Console.WriteLine("Одинаковые сущности!");
-
-            // ПРИМЕЧАНИЕ: эти объекты идентичны и предназначены для тестирования методов Equals() и GetHashCode().
-            Person p3 = new Person("Гомер", "Симпсон", 50);
-            Person p4 = new Person("Гомер", "Симпсон", 50);
-
-            // Получить строковые версии объектов.
-            Console.WriteLine($"p3.ToString() = {p3.ToString()}");
-            Console.WriteLine($"p4.ToString() = {p4.ToString()}");
-
-            // Протестировать переопределённый метод Equals().
-            Console.WriteLine($"p3 = p4?: {p3.Equals(p4)}");
-
-            // Проверить хеш-коды.
-            Console.WriteLine($"Одинаковые хеш-коды?: {p3.GetHashCode() == p4.GetHashCode()}");
-            Console.WriteLine();
-
-            // Изменить возраст p4 и протестировать снова.
-            p4.Age = 45;
-            Console.WriteLine($"p3.ToString() = {p3.ToString()}");
-            Console.WriteLine($"p4.ToString() = {p4.ToString()}");
-            Console.WriteLine($"p3 = p4?: {p3.Equals(p4)}");
-            Console.WriteLine($"Одинаковые хеш-коды?: {p3.GetHashCode() == p4.GetHashCode()}");
-
-            StaticMembersOfObject();
-
-            Console.ReadLine();
-        }
-
-        static void StaticMembersOfObject()
-        {
-            // Статические члены System.Object.
-            Person p3 = new Person("Салли", "Джонс", 4);
-            Person p4 = new Person("Салли", "Джонс", 4);
-            Console.WriteLine($"У P3 и P4 одинаковое состояние: {Equals(p3, p4)}");
-            Console.WriteLine($"P3 и P4 указывают на один и тот же объект: {ReferenceEquals(p3, p4)}");
-        }
-    }
+    // Статические члены System.Object.
+    var p3 = new Person("Салли", "Джонс", 4, string.Empty);
+    var p4 = new Person("Салли", "Джонс", 4, string.Empty);
+    Console.WriteLine($"У P3 и P4 одинаковое состояние: {Equals(p3, p4)}");
+    Console.WriteLine($"P3 и P4 указывают на один и тот же объект: {ReferenceEquals(p3, p4)}");
 }
