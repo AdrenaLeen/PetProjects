a2b4b65cdcb326d7b6f02f49d62cef181d81d7b8
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-04-13 23:59:12 +0300

    Обновила целевую среду до .NET 6.

--- FunWithObservableCollection/Program.cs
+++ FunWithObservableCollection/Program.cs
@@ -1,48 +1,44 @@
-﻿using System;
+﻿using FunWithObservableCollection;
 using System.Collections.ObjectModel;
 using System.Collections.Specialized;
 
-namespace FunWithObservableCollection
+var people = new ObservableCollection<Person>()
 {
-    class Program
+    new Person{ FirstName = "Питэр", LastName = "Мерфи", Age = 52 },
+    new Person{ FirstName = "Кевин", LastName = "Кей", Age = 48 },
+};
+
+// Привязаться к событию CollectionChanged.
+people.CollectionChanged += People_CollectionChanged;
+people.Add(new Person("Фрэд", "Смит", 32));
+people.RemoveAt(0);
+
+Console.ReadLine();
+
+static void People_CollectionChanged(object? sender, NotifyCollectionChangedEventArgs e)
+{
+    // Выяснить действие, которое привело к генерации события.
+    Console.WriteLine($"Событие для этого события: {e.Action}");
+
+    // Было что-то удалено.
+    if (e.Action == NotifyCollectionChangedAction.Remove)
     {
-        static void Main()
+        Console.WriteLine("Здесь старые элементы:");
+        if (e.OldItems != null)
         {
-            // Сделать коллекцию наблюдаемой и добавить в неё несколько объектов Person.
-            ObservableCollection<Person> people = new ObservableCollection<Person>()
-            {
-                new Person{ FirstName = "Питэр", LastName = "Мёрфи", Age = 52 },
-                new Person{ FirstName = "Кевин", LastName = "Кей", Age = 48 },
-            };
-
-            // Привязаться к событию CollectionChanged.
-            people.CollectionChanged += People_CollectionChanged;
-            people.Add(new Person("Фрэд", "Смит", 32));
-            people.RemoveAt(0);
-
-            Console.ReadLine();
+            foreach (Person p in e.OldItems) Console.WriteLine(p);
         }
+        Console.WriteLine();
+    }
 
-        static void People_CollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    // Было что-то добавлено.
+    if (e.Action == NotifyCollectionChangedAction.Add)
+    {
+        // Теперь вывести новые элементы, которые были вставлены.
+        Console.WriteLine("Здесь новые элементы:");
+        if (e.NewItems != null)
         {
-            // Выяснить действие, которое привело к генерации события.
-            Console.WriteLine($"Событие для этого события: {e.Action}");
-
-            // Было что-то удалено.
-            if (e.Action == NotifyCollectionChangedAction.Remove)
-            {
-                Console.WriteLine("Здесь старые элементы:");
-                foreach (Person p in e.OldItems) Console.WriteLine(p);
-                Console.WriteLine();
-            }
-
-            // Было что-то добавлено.
-            if (e.Action == NotifyCollectionChangedAction.Add)
-            {
-                // Теперь вывести новые элементы, которые были вставлены.
-                Console.WriteLine("Здесь новые элементы:");
-                foreach (Person p in e.NewItems) Console.WriteLine(p);
-            }
+            foreach (Person p in e.NewItems) Console.WriteLine(p);
         }
     }
 }
