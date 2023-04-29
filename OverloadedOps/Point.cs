851ad1bdc2556284fa11a06f9819dee06c22e463
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-04-29 14:16:41 +0300

    Обновила целевую среду до .NET 6.

--- OverloadedOps/Point.cs
+++ OverloadedOps/Point.cs
@@ -1,8 +1,7 @@
 ﻿namespace OverloadedOps
 {
-    // Простой будничный класс C#.
     // Более интеллектуальный тип Point.
-    class Point
+    public class Point : IComparable<Point>
     {
         public int X { get; set; }
         public int Y { get; set; }
@@ -15,35 +14,35 @@
 
         public override string ToString() => $"[{X}, {Y}]";
 
-        public override bool Equals(object o) => o.ToString() == ToString();
-
-        public override int GetHashCode() => ToString().GetHashCode();
-
         // Перегруженная операция +
-        public static Point operator +(Point p1, Point p2) => new Point(p1.X + p2.X, p1.Y + p2.Y);
+        public static Point operator +(Point p1, Point p2) => new(p1.X + p2.X, p1.Y + p2.Y);
 
         // Перегруженная операция -
-        public static Point operator -(Point p1, Point p2) => new Point(p1.X - p2.X, p1.Y - p2.Y);
+        public static Point operator -(Point p1, Point p2) => new(p1.X - p2.X, p1.Y - p2.Y);
 
-        public static Point operator +(Point p1, int change) => new Point(p1.X + change, p1.Y + change);
+        public static Point operator +(Point p1, int change) => new(p1.X + change, p1.Y + change);
 
-        public static Point operator +(int change, Point p1) => new Point(p1.X + change, p1.Y + change);
+        public static Point operator +(int change, Point p1) => new(p1.X + change, p1.Y + change);
 
         // Добавить 1 к значениям X/Y входного объекта Point.
-        public static Point operator ++(Point p1) => new Point(p1.X + 1, p1.Y + 1);
+        public static Point operator ++(Point p1) => new(p1.X + 1, p1.Y + 1);
 
         // Вычесть 1 из значений X/Y входного объекта Point.
-        public static Point operator --(Point p1) => new Point(p1.X - 1, p1.Y - 1);
+        public static Point operator --(Point p1) => new(p1.X - 1, p1.Y - 1);
+
+        public override bool Equals(object? o) => o?.ToString() == ToString();
+
+        public override int GetHashCode() => ToString().GetHashCode();
 
         // Теперь перегрузить операции == и !=.
         public static bool operator ==(Point p1, Point p2) => p1.Equals(p2);
 
         public static bool operator !=(Point p1, Point p2) => !p1.Equals(p2);
 
-        public int CompareTo(Point other)
+        public int CompareTo(Point? other)
         {
-            if (X > other.X && Y > other.Y) return 1;
-            if (X < other.X && Y < other.Y) return -1;
+            if (X > other?.X && Y > other.Y) return 1;
+            if (X < other?.X && Y < other.Y) return -1;
             else return 0;
         }
 
--- OverloadedOps/Program.cs
+++ OverloadedOps/Program.cs
@@ -1,69 +1,60 @@
-﻿using System;
+﻿using OverloadedOps;
 
-namespace OverloadedOps
-{
-    class Program
-    {
-        // Сложение и вычитание двух точек?
-        // Перегрузка бинарных операций автоматически обеспечивает перегрузку сокращённых операций.
-        static void Main()
-        {
-            Console.WriteLine("***** Перегрузка операторов *****");
+Console.WriteLine("***** Перегрузка операторов *****");
 
-            // Создать две точки.
-            Point ptOne = new Point(100, 100);
-            Point ptTwo = new Point(40, 40);
-            Console.WriteLine($"ptOne = {ptOne}");
-            Console.WriteLine($"ptTwo = {ptTwo}");
+// Создать две точки.
+var ptOne = new Point(100, 100);
+var ptTwo = new Point(40, 40);
+Console.WriteLine($"ptOne = {ptOne}");
+Console.WriteLine($"ptTwo = {ptTwo}");
 
-            // Сложить две точки, чтобы получить большую?
-            Console.WriteLine($"ptOne + ptTwo: {ptOne + ptTwo}");
+// Сложить две точки, чтобы получить большую?
+Console.WriteLine($"ptOne + ptTwo: {ptOne + ptTwo}");
 
-            // Вычесть одну точку из другой, чтобы получить меньшую?
-            Console.WriteLine("ptOne - ptTwo: {0} ", ptOne - ptTwo);
+// Вычесть одну точку из другой, чтобы получить меньшую?
+Console.WriteLine("ptOne - ptTwo: {0} ", ptOne - ptTwo);
 
-            // Выводит [110, 110]
-            Point biggerPoint = ptOne + 10;
-            Console.WriteLine($"ptOne + 10 = {biggerPoint}");
+// Выводит [110, 110]
+Point biggerPoint = ptOne + 10;
+Console.WriteLine($"ptOne + 10 = {biggerPoint}");
 
-            // Выводит [120, 120]
-            Console.WriteLine($"10 + biggerPoint = {10 + biggerPoint}");
-            Console.WriteLine();
+// Выводит [120, 120]
+Console.WriteLine($"10 + biggerPoint = {10 + biggerPoint}");
+Console.WriteLine();
 
-            // Операция += перегружена автоматически.
-            Point ptThree = new Point(90, 5);
-            Console.WriteLine($"ptThree = {ptThree}");
-            Console.WriteLine($"ptThree += ptTwo: {ptThree += ptTwo}");
+// Операция += перегружена автоматически.
+var ptThree = new Point(90, 5);
+Console.WriteLine($"ptThree = {ptThree}");
+Console.WriteLine($"ptThree += ptTwo: {ptThree += ptTwo}");
 
-            // Операция -= перегружена автоматически.
-            Point ptFour = new Point(0, 500);
-            Console.WriteLine($"ptFour = {ptFour}");
-            Console.WriteLine($"ptFour -= ptThree: {ptFour -= ptThree}");
-            Console.WriteLine();
+// Операция -= перегружена автоматически.
+var ptFour = new Point(0, 500);
+Console.WriteLine($"ptFour = {ptFour}");
+Console.WriteLine($"ptFour -= ptThree: {ptFour -= ptThree}");
+Console.WriteLine();
 
-            // Применение унарных операций ++ и -- к объекту Point.
-            Point ptFive = new Point(1, 1);
-            // [2, 2]
-            Console.WriteLine($"++ptFive = {++ptFive}");
-            // [1, 1]
-            Console.WriteLine($"--ptFive = {--ptFive}");
+// Применение унарных операций ++ и -- к объекту Point.
+var ptFive = new Point(1, 1);
+// [2, 2]
+Console.WriteLine($"++ptFive = {++ptFive}");
+// [1, 1]
+Console.WriteLine($"--ptFive = {--ptFive}");
 
-            // Применение тех же операций в виде постфиксного инкремента/декремента.
-            Point ptSix = new Point(20, 20);
-            // [20, 20]
-            Console.WriteLine($"ptSix++ = {ptSix++}");
-            // [21, 21]
-            Console.WriteLine($"ptSix-- = {ptSix--}");
-            Console.WriteLine();
+// Применение тех же операций в виде постфиксного инкремента/декремента.
+var ptSix = new Point(20, 20);
+// [20, 20]
+Console.WriteLine($"ptSix++ = {ptSix++}");
+// [21, 21]
+Console.WriteLine($"ptSix-- = {ptSix--}");
+Console.WriteLine();
 
-            Console.WriteLine($"ptOne == ptTwo: {ptOne == ptTwo}");
-            Console.WriteLine($"ptOne != ptTwo: {ptOne != ptTwo}");
-            Console.WriteLine();
+// Использование перегруженных операций эквивалентности.
+Console.WriteLine($"ptOne == ptTwo: {ptOne == ptTwo}");
+Console.WriteLine($"ptOne != ptTwo: {ptOne != ptTwo}");
+Console.WriteLine();
 
-            Console.WriteLine($"ptOne < ptTwo: {ptOne < ptTwo}");
-            Console.WriteLine($"ptOne > ptTwo: {ptOne > ptTwo}");
+// Использование перегруженных операций < и >.
+Console.WriteLine($"ptOne < ptTwo: {ptOne < ptTwo}");
+Console.WriteLine($"ptOne > ptTwo: {ptOne > ptTwo}");
 
-            Console.ReadLine();
-        }
-    }
-}
+Console.ReadLine();
