9375c6c7a3ea0b43878b64f9c48597d5cf04bace
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-05-16 23:54:47 +0300

    Обновила целевую платформу до .NET 6.

--- ActionAndFuncDelegates/Program.cs
+++ ActionAndFuncDelegates/Program.cs
@@ -1,44 +1,33 @@
-﻿using System;
+﻿Console.WriteLine("***** Делегаты Action<> and Func<> *****");
 
-namespace ActionAndFuncDelegates
+// Использовать делегат Action<> для указания на DisplayMessage.
+Action<string, ConsoleColor, int> actionTarget = DisplayMessage;
+actionTarget("Сообщение о действии!", ConsoleColor.Yellow, 5);
+
+Func<int, int, int> funcTarget = Add;
+int result = funcTarget.Invoke(40, 40);
+Console.WriteLine($"40 + 40 = {result}");
+
+Func<int, int, string> funcTarget2 = SumToString;
+string sum = funcTarget2(90, 300);
+Console.WriteLine(sum);
+
+Console.ReadLine();
+
+// Это цель для делегата Action<>.
+static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Делегаты Action<> and Func<> *****");
+    // Установить цвет текста консоли.
+    ConsoleColor previous = Console.ForegroundColor;
+    Console.ForegroundColor = txtColor;
 
-            // Использовать делегат Action<> для указания на DisplayMessage.
-            Action<string, ConsoleColor, int> actionTarget = new Action<string, ConsoleColor, int>(DisplayMessage);
-            actionTarget("Сообщение о действии!", ConsoleColor.Yellow, 5);
+    for (int i = 0; i < printCount; i++) Console.WriteLine(msg);
 
-            Func<int, int, int> funcTarget = Add;
-            int result = funcTarget.Invoke(40, 40);
-            Console.WriteLine($"40 + 40 = {result}");
-
-            Func<int, int, string> funcTarget2 = SumToString;
-            string sum = funcTarget2(90, 300);
-            Console.WriteLine(sum);
-
-            Console.ReadLine();
-        }
-
-        // Это цель для делегата Action<>.
-        static void DisplayMessage(string msg, ConsoleColor txtColor, int printCount)
-        {
-            // Установить цвет текста консоли.
-            ConsoleColor previous = Console.ForegroundColor;
-            Console.ForegroundColor = txtColor;
-
-            for (int i = 0; i < printCount; i++) Console.WriteLine(msg);
-
-            // Восстановить цвет.
-            Console.ForegroundColor = previous;
-        }
-
-        // Цель для делегата Func<>.
-        static int Add(int x, int y) => x + y;
-
-        static string SumToString(int x, int y) => (x + y).ToString();
-    }
+    // Восстановить цвет.
+    Console.ForegroundColor = previous;
 }
+
+// Цель для делегата Func<>.
+static int Add(int x, int y) => x + y;
+
+static string SumToString(int x, int y) => (x + y).ToString();
