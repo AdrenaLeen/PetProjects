79503f60974ccce2ddb465965bef503f57865633
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-06-29 12:02:33 +0300

    Изоляция сборок с помощью контекстов загрузки приложений.

---
+++ ClassLibrary/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace ClassLibrary
+{
+    public class Car
+    {
+        public string PetName { get; set; } = string.Empty;
+        public string Make { get; set; } = string.Empty;
+        public int Speed { get; set; }
+    }
+}
--- DefaultAppDomainApp/Program.cs
+++ DefaultAppDomainApp/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System.Reflection;
+using System.Runtime.Loader;
 
 Console.WriteLine("***** Стандартный домен приложения *****");
-InitDAD();
 DisplayDADStats();
 ListAllAssembliesInAppDomain();
+LoadAdditionalAssembliesDifferentContexts();
+LoadAdditionalAssembliesSameContext();
 Console.ReadLine();
 
 static void DisplayDADStats()
@@ -16,6 +18,10 @@ static void DisplayDADStats()
     Console.WriteLine($"Идентификатор домена в этом процессе: {defaultAD.Id}");
     Console.WriteLine($"Является ли стандартным доменом?: {defaultAD.IsDefaultAppDomain()}");
     Console.WriteLine($"Базовый каталог этого домена: {defaultAD.BaseDirectory}");
+    Console.WriteLine("Информация о настройке этого домена:");
+    Console.WriteLine($"Базовый каталог приложения: {defaultAD.SetupInformation.ApplicationBase}");
+    Console.WriteLine($"Целевая платформа: {defaultAD.SetupInformation.TargetFrameworkName}");
+    Console.WriteLine();
 }
 
 static void ListAllAssembliesInAppDomain()
@@ -24,22 +30,41 @@ static void ListAllAssembliesInAppDomain()
     AppDomain defaultAD = AppDomain.CurrentDomain;
 
     // Извлечь все сборки, загруженные в стандартный домен приложения.
-    IOrderedEnumerable<Assembly> loadedAssemblies = from a in defaultAD.GetAssemblies()
-                                                    orderby a.GetName().Name
-                                                    select a;
+    IOrderedEnumerable<Assembly> loadedAssemblies = defaultAD.GetAssemblies().OrderBy(x => x.GetName().Name);
 
     Console.WriteLine($"***** Здесь сборки, загруженные в {defaultAD.FriendlyName} *****");
-    foreach (Assembly a in loadedAssemblies)
-    {
-        Console.WriteLine($"-> Имя: {a.GetName().Name}");
-        Console.WriteLine($"-> Версия: {a.GetName().Version}");
-        Console.WriteLine();
-    }
+    foreach (Assembly a in loadedAssemblies) Console.WriteLine($"-> Имя, версия: {a.GetName().Name}:{a.GetName().Version}");
+    Console.WriteLine();
 }
 
-static void InitDAD()
+static void LoadAdditionalAssembliesDifferentContexts()
 {
-    // Эта логика будет выводить имя любой сборки, загруженной в домен приложения после его создания.
-    AppDomain defaultAD = AppDomain.CurrentDomain;
-    defaultAD.AssemblyLoad += (o, s) => Console.WriteLine($"{s.LoadedAssembly.GetName().Name} загружена!");
+    var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ClassLibrary.dll");
+    var lc1 = new AssemblyLoadContext("NewContext1", false);
+    var cl1 = lc1.LoadFromAssemblyPath(path);
+    var c1 = cl1.CreateInstance("ClassLibrary.Car");
+    var lc2 = new AssemblyLoadContext("NewContext2", false);
+    var cl2 = lc2.LoadFromAssemblyPath(path);
+    var c2 = cl2.CreateInstance("ClassLibrary.Car");
+    Console.WriteLine("***** Загрузка дополнительных сборок в различных контекстах *****");
+    Console.WriteLine($"Assembly1 Equals(Assembly2) {cl1.Equals(cl2)}");
+    Console.WriteLine($"Assembly1 == Assembly2 {cl1 == cl2}");
+    Console.WriteLine($"Class1.Equals(Class2) {c1?.Equals(c2)}");
+    Console.WriteLine($"Class1 == Class2 {c1 == c2}");
+    Console.WriteLine();
+}
+
+static void LoadAdditionalAssembliesSameContext()
+{
+    var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "ClassLibrary.dll");
+    var lc1 = new AssemblyLoadContext(null, false);
+    var cl1 = lc1.LoadFromAssemblyPath(path);
+    var c1 = cl1.CreateInstance("ClassLibrary.Car");
+    var cl2 = lc1.LoadFromAssemblyPath(path);
+    var c2 = cl2.CreateInstance("ClassLibrary.Car");
+    Console.WriteLine("***** Загрузка дополнительных сборок в различных контекстах *****");
+    Console.WriteLine($"Assembly1 Equals(Assembly2) {cl1.Equals(cl2)}");
+    Console.WriteLine($"Assembly1 == Assembly2 {cl1 == cl2}");
+    Console.WriteLine($"Class1.Equals(Class2) {c1?.Equals(c2)}");
+    Console.WriteLine($"Class1 == Class2 {c1 == c2}");
 }
