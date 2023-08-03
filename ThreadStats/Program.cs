b55b684588f4ab02b5c9629dc793ff0f7c610533
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-08-03 10:37:49 +0300

    Обновила целевую среду до .NET 6.

--- ThreadStats/Program.cs
+++ ThreadStats/Program.cs
@@ -1,28 +1,13 @@
-﻿using System;
-using System.Threading;
+﻿Console.WriteLine("***** Статистика о текущем потоке *****");
 
-namespace ThreadStats
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Статистика о текущем потоке *****");
+// Получить имя текущего потока.
+Thread primaryThread = Thread.CurrentThread;
+primaryThread.Name = "ThePrimaryThread";
 
-            // Получить имя текущего потока.
-            Thread primaryThread = Thread.CurrentThread;
-            primaryThread.Name = "ThePrimaryThread";
-
-            // Показать детали обслуживающего домена приложения и контекста.
-            Console.WriteLine($"Имя текущего домена приложения: {Thread.GetDomain().FriendlyName}");
-            Console.WriteLine($"Идентификатор текущего контекста: {Thread.CurrentContext.ContextID}");
-
-            // Вывести некоторую статистику о текущем потоке.
-            Console.WriteLine($"Имя потока: {primaryThread.Name}");
-            Console.WriteLine($"Запущен ли поток?: {primaryThread.IsAlive}");
-            Console.WriteLine($"Приоритет потока: {primaryThread.Priority}");
-            Console.WriteLine($"Состояние потока: {primaryThread.ThreadState}");
-            Console.ReadLine();
-        }
-    }
-}
+// Вывести некоторую статистику о текущем потоке.
+Console.WriteLine($"Идентификатор текущего потока: {primaryThread.ManagedThreadId}");
+Console.WriteLine($"Имя потока: {primaryThread.Name}");
+Console.WriteLine($"Запущен ли поток?: {primaryThread.IsAlive}");
+Console.WriteLine($"Приоритет потока: {primaryThread.Priority}");
+Console.WriteLine($"Состояние потока: {primaryThread.ThreadState}");
+Console.ReadLine();
