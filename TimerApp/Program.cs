5d4785d825856140837b9e4d709866285586791c
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-08-04 12:43:00 +0300

    Обновила целевую среду до .NET 6.

--- TimerApp/Program.cs
+++ TimerApp/Program.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.Threading;
+﻿Console.WriteLine("***** Работа с типом Timer *****");
 
-namespace TimerApp
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Работа с типом Timer *****");
+// Создать делегат для типа Timer.
+var timeCB = new TimerCallback(PrintTime);
 
-            // Создать делегат для типа Timer.
-            TimerCallback timeCB = new TimerCallback(PrintTime);
+// Установить параметры таймера.
+var _ = new Timer(
+  timeCB, // Объект делегата TimerCallback.
+  "Привет из Main", // Информация для передачи в вызванный метод (null, если информация отсутствует).
+  0, // Период ожидания перед запуском (в миллисекундах).
+  1000); // Интервал между вызовами (в миллисекундах).
 
-            // Установить параметры таймера.
-            Timer t = new Timer(
-              timeCB, // Объект делегата TimerCallback.
-              "Привет из Main", // Информация для передачи в вызванный метод (null, если информация отсутствует).
-              0, // Период ожидания перед запуском (в миллисекундах).
-              1000); // Интервал между вызовами (в миллисекундах).
+Console.WriteLine("Нажмите клавишу для завершения...");
+Console.ReadLine();
 
-            Console.WriteLine("Нажмите клавишу для завершения...");
-            Console.ReadLine();
-        }
-
-        static void PrintTime(object state) => Console.WriteLine($"Время: {DateTime.Now.ToLongTimeString()}, параметр: {state.ToString()}");
-    }
-}
+static void PrintTime(object? state) => Console.WriteLine($"Время: {DateTime.Now.ToLongTimeString()}, параметр: {state?.ToString()}");
