65a1e47a362c28fce046322e408ee7ab4642e9df
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-08-04 13:19:39 +0300

    Обновила целевую среду до .NET 6.

--- ThreadPoolApp/Printer.cs
+++ ThreadPoolApp/Printer.cs
@@ -1,26 +1,25 @@
-﻿using System;
-using System.Runtime.Remoting.Contexts;
-using System.Threading;
-
-namespace ThreadPoolApp
+﻿namespace ThreadPoolApp
 {
-    [Synchronization]
     class Printer : ContextBoundObject
     {
+        private readonly object lockToken = new();
         public void PrintNumbers()
         {
-            // Вывести информацию о потоке.
-            Console.WriteLine($"-> {Thread.CurrentThread.Name} выполняет PrintNumbers()");
-
-            // Вывести числа.
-            Console.Write("Ваши числа: ");
-            for (int i = 0; i < 10; i++)
+            lock (lockToken)
             {
-                Random r = new Random();
-                Thread.Sleep(100 * r.Next(5));
-                Console.Write($"{i}, ");
+                // Вывести информацию о потоке.
+                Console.WriteLine($"-> {Thread.CurrentThread.Name} выполняет PrintNumbers()");
+
+                // Вывести числа.
+                Console.Write("Ваши числа: ");
+                for (int i = 0; i < 10; i++)
+                {
+                    var r = new Random();
+                    Thread.Sleep(100 * r.Next(5));
+                    Console.Write($"{i}, ");
+                }
+                Console.WriteLine();
             }
-            Console.WriteLine();
         }
     }
 }
--- ThreadPoolApp/Program.cs
+++ ThreadPoolApp/Program.cs
@@ -1,29 +1,19 @@
-﻿using System;
-using System.Threading;
+﻿using ThreadPoolApp;
 
-namespace ThreadPoolApp
+Console.WriteLine("***** Пул потоков CLR *****");
+
+Console.WriteLine($"Запущен главный поток. ThreadID = {Environment.CurrentManagedThreadId}");
+var p = new Printer();
+var workItem = new WaitCallback(PrintTheNumbers);
+
+// Поставить в очередь метод 10 раз.
+for (int i = 0; i < 10; i++) ThreadPool.QueueUserWorkItem(workItem, p);
+
+Console.WriteLine("Все задачи поставлены в очередь.");
+Console.ReadLine();
+
+static void PrintTheNumbers(object? state)
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Пул потоков CLR *****");
-
-            Console.WriteLine($"Запущен главный поток. ThreadID = {Thread.CurrentThread.ManagedThreadId}");
-            Printer p = new Printer();
-            WaitCallback workItem = new WaitCallback(PrintTheNumbers);
-
-            // Поставить в очередь метод 10 раз.
-            for (int i = 0; i < 10; i++) ThreadPool.QueueUserWorkItem(workItem, p);
-
-            Console.WriteLine("Все задачи поставлены в очередь.");
-            Console.ReadLine();
-        }
-
-        static void PrintTheNumbers(object state)
-        {
-            Printer task = (Printer)state;
-            task.PrintNumbers();
-        }
-    }
+    var task = state as Printer;
+    task?.PrintNumbers();
 }
