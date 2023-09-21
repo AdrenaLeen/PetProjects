041e772722aa4415b43796653f43eaba8284ac50
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-09-21 22:36:47 +0300

    Добавила проект с конфигурированием приложений.

---
+++ FunWithConfiguration/Program.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.Extensions.Configuration;
+
+IConfiguration config = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json", true, true)
+    .Build();
+Console.WriteLine($"Имя моей машины: {config["CarName"]}");
+Console.ReadLine();
