f05116ce3f829a1a5578810c49395819b4b68fe0
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-09-25 20:24:30 +0300

    Обновила целевую среду до .NET 6.

--- CShaprCarClient/Program.cs
+++ CShaprCarClient/Program.cs
@@ -1,23 +1,14 @@
 ﻿using CarLibrary;
-using System;
 
-namespace CShaprCarClient
-{
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Клиентское приложение C# CarLibrary *****");
-            // Создать объект SportsCar.
-            SportsCar viper = new SportsCar("Вайпер", 240, 40);
-            viper.TurboBoost();
+Console.WriteLine("***** Клиентское приложение C# CarLibrary *****");
 
-            // Создать объект MiniVan.
-            MiniVan mv = new MiniVan();
-            mv.TurboBoost();
+// Создать объект SportsCar.
+var viper = new SportsCar("Вайпер", 240, 40);
+viper.TurboBoost();
 
-            Console.WriteLine("Готово. Нажмите любую клавишу для завершения");
-            Console.ReadLine();
-        }
-    }
-}
+// Создать объект MiniVan.
+var mv = new MiniVan();
+mv.TurboBoost();
+
+Console.WriteLine("Готово. Нажмите любую клавишу для завершения");
+Console.ReadLine();
