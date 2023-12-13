3cdeb473c068545208c4a52ce24d74c5222eb8be
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-12-14 00:29:02 +0300

    Обновила целевую среду до .NET 8.

--- ExternalAssemblyReflector/Program.cs
+++ ExternalAssemblyReflector/Program.cs
@@ -1,43 +1,33 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
-namespace ExternalAssemblyReflector
+Console.WriteLine("***** Динамически загружаемые сборки *****");
+do
 {
-    class Program
+    Console.Write("Введите имя сборки для оценки или введите Q для выхода: ");
+
+    // Получить имя сборки.
+    string asmName = Console.ReadLine() ?? string.Empty;
+
+    // Пользователь желает завершить программу?
+    if (asmName.Equals("Q", StringComparison.CurrentCultureIgnoreCase)) break;
+
+    // Попробовать загрузить сборку.
+    try
     {
-        static void DisplayTypesInAsm(Assembly asm)
-        {
-            Console.WriteLine("***** Типы в сборке *****");
-            Console.WriteLine($"->{asm.FullName}");
-            Type[] types = asm.GetTypes();
-            foreach (Type t in types) Console.WriteLine($"Тип: {t}");
-            Console.WriteLine("");
-        }
-
-        static void Main()
-        {
-            Console.WriteLine("***** Динамически загружаемые сборки *****");
-            do
-            {
-                Console.Write("Введите имя сборки для оценки или введите Q для выхода: ");
-
-                // Получить имя сборки.
-                string asmName = Console.ReadLine();
-
-                // Пользователь желает завершить программу?
-                if (asmName.ToUpper() == "Q") break;
-
-                // Попробовать загрузить сборку.
-                try
-                {
-                    Assembly asm = Assembly.LoadFrom(asmName);
-                    DisplayTypesInAsm(asm);
-                }
-                catch
-                {
-                    Console.WriteLine("Сборка не найдена.");
-                }
-            } while (true);
-        }
+        var asm = Assembly.LoadFrom(asmName);
+        DisplayTypesInAsm(asm);
     }
+    catch
+    {
+        Console.WriteLine("Сборка не найдена.");
+    }
+} while (true);
+
+static void DisplayTypesInAsm(Assembly asm)
+{
+    Console.WriteLine("***** Типы в сборке *****");
+    Console.WriteLine($"->{asm.FullName}");
+    Type[] types = asm.GetTypes();
+    foreach (Type t in types) Console.WriteLine($"Тип: {t}");
+    Console.WriteLine("");
 }
