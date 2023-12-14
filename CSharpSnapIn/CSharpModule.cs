3059e844489fc1473b5c62802b5b5048a09090b3
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-12-14 20:52:37 +0300

    Обновила целевую среду до .NET 8.

--- CSharpSnapIn/CSharpModule.cs
+++ CSharpSnapIn/CSharpModule.cs
@@ -1,11 +1,10 @@
 ﻿using CommonSnappableTypes;
-using System.Windows.Forms;
 
 namespace CSharpSnapIn
 {
     [CompanyInfo(CompanyName = "FooBar", CompanyUrl = "www.FooBar.com")]
     public class CSharpModule : IAppFunctionality
     {
-        void IAppFunctionality.DoIt() => MessageBox.Show("Вы только что использовали оснастку C#!");
+        void IAppFunctionality.DoIt() => Console.WriteLine("Вы только что использовали оснастку C#!");
     }
 }
