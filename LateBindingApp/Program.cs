1333c6106623b1dd7f54c1f21fa4c2c7dbd7165d
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-12-14 20:09:45 +0300

    Обновила целевую среду до .NET 8.

--- LateBindingApp/Program.cs
+++ LateBindingApp/Program.cs
@@ -1,76 +1,64 @@
-﻿using System;
-using System.IO;
-using System.Reflection;
+﻿using System.Reflection;
 
-namespace LateBindingApp
+Console.WriteLine("***** Позднее связывание *****");
+// Попробовать загрузить локальную копию CarLibrary.
+Assembly a;
+try
 {
-    // Это приложение будет загружать внешнюю сборку и создавать объект, используя позднее связывание.
-    class Program
+    a = Assembly.LoadFrom("CarLibrary");
+}
+catch (FileNotFoundException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+if (a != null)
+{
+    CreateUsingLateBinding(a);
+    InvokeMethodWithArgsUsingLateBinding(a);
+}
+
+Console.ReadLine();
+
+static void CreateUsingLateBinding(Assembly asm)
+{
+    try
     {
-        static void Main()
-        {
-            Console.WriteLine("***** Позднее связывание *****");
-            // Попробовать загрузить локальную копию CarLibrary.
-            Assembly a;
-            try
-            {
-                a = Assembly.Load("CarLibrary");
-            }
-            catch (FileNotFoundException ex)
-            {
-                Console.WriteLine(ex.Message);
-                return;
-            }
-            if (a != null)
-            {
-                CreateUsingLateBinding(a);
-                InvokeMethodWithArgsUsingLateBinding(a);
-            }
+        // Получить метаданные для типа Minivan.
+        Type miniVan = asm.GetType("CarLibrary.MiniVan") ?? typeof(int);
 
-            Console.ReadLine();
-        }
+        // Создать экземпляр Minivan на лету.
+        object obj = Activator.CreateInstance(miniVan) ?? 0;
+        Console.WriteLine($"Создан {obj}, используя позднее связывание!");
 
-        static void CreateUsingLateBinding(Assembly asm)
-        {
-            try
-            {
-                // Получить метаданные для типа Minivan.
-                Type miniVan = asm.GetType("CarLibrary.MiniVan");
+        // Получить информацию о TurboBoost.
+        MethodInfo? mi = miniVan.GetMethod("TurboBoost");
 
-                // Создать экземпляр Minivan на лету.
-                object obj = Activator.CreateInstance(miniVan);
-                Console.WriteLine($"Создан {obj}, используя позднее связывание!");
+        // Вызвать метод (null означает отсутствие параметров).
+        mi?.Invoke(obj, null);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
 
-                // Получить информацию о TurboBoost.
-                MethodInfo mi = miniVan.GetMethod("TurboBoost");
+static void InvokeMethodWithArgsUsingLateBinding(Assembly asm)
+{
+    try
+    {
+        // Получить описание метаданных для типа SportsCar.
+        Type sport = asm.GetType("CarLibrary.SportsCar") ?? typeof(int);
 
-                // Вызвать метод (null означает отсутствие параметров).
-                mi.Invoke(obj, null);
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+        // Создать объект типа SportsCar.
+        object obj = Activator.CreateInstance(sport) ?? 0;
 
-        static void InvokeMethodWithArgsUsingLateBinding(Assembly asm)
-        {
-            try
-            {
-                // Получить описание метаданных для типа SportsCar.
-                Type sport = asm.GetType("CarLibrary.SportsCar");
-
-                // Создать объект типа SportsCar.
-                object obj = Activator.CreateInstance(sport);
-
-                // Вызвать метод TurnOnRadio() с аргументами.
-                MethodInfo mi = sport.GetMethod("TurnOnRadio");
-                mi.Invoke(obj, new object[] { true, 2 });
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+        // Вызвать метод TurnOnRadio() с аргументами.
+        MethodInfo? mi = sport.GetMethod("TurnOnRadio");
+        mi?.Invoke(obj, new object[] { true, 2 });
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
     }
 }
