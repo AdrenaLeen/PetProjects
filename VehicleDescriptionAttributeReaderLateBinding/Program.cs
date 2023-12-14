7fcb42fbf08c85d175fe238d122243c81b3a371c
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2023-12-14 20:41:13 +0300

    Обновила целевую среду до .NET 8.

--- VehicleDescriptionAttributeReaderLateBinding/Program.cs
+++ VehicleDescriptionAttributeReaderLateBinding/Program.cs
@@ -1,46 +1,36 @@
-﻿using System;
-using System.Reflection;
+﻿using System.Reflection;
 
-namespace VehicleDescriptionAttributeReaderLateBinding
+Console.WriteLine("***** Значение VehicleDescriptionAttribute *****");
+ReflectAttributesUsingLateBinding();
+Console.ReadLine();
+
+static void ReflectAttributesUsingLateBinding()
 {
-    class Program
+    try
     {
-        static void Main()
+        // Загрузить локальную копию сборки AttributedCarLibrary.
+        var asm = Assembly.LoadFrom("AttributedCarLibrary");
+
+        // Получить информацию о типе VehicleDescriptionAttribute.
+        Type vehicleDesc = asm.GetType("AttributedCarLibrary.VehicleDescriptionAttribute") ?? typeof(int);
+
+        // Получить информацию о типе свойства Description.
+        PropertyInfo? propDesc = vehicleDesc.GetProperty("Description");
+
+        // Получить все типы в сборке.
+        Type[] types = asm.GetTypes();
+
+        // Пройти по всем типам и получить любые атрибуты VehicleDescriptionAttributes.
+        foreach (Type t in types)
         {
-            Console.WriteLine("***** Значение VehicleDescriptionAttribute *****");
-            ReflectAttributesUsingLateBinding();
-            Console.ReadLine();
+            object[] objs = t.GetCustomAttributes(vehicleDesc, false);
+
+            // Пройти по каждому VehicleDescriptionAttribute и вывести описание, используя позднее связывание.
+            foreach (object o in objs) Console.WriteLine($"-> {t.Name}: {propDesc?.GetValue(o, null)}");
         }
-
-        private static void ReflectAttributesUsingLateBinding()
-        {
-            try
-            {
-                // Загрузить локальную копию сборки AttributedCarLibrary.
-                Assembly asm = Assembly.Load("AttributedCarLibrary");
-
-                // Получить информацию о типе VehicleDescriptionAttribute.
-                Type vehicleDesc = asm.GetType("AttributedCarLibrary.VehicleDescriptionAttribute");
-
-                // Получить информацию о типе свойства Description.
-                PropertyInfo propDesc = vehicleDesc.GetProperty("Description");
-
-                // Получить все типы в сборке.
-                Type[] types = asm.GetTypes();
-
-                // Пройти по всем типам и получить любые атрибуты VehicleDescriptionAttributes.
-                foreach (Type t in types)
-                {
-                    object[] objs = t.GetCustomAttributes(vehicleDesc, false);
-
-                    // Пройти по каждому VehicleDescriptionAttribute и вывести описание, используя позднее связывание.
-                    foreach (object o in objs) Console.WriteLine($"-> {t.Name}: {propDesc.GetValue(o, null)}");
-                }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
     }
 }
