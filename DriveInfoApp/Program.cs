6e35a8a27bc44d5b00424f3c3457c811ebac9474
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-02-12 13:22:47 +0300

    Обновила целевую среду до .NET 8.

--- DriveInfoApp/Program.cs
+++ DriveInfoApp/Program.cs
@@ -1,33 +1,21 @@
-﻿using System;
-using System.IO;
+﻿Console.WriteLine("***** DriveInfo *****");
 
-namespace DriveInfoApp
+// Получить информацию обо всех устройствах.
+DriveInfo[] myDrives = DriveInfo.GetDrives();
+
+// Вывести сведения об устройствах.
+foreach (DriveInfo d in myDrives)
 {
-    class Program
+    Console.WriteLine($"Имя: {d.Name}");
+    Console.WriteLine($"Тип: {d.DriveType}");
+
+    // Проверить, смонтировано ли устройство.
+    if (d.IsReady)
     {
-        static void Main()
-        {
-            Console.WriteLine("***** DriveInfo *****");
-
-            // Получить информацию обо всех устройствах.
-            DriveInfo[] myDrives = DriveInfo.GetDrives();
-
-            // Вывести сведения об устройствах.
-            foreach (DriveInfo d in myDrives)
-            {
-                Console.WriteLine($"Имя: {d.Name}");
-                Console.WriteLine($"Тип: {d.DriveType}");
-
-                // Проверить, смонтировано ли устройство.
-                if (d.IsReady)
-                {
-                    Console.WriteLine($"Свободное пространство: {d.TotalFreeSpace}");
-                    Console.WriteLine($"Формат устройства: {d.DriveFormat}");
-                    Console.WriteLine($"Метка тома: {d.VolumeLabel}");
-                }
-                Console.WriteLine();
-            }
-            Console.ReadLine();
-        }
+        Console.WriteLine($"Свободное пространство: {d.TotalFreeSpace}");
+        Console.WriteLine($"Формат устройства: {d.DriveFormat}");
+        Console.WriteLine($"Метка тома: {d.VolumeLabel}");
     }
+    Console.WriteLine();
 }
+Console.ReadLine();
