66b997a2e1662746dff9d9575d811ace91361dde
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-02-12 13:49:40 +0300

    Обновила целевую среду до .NET 8.

--- StreamWriterReaderApp/Program.cs
+++ StreamWriterReaderApp/Program.cs
@@ -1,36 +1,24 @@
-﻿using System;
-using System.IO;
+﻿Console.WriteLine("***** StreamWriter / StreamReader *****");
 
-namespace StreamWriterReaderApp
+// Получить объект StreamWriter и записать строковые данные.
+using (StreamWriter writer = File.CreateText("reminders.txt"))
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** StreamWriter / StreamReader *****");
+    writer.WriteLine("Не забыть день матери в этом году...");
+    writer.WriteLine("Не забыть день отца в этом году...");
+    writer.WriteLine("Не забыть эти числа:");
+    for (int i = 0; i < 10; i++) writer.Write($"{i} ");
 
-            // Получить объект StreamWriter и записать строковые данные.
-            using (StreamWriter writer = File.CreateText("reminders.txt"))
-            {
-                writer.WriteLine("Не забыть день матери в этом году...");
-                writer.WriteLine("Не забыть день отца в этом году...");
-                writer.WriteLine("Не забыть эти числа:");
-                for (int i = 0; i < 10; i++) writer.Write($"{i} ");
-
-                // Вставить новую строку.
-                writer.Write(writer.NewLine);
-            }
-            Console.WriteLine("Создать файл и записать некоторые мысли...");
-
-            // Прочитать данные из файла.
-            Console.WriteLine("Вот ваши мысли:");
-            using (StreamReader sr = File.OpenText("reminders.txt"))
-            {
-                string input = null;
-                while ((input = sr.ReadLine()) != null) Console.WriteLine(input);
-            }
-
-            Console.ReadLine();
-        }
-    }
+    // Вставить новую строку.
+    writer.Write(writer.NewLine);
 }
+Console.WriteLine("Создать файл и записать некоторые мысли...");
+
+// Прочитать данные из файла.
+Console.WriteLine("Вот ваши мысли:");
+using (StreamReader sr = File.OpenText("reminders.txt"))
+{
+    string? input = null;
+    while ((input = sr.ReadLine()) != null) Console.WriteLine(input);
+}
+
+Console.ReadLine();
