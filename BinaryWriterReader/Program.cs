11a9ac8a7622374aac7a73623fa8406addea69f4
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-02-12 13:59:10 +0300

    Обновила целевую среду до .NET 8.

--- BinaryWriterReader/Program.cs
+++ BinaryWriterReader/Program.cs
@@ -1,42 +1,30 @@
-﻿using System;
-using System.IO;
+﻿Console.WriteLine("***** Binary Writer / Reader *****");
 
-namespace BinaryWriterReader
+// Открыть средство двоичной записи в файл.
+var f = new FileInfo("BinFile.dat");
+using (var bw = new BinaryWriter(f.OpenWrite()))
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Binary Writer / Reader *****");
+    // Вывести на консоль тип BaseStream (System.IO.FileStream в данном случае).
+    Console.WriteLine($"Основной поток: {bw.BaseStream}");
 
-            // Открыть средство двоичной записи в файл.
-            FileInfo f = new FileInfo("BinFile.dat");
-            using (BinaryWriter bw = new BinaryWriter(f.OpenWrite()))
-            {
-                // Вывести на консоль тип BaseStream (System.IO.FileStream в данном случае).
-                Console.WriteLine($"Основной поток: {bw.BaseStream}");
+    // Создать некоторые данные для сохранения в файле.
+    double aDouble = 1234.67;
+    int anInt = 34567;
+    string aString = "A, B, C";
 
-                // Создать некоторые данные для сохранения в файле.
-                double aDouble = 1234.67;
-                int anInt = 34567;
-                string aString = "A, B, C";
-
-                // Записать данные.
-                bw.Write(aDouble);
-                bw.Write(anInt);
-                bw.Write(aString);
-            }
-            Console.WriteLine("Готово!");
-
-            // Читать двоичные данные из потока.
-            using (BinaryReader br = new BinaryReader(f.OpenRead()))
-            {
-                Console.WriteLine(br.ReadDouble());
-                Console.WriteLine(br.ReadInt32());
-                Console.WriteLine(br.ReadString());
-            }
-
-            Console.ReadLine();
-        }
-    }
+    // Записать данные.
+    bw.Write(aDouble);
+    bw.Write(anInt);
+    bw.Write(aString);
 }
+Console.WriteLine("Готово!");
+
+// Читать двоичные данные из потока.
+using (var br = new BinaryReader(f.OpenRead()))
+{
+    Console.WriteLine(br.ReadDouble());
+    Console.WriteLine(br.ReadInt32());
+    Console.WriteLine(br.ReadString());
+}
+
+Console.ReadLine();
