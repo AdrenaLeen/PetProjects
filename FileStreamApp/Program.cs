2dd9890f177ea95f1d49321e969592a1c9c51424
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-02-12 13:42:11 +0300

    Обновила целевую среду до .NET 8.

--- FileStreamApp/Program.cs
+++ FileStreamApp/Program.cs
@@ -1,44 +1,33 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 
-namespace FileStreamApp
+Console.WriteLine("***** Класс FileStream *****");
+
+// Получить объект FileStream.
+using (FileStream fStream = File.Open(@"D:\OneDrive\myMessage.dat", FileMode.Create))
 {
-    class Program
+    // Закодировать строку в массив байтов.
+    string msg = "Привет!";
+    byte[] msgAsByteArray = Encoding.Default.GetBytes(msg);
+
+    // Записать byte[] в файл.
+    fStream.Write(msgAsByteArray, 0, msgAsByteArray.Length);
+
+    // Сбросить внутреннюю позицию потока.
+    fStream.Position = 0;
+
+    // Прочитать byte[] из файла и вывести на консоль.
+    Console.Write("Ваше сообщение как массив байтов: ");
+    byte[] bytesFromFile = new byte[msgAsByteArray.Length];
+    for (int i = 0; i < msgAsByteArray.Length; i++)
     {
-        static void Main()
-        {
-            Console.WriteLine("***** Класс FileStream *****");
-
-            // Получить объект FileStream.
-            using (FileStream fStream = File.Open(@"D:\OneDrive\myMessage.dat", FileMode.Create))
-            {
-                // Закодировать строку в массив байтов.
-                string msg = "Привет!";
-                byte[] msgAsByteArray = Encoding.Default.GetBytes(msg);
-
-                // Записать byte[] в файл.
-                fStream.Write(msgAsByteArray, 0, msgAsByteArray.Length);
-
-                // Сбросить внутреннюю позицию потока.
-                fStream.Position = 0;
-
-                // Прочитать byte[] из файла и вывести на консоль.
-                Console.Write("Ваше сообщение как массив байтов: ");
-                byte[] bytesFromFile = new byte[msgAsByteArray.Length];
-                for (int i = 0; i < msgAsByteArray.Length; i++)
-                {
-                    bytesFromFile[i] = (byte)fStream.ReadByte();
-                    Console.Write(bytesFromFile[i]);
-                }
-                Console.WriteLine();
-
-                // Вывести декодированное сообщение.
-                Console.Write("Декодированное сообщение: ");
-                Console.WriteLine(Encoding.Default.GetString(bytesFromFile));
-            }
-
-            Console.ReadLine();
-        }
+        bytesFromFile[i] = (byte)fStream.ReadByte();
+        Console.Write(bytesFromFile[i]);
     }
+    Console.WriteLine();
+
+    // Вывести декодированное сообщение.
+    Console.Write("Декодированное сообщение: ");
+    Console.WriteLine(Encoding.Default.GetString(bytesFromFile));
 }
+
+Console.ReadLine();
