e6002f1c71c7b1ddd23a72c7eae247a6bc2945f6
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-02-12 13:35:42 +0300

    Обновила целевую среду до .NET 8.

--- SimpleFileIO/Program.cs
+++ SimpleFileIO/Program.cs
@@ -1,28 +1,16 @@
-﻿using System;
-using System.IO;
-
-namespace SimpleFileIO
+﻿Console.WriteLine("***** Просто ввод-вывод с типом File *****");
+try
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Просто ввод-вывод с типом File *****");
-            try
-            {
-                string[] myTasks = { "Починить раковину в ванной", "Позвонить Дэйву", "Позвонить маме и папе", "Поиграть в Xbox 360"};
+    string[] myTasks = ["Починить раковину в ванной", "Позвонить Дэйву", "Позвонить маме и папе", "Поиграть в Xbox 360"];
 
-                // Записать все данные в файл на диске D:.
-                File.WriteAllLines(@"D:\tasks.txt", myTasks);
+    // Записать все данные в файл на диске D:.
+    File.WriteAllLines(@"D:\tasks.txt", myTasks);
 
-                // Прочитать все данные и вывести на консоль.
-                foreach (string task in File.ReadAllLines(@"D:\tasks.txt")) Console.WriteLine($"TODO: {task}");
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex.Message);
-            }
-            Console.ReadLine();
-        }
-    }
+    // Прочитать все данные и вывести на консоль.
+    foreach (string task in File.ReadAllLines(@"D:\tasks.txt")) Console.WriteLine($"TODO: {task}");
 }
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+Console.ReadLine();
