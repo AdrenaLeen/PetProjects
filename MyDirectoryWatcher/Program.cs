2d27bd04c4f6d89372f3b3849764309c13f79f41
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-02-12 23:11:46 +0300

    Обновила целевую среду до .NET 8.

--- MyDirectoryWatcher/Program.cs
+++ MyDirectoryWatcher/Program.cs
@@ -1,54 +1,47 @@
-﻿using System;
-using System.IO;
+﻿using System.Threading.Channels;
 
-namespace MyDirectoryWatcher
+Console.WriteLine("***** Программное слежение за файлами *****");
+
+// Установить путь к каталогу, за которым нужно наблюдать.
+var watcher = new FileSystemWatcher();
+
+try
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** Программное слежение за файлами *****");
-
-            // Установить путь к каталогу, за которым нужно наблюдать.
-            using (FileSystemWatcher watcher = new FileSystemWatcher())
-            {
-                try
-                {
-                    watcher.Path = @"D:\MyFolder";
-                }
-                catch (ArgumentException ex)
-                {
-                    Console.WriteLine(ex.Message);
-                    return;
-                }
-
-                // Указать цели наблюдения.
-                watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
-
-                // Следить только за текстовыми файлами.
-                watcher.Filter = "*.txt";
-
-                // Добавить обработчики событий.
-                watcher.Changed += new FileSystemEventHandler(OnChanged);
-                watcher.Created += new FileSystemEventHandler(OnChanged);
-                watcher.Deleted += new FileSystemEventHandler(OnChanged);
-                watcher.Renamed += new RenamedEventHandler(OnRenamed);
-
-                // Начать наблюдение за каталогом.
-                watcher.EnableRaisingEvents = true;
-            }
-
-            // Ожидать команду пользователя на завершение программы.
-            Console.WriteLine(@"Нажмите 'q', чтобы завершить программу.");
-            while (Console.Read() != 'q') ;
-        }
-
-        static void OnChanged(object source, FileSystemEventArgs e) =>
-            // Сообщить о действии изменения, создания или удаления файла.
-            Console.WriteLine($"Файл: {e.FullPath} {e.ChangeType}!");
-
-        static void OnRenamed(object source, RenamedEventArgs e) =>
-            // Сообщить о действии переименования файла.
-            Console.WriteLine($"Файл: {e.OldFullPath} переименован в {e.FullPath}");
-    }
+    watcher.Path = @".";
 }
+catch (ArgumentException ex)
+{
+    Console.WriteLine(ex.Message);
+    return;
+}
+
+// Указать цели наблюдения.
+watcher.NotifyFilter = NotifyFilters.LastAccess | NotifyFilters.LastWrite | NotifyFilters.FileName | NotifyFilters.DirectoryName;
+
+// Следить только за текстовыми файлами.
+watcher.Filter = "*.txt";
+
+// Добавить обработчики событий.
+// Указать, что будет происходить при изменении, создании или удалении файла.
+watcher.Changed += (s, e) => Console.WriteLine($"Файл: {e.FullPath} {e.ChangeType}!");
+watcher.Created += (s, e) => Console.WriteLine($"Файл: {e.FullPath} {e.ChangeType}!");
+watcher.Deleted += (s, e) => Console.WriteLine($"Файл: {e.FullPath} {e.ChangeType}!");
+
+// Указать, что будет происходить при переименовании файла.
+watcher.Renamed += (s, e) => Console.WriteLine($"Файл: {e.OldFullPath} переименован в {e.FullPath}");
+
+// Начать наблюдение за каталогом.
+watcher.EnableRaisingEvents = true;
+
+// Ожидать команду пользователя на завершение программы.
+Console.WriteLine(@"Нажмите 'q', чтобы завершить программу.");
+
+// Сгенерировать несколько событий.
+using (var sw = File.CreateText("Test.txt"))
+{
+    sw.Write("Немного текста");
+}
+File.Move("Test.txt", "Test2.txt");
+File.Delete("Test2.txt");
+
+while (Console.Read() != 'q') ;
