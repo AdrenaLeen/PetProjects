263f5ee7b85339d8e3da1296a9ac736f3c02621d
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-02-12 13:53:16 +0300

    Обновила целевую среду до .NET 8.

--- StringReaderWriterApp/Program.cs
+++ StringReaderWriterApp/Program.cs
@@ -1,44 +1,31 @@
-﻿using System;
-using System.IO;
-using System.Text;
+﻿using System.Text;
 
-namespace StringReaderWriterApp
+Console.WriteLine("***** StringWriter / StringReader *****");
+
+// Создать объект StringWriter и записать символьные данные в память.
+using (var strWriter = new StringWriter())
 {
-    class Program
-    {
-        static void Main()
-        {
-            Console.WriteLine("***** StringWriter / StringReader *****");
+    strWriter.WriteLine("Не забыть день матери в этом году...");
 
-            // Создать объект StringWriter и записать символьные данные в память.
-            using (StringWriter strWriter = new StringWriter())
-            {
-                strWriter.WriteLine("Не забыть день матери в этом году...");
+    // Получить копию содержимого (хранящегося в строке) и вывести на консоль.
+    Console.WriteLine($"Содержимое StringWriter: {strWriter}");
 
-                // Получить копию содержимого (хранящегося в строке) и вывести на консоль.
-                Console.WriteLine($"Содержимое StringWriter: {strWriter}");
-
-                // Получить внутренний объект StringBuilder.
-                StringBuilder sb = strWriter.GetStringBuilder();
-                sb.Insert(0, "Привет!! ");
-                Console.WriteLine($"-> {sb}");
-                sb.Remove(0, "Привет!! ".Length);
-                Console.WriteLine($"-> {sb}");
-            }
-            using (StringWriter strWriter = new StringWriter())
-            {
-                strWriter.WriteLine("Не забыть день матери в этом году...");
-                Console.WriteLine($"Содержимое StringWriter: {strWriter}");
-
-                // Читать данные из объекта StringWriter.
-                using (StringReader strReader = new StringReader(strWriter.ToString()))
-                {
-                    string input = null;
-                    while ((input = strReader.ReadLine()) != null) Console.WriteLine(input);
-                }
-            }
-
-            Console.ReadLine();
-        }
-    }
+    // Получить внутренний объект StringBuilder.
+    StringBuilder sb = strWriter.GetStringBuilder();
+    sb.Insert(0, "Привет!! ");
+    Console.WriteLine($"-> {sb}");
+    sb.Remove(0, "Привет!! ".Length);
+    Console.WriteLine($"-> {sb}");
 }
+using (var strWriter = new StringWriter())
+{
+    strWriter.WriteLine("Не забыть день матери в этом году...");
+    Console.WriteLine($"Содержимое StringWriter: {strWriter}");
+
+    // Читать данные из объекта StringWriter.
+    using var strReader = new StringReader(strWriter.ToString());
+    string? input = null;
+    while ((input = strReader.ReadLine()) != null) Console.WriteLine(input);
+}
+
+Console.ReadLine();
