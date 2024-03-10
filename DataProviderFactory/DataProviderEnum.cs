141d6c9a7f38cb06ce9aae2a50057a9a5b0d73b9
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-03-11 01:01:47 +0300

    Обновила целевую среду до .NET 8.

---
+++ DataProviderFactory/DataProviderEnum.cs
@@ -0,0 +1,13 @@
+﻿namespace DataProviderFactory
+{
+    // OleDb поддерживается только в Windows, но не в .NET Core.
+    enum DataProviderEnum
+    {
+        SqlServer,
+#if PC
+        OleDb,
+#endif
+        Odbc,
+        PostgreSQL
+    }
+}
--- DataProviderFactory/Program.cs
+++ DataProviderFactory/Program.cs
@@ -1,64 +1,74 @@
-﻿using System;
-using System.Configuration;
-using System.Data.Common;
-using System.Data.SqlClient;
+﻿using System.Data.Common;
+using System.Data.Odbc;
+#if PC
+using System.Data.OleDb;
+#endif
+using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Configuration;
+using DataProviderFactory;
+using Npgsql;
 
-namespace DataProviderFactory
+Console.WriteLine("***** Фабрика поставщиков данных *****");
+
+var (provider, connectionString) = GetProviderFromConfiguration();
+DbProviderFactory? factory = GetDbProviderFactory(provider);
+
+// Получить объект подключения.
+using (DbConnection? connection = factory?.CreateConnection())
 {
-    class Program
+    if (connection == null)
     {
-        static void Main()
-        {
-            Console.WriteLine("***** Фабрика поставщиков данных *****");
-
-            // Получить строку подключения и поставщик из файла *.config.
-            string dataProvider = ConfigurationManager.AppSettings["provider"];
-            string connectionString = ConfigurationManager.ConnectionStrings["AutoLotSqlProvider"].ConnectionString;
-
-            // Получить фабрику поставщиков.
-            DbProviderFactory factory = DbProviderFactories.GetFactory(dataProvider);
-
-            // Получить объект подключения.
-            using (DbConnection connection = factory.CreateConnection())
-            {
-                if (connection == null)
-                {
-                    ShowError("Connection");
-                    return;
-                }
-                Console.WriteLine($"Ваш объект подключения: {connection.GetType().Name}");
-                connection.ConnectionString = connectionString;
-                connection.Open();
-
-                // Вывести информацию об используемой версии SQL Server.
-                if (connection is SqlConnection sqlConnection) Console.WriteLine(sqlConnection.ServerVersion);
-
-                // Создать объект команды.
-                DbCommand command = factory.CreateCommand();
-                if (command == null)
-                {
-                    ShowError("Command");
-                    return;
-                }
-                Console.WriteLine($"Ваш объект команды: {command.GetType().Name}");
-                command.Connection = connection;
-                command.CommandText = "Select * From Inventory";
-
-                // Вывести данные с помощью объекта чтения данных.
-                using (DbDataReader dataReader = command.ExecuteReader())
-                {
-                    Console.WriteLine($"Ваш объект чтения данных: {dataReader.GetType().Name}");
-                    Console.WriteLine("***** Inventory *****");
-                    while (dataReader.Read()) Console.WriteLine($"-> Машина #{dataReader["CarId"]} - это {dataReader["Make"]}.");
-                }
-            }
-            Console.ReadLine();
-        }
-
-        private static void ShowError(string objectName)
-        {
-            Console.WriteLine($"Возникла проблема с созданием объекта {objectName}");
-            Console.ReadLine();
-        }
+        Console.WriteLine("Не удалость создать объект подключения");
+        return;
     }
+    Console.WriteLine($"Ваш объект подключения: {connection.GetType().Name}");
+    connection.ConnectionString = connectionString;
+    connection.Open();
+
+    // Вывести информацию об используемой версии SQL Server.
+    if (connection is SqlConnection sqlConnection) Console.WriteLine(sqlConnection.ServerVersion);
+
+    // Создать объект команды.
+    DbCommand? command = factory?.CreateCommand();
+    if (command == null)
+    {
+        Console.WriteLine("Не удалось создать объект команды");
+        return;
+    }
+    Console.WriteLine($"Ваш объект команды: {command.GetType().Name}");
+    command.Connection = connection;
+    command.CommandText = "SELECT i.\"Id\", m.\"Name\" FROM \"Inventory\" i INNER JOIN \"Makes\" m ON m.\"Id\" = i.\"MakeId\"";
+
+    // Вывести данные с помощью объекта чтения данных.
+    using DbDataReader dataReader = command.ExecuteReader();
+    Console.WriteLine($"Ваш объект чтения данных: {dataReader.GetType().Name}");
+    Console.WriteLine("***** Inventory *****");
+    while (dataReader.Read()) Console.WriteLine($"-> Машина #{dataReader["Id"]} - это {dataReader["Name"]}.");
+}
+Console.ReadLine();
+
+static DbProviderFactory? GetDbProviderFactory(DataProviderEnum provider)
+    => provider switch
+    {
+        DataProviderEnum.SqlServer => SqlClientFactory.Instance,
+        DataProviderEnum.Odbc => OdbcFactory.Instance,
+#if PC
+        DataProviderEnum.OleDb => OleDbFactory.Instance,
+#endif
+        DataProviderEnum.PostgreSQL => NpgsqlFactory.Instance,
+        _ => null
+    };
+
+static (DataProviderEnum provider, string? connectionString) GetProviderFromConfiguration()
+{
+    IConfiguration config = new ConfigurationBuilder()
+        .SetBasePath(Directory.GetCurrentDirectory())
+        .AddJsonFile("appsettings.json", true, true)
+        .Build();
+    var providerName = config["ProviderName"];
+    if (Enum.TryParse(providerName, out DataProviderEnum provider))
+    {
+        return (provider, config[$"{providerName}:ConnectionString"]);
+    };
+    throw new Exception("Получено недопустимое значение поставщика данных.");
 }
