ea11c3e3ebba88a0e4f02fbf574b7020f79d4980
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-05-27 22:21:10 +0300

    Удалила проект Filters.

--- Filters/Controllers/GlobalController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace Filters.Controllers
-{
-    public class GlobalController : Controller
-    {
-        public ViewResult Index() => View("Message", "Это глобальный контроллер");
-    }
-}
