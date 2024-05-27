7b915edcdfde073df82e4069bcc3ae983dab0799
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-05-27 22:27:10 +0300

    Удалила проект UsingViewComponents.

--- UsingViewComponents/Models/City.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace UsingViewComponents.Models
-{
-    public class City
-    {
-        public string Name { get; set; }
-        public string Country { get; set; }
-        public int Population { get; set; }
-    }
-}
--- UsingViewComponents/Models/CityViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UsingViewComponents.Models
-{
-    public class CityViewModel
-    {
-        public int Cities { get; set; }
-        public int Population { get; set; }
-    }
-}
