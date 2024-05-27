5de31a1fa1bd9bd77da67cda8336414f488828c9
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-05-27 22:07:55 +0300

    Удалила проект WorkingWithVisualStudio.

--- WorkingWithVisualStudio/wwwroot/js/fourth.js
+++
@@ -1,5 +0,0 @@
-﻿document.addEventListener("DOMContentLoaded", function () {
-    var element = document.createElement("p");
-    element.textContent = "Это элемент из файла fourth.js";
-    document.querySelector("body").appendChild(element);
-});
