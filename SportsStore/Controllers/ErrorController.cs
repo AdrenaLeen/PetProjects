bf67826b8ca399d97aa391d9c05e4d1a147dad7e
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-05-27 22:29:24 +0300

    Удалила проект SportsStore.

--- SportsStore/Controllers/ErrorController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
-namespace SportsStore.Controllers
-{
-    public class ErrorController : Controller
-    {
-        public ViewResult Error() => View();
-    }
-}
--- SportsStore/Models/ViewModels/CartIndexViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace SportsStore.Models.ViewModels
-{
-    public class CartIndexViewModel
-    {
-        public Cart Cart { get; set; }
-        public string ReturnUrl { get; set; }
-    }
-}
