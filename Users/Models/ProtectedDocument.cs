b1fb5f1f217eb253ce7dfbc808ab3ef5b10eaa10
Кузнецова Ксения Сергеевна <xskuznetsova@gmail.com>
2024-05-27 22:24:46 +0300

    Удалила проект Users.

--- Users/Models/ProtectedDocument.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Users.Models
-{
-    public class ProtectedDocument
-    {
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Editor { get; set; }
-    }
-}
